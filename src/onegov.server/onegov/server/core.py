d481717c4ea86c9853f8e6fb3b359ad7603c11d2
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-02-17 13:46:44 +0100

    Pass the correct script_name and path_info

--- src/onegov.server/onegov/server/core.py
+++ src/onegov.server/onegov/server/core.py
@@ -36,7 +36,8 @@ class Server(object):
             a.root for a in config.applications if not a.is_static)
 
     def __call__(self, environ, start_response):
-        path_fragments = BaseRequest(environ).path.split('/')
+        request = BaseRequest(environ)
+        path_fragments = request.path.split('/')
 
         application_root = '/'.join(path_fragments[:2])
         application = self.applications.get(application_root)
@@ -55,6 +56,9 @@ class Server(object):
         # ('/wildcard' from '/wildcard/*') - this is not allowed
         if not application_id:
             return HTTPNotFound()(environ, start_response)
+
+        environ['SCRIPT_NAME'] = base_path
+        environ['PATH_INFO'] = request.path.replace(base_path, '')
 
         application.set_application_base_path(base_path)
         application.set_application_id(application_id)
--- src/onegov.server/onegov/server/tests/test_server.py
+++ src/onegov.server/onegov/server/tests/test_server.py
@@ -1,7 +1,7 @@
 from onegov.server.application import Application
 from onegov.server.core import Server
 from onegov.server.config import Config
-from webob import Response
+from webob import BaseRequest, Response
 from webtest import TestApp as Client
 
 
@@ -45,3 +45,38 @@ def test_application_mapping():
     c.get('/wildcard', status=404)
     c.get('/wildcard/', status=404)
     c.get('/not-there', status=404)
+
+
+def test_path_prefix():
+
+    class EchoApplication(Application):
+
+        def __call__(self, environ, start_response):
+            request = BaseRequest(environ)
+
+            response = Response()
+            response.text = u', '.join(
+                (request.script_name, request.path_info))
+
+            return response(environ, start_response)
+
+    server = Server(Config({
+        'applications': [
+            {
+                'path': '/static',
+                'class': EchoApplication
+            },
+            {
+                'path': '/wildcard/*',
+                'class': EchoApplication
+            },
+        ]
+    }))
+
+    c = Client(server)
+    c.get('/static').body == b'/static, '
+    c.get('/static/info').body == b'/static, /info'
+    c.get('/static/info/test').body == b'/static, /info/test'
+    c.get('/wildcard/info').body == b'/wildcard/info, '
+    c.get('/wildcard/info/test').body == b'/wildcard/info, /test'
+    c.get('/wildcard/x/y/z').body == b'/wildcard/x, /y/z'
