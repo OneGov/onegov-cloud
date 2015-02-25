369046c0ac9665990d0f9efd219799793dab4bb4
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-02-25 09:25:47 +0100

    Document Framework methods/attributes

--- src/onegov.core/onegov/core/framework.py
+++ src/onegov.core/onegov/core/framework.py
@@ -13,11 +13,35 @@ class Framework(TransactionApp, ServerApplication):
     #: holds the database connection string, *if* there is a database connected
     dsn = None
 
+    #: holdes the current schema associated with the database connection, set
+    #: by and derived from :meth:`set_application_id`.
+    schema = None
+
     @property
     def has_database_connection(self):
+        """ onegov.core has good integration for Postgres using SQLAlchemy, but
+        it doesn't require a connection.
+
+        It's possible to have Onegov applications using a different database
+        or not using one at all.
+
+        """
         return self.dsn is not None
 
     def configure_application(self, **configuration):
+        """ Configures the application, supporting the following parameters:
+
+        :dsn:
+            The database connection to use. May be None.
+
+            See :meth:`onegov.core.orm.session_manager.setup`
+
+        :base:
+            The declarative base class used. By default,
+            :attr:`onegov.core.orm.Base`is used.
+
+        """
+
         super(Framework, self).configure_application(**configuration)
 
         self.dsn = configuration.get('dsn')
@@ -28,6 +52,11 @@ class Framework(TransactionApp, ServerApplication):
             self.session_manager.setup(self.dsn, self.declarative_base)
 
     def set_application_id(self, application_id):
+        """ Set before the request is handled. Gets the schema from the
+        application id and makes sure it exists, *if* a database connection
+        is present.
+
+        """
         super(Framework, self).set_application_id(application_id)
         self.schema = application_id.replace('/', '-')
 
