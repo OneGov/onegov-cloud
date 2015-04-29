9822da8a1e56eadbf4e3f5dfc81197587b85e66a
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-04-29 15:20:37 +0200

    "Release 0.0.1"

--- src/onegov.server/HISTORY.rst
+++ src/onegov.server/HISTORY.rst
@@ -3,3 +3,8 @@ Changelog
 
 Unreleased
 ~~~~~~~~~~
+
+0.0.1 (2015-04-29)
+~~~~~~~~~~~~~~~~~~~
+
+- Initial Release [href]
--- src/onegov.server/setup.py
+++ src/onegov.server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.server'
 description = 'Serves OneGov applications.'
-version = '0.0.0'
+version = '0.0.1'
 
 
 def get_long_description():
