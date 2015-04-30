079e08a0665e2aead7ff344d5863fd48054f6b4e
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-04-30 16:26:53 +0200

    Stop the editor from stealing the focus

--- src/onegov.town/onegov/town/assets/js/load-editor.js
+++ src/onegov.town/onegov/town/assets/js/load-editor.js
@@ -5,6 +5,5 @@ for (var i=0; i<inputs.length; i++) {
     inputs[i].markdown_editor = new Editor({
         status: false,
         element: inputs[i]
-    });    
-    inputs[i].markdown_editor.render();
+    });
 }
