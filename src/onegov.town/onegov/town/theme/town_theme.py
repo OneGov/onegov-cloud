17e50b1d00586c2d3b1e6960ab4eb00e5517ccfb
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-05-01 11:05:06 +0200

    Make bumpversion work with the theme

--- src/onegov.town/onegov/town/theme/town_theme.py
+++ src/onegov.town/onegov/town/theme/town_theme.py
@@ -10,7 +10,10 @@ user_colors = {
 
 class TownTheme(BaseTheme):
     name = 'onegov.town.foundation'
-    version = '1.0'
+
+    # don't touch this number, it's incremented using bumpversion, so every
+    # release will automatically trigger a rebuild of the theme
+    version = '0.0.1'
 
     @property
     def default_options(self):
