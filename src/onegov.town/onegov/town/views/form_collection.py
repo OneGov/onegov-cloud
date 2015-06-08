55679934670569f9add6c512bd8c10c31cc76649
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-06-08 17:04:15 +0200

    Add submission counts to forms view

--- src/onegov.town/onegov/town/views/form_collection.py
+++ src/onegov.town/onegov/town/views/form_collection.py
@@ -1,15 +1,44 @@
 """ Lists the builtin and custom forms. """
 
 from onegov.core.security import Public
-from onegov.form import FormCollection, FormDefinition
+from onegov.form import FormCollection, FormDefinition, FormSubmission
 from onegov.town import _
 from onegov.town.app import TownApp
 from onegov.town.layout import FormCollectionLayout
 
+from sqlalchemy import func
+
+
+def get_definitions_with_submission_count(session):
+    """ Returns all form definitions and the number of submissions belonging
+    to those definitions, in a single query.
+
+    """
+    submissions = session.query(
+        FormSubmission.name,
+        func.count(FormSubmission.id).label('count')
+    )
+    submissions = submissions.filter(FormSubmission.state == 'complete')
+    submissions = submissions.group_by(FormSubmission.name).subquery()
+
+    definitions = session.query(FormDefinition, submissions.c.count)
+    definitions = definitions.outerjoin(
+        submissions, submissions.c.name == FormDefinition.name
+    )
+    definitions = definitions.order_by(FormDefinition.name)
+
+    for form, submissions_count in definitions.all():
+        form.submissions_count = submissions_count or 0
+        yield form
+
 
 @TownApp.html(model=FormCollection, template='forms.pt', permission=Public)
 def view_get_form_collection(self, request):
-    forms = self.definitions.query().order_by(FormDefinition.name).all()
+
+    if request.is_logged_in:
+        forms = get_definitions_with_submission_count(request.app.session())
+    else:
+        forms = self.definitions.query().order_by(FormDefinition.name).all()
 
     return {
         'layout': FormCollectionLayout(self, request),
