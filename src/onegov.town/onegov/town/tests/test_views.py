1ffa500b49d37d6cce07134b2c905b9e8514063d
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-06-16 16:56:45 +0200

    Move the explicit view permission test to onegov.testing

--- src/onegov.town/onegov/town/tests/test_views.py
+++ src/onegov.town/onegov/town/tests/test_views.py
@@ -3,30 +3,14 @@ import onegov.core
 import onegov.town
 import textwrap
 
-from mock import patch
-from morepath import setup
 from onegov.form import FormCollection
 from onegov.testing import utils
 from webtest import TestApp as Client
 from webtest import Upload
 
 
-@patch('morepath.directive.register_view')
-def test_view_permissions(register_view):
-    config = setup()
-    config.scan(onegov.core)
-    config.scan(onegov.town)
-    config.commit()
-
-    # make sure that all registered views have an explicit permission
-    for call in register_view.call_args_list:
-        view = call[0][2]
-        module = view.__venusian_callbacks__[None][0][1]
-        permission = call[0][5]
-
-        if module.startswith('onegov.town') and permission is None:
-            assert permission is not None, (
-                'view {}.{} has no permission'.format(module, view.__name__))
+def test_view_permissions():
+    utils.assert_explicit_permissions(onegov.town)
 
 
 def test_view_login(town_app):
