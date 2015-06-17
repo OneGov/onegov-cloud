046da7b468b344c75f31e2da4e38773cb89a02d8
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-06-17 12:16:27 +0200

    Adds rendering of votes by domain on the homepage

--- src/onegov.election_day/onegov/election_day/views/homepage.py
+++ src/onegov.election_day/onegov/election_day/views/homepage.py
@@ -1,4 +1,6 @@
 from onegov.core.security import Public
+from onegov.core.utils import groupbylist
+from onegov.ballot import VoteCollection
 from onegov.election_day import ElectionDayApp
 from onegov.election_day.layout import DefaultLayout
 from onegov.election_day.model import Principal
@@ -7,6 +9,15 @@ from onegov.election_day.model import Principal
 @ElectionDayApp.html(model=Principal, template='homepage.pt',
                      permission=Public)
 def view_principal(self, request):
+
+    votes = VoteCollection(request.app.session()).get_latest()
+
+    if votes:
+        votes_by_domain = groupbylist(votes, lambda v: v.domain)
+    else:
+        votes_by_domain = None
+
     return {
-        'layout': DefaultLayout(self, request)
+        'votes_by_domain': votes_by_domain,
+        'layout': DefaultLayout(self, request),
     }
