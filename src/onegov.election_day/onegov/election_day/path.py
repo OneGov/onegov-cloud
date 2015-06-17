d3089828cb10103523e19fbfbb789d6f379dcee9
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-06-17 18:16:57 +0200

    Adds linking for vote objects

--- src/onegov.election_day/onegov/election_day/path.py
+++ src/onegov.election_day/onegov/election_day/path.py
@@ -1,3 +1,4 @@
+from onegov.ballot import Vote, VoteCollection
 from onegov.election_day import ElectionDayApp
 from onegov.election_day.model import Principal
 
@@ -5,3 +6,8 @@ from onegov.election_day.model import Principal
 @ElectionDayApp.path(model=Principal, path='/')
 def get_principal(app):
     return app.principal
+
+
+@ElectionDayApp.path(model=Vote, path='/abstimmung/{id}')
+def get_vote(app, id):
+    return VoteCollection(app.session()).by_id(id)
