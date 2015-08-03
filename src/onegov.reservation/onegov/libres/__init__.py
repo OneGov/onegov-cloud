54a3f41bdf995f7dfb714cdc3efb218164c02b3f
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-03 15:50:57 +0200

    Export Resource class on the top level

--- src/onegov.reservation/onegov/libres/__init__.py
+++ src/onegov.reservation/onegov/libres/__init__.py
@@ -1,4 +1,5 @@
 from onegov.libres.collection import ResourceCollection
 from onegov.libres.core import LibresIntegration
+from onegov.libres.models import Resource
 
-__all__ = ['LibresIntegration', 'ResourceCollection']
+__all__ = ['LibresIntegration', 'Resource', 'ResourceCollection']
