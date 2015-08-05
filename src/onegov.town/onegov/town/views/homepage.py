e4e2b5163e595392fc542f02e86476ee263721f4
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-05 12:51:48 +0200

    Adds a link to the daypasses and the resources to the homepage

--- src/onegov.town/onegov/town/views/homepage.py
+++ src/onegov.town/onegov/town/views/homepage.py
@@ -5,6 +5,7 @@
 from collections import namedtuple
 from onegov.core.security import Public
 from onegov.form import FormCollection
+from onegov.libres import ResourceCollection
 from onegov.people import PersonCollection
 from onegov.town import _
 from onegov.town.app import TownApp
@@ -18,6 +19,7 @@ def view_town(self, request):
     """ Renders the town's homepage. """
 
     session = request.app.session()
+    libres_context = request.app.libres_context
     layout = DefaultLayout(self, request)
     Tile = namedtuple('Tile', ['page', 'links', 'number'])
 
@@ -35,25 +37,33 @@ def view_town(self, request):
     # the panels on the homepage are currently mostly place-holders, real
     # links as well as translatable text is added every time we implement
     # a new feature that we need to link up on the homepage
-    online_counter = LinkGroup(
-        title=_("Services"),
-        links=[
-            Link(
-                text=_("Online Counter"),
-                url=request.link(FormCollection(session)),
-                subtitle=_("Forms and applications")
-            ),
-            Link(
-                text=u"Raumreservationen",
-                url="#",
-                subtitle=u"Turnhalle und Gesellschaftsräume"
-            ),
+    online_counter_links = [
+        Link(
+            text=_("Online Counter"),
+            url=request.link(FormCollection(session)),
+            subtitle=_("Forms and applications")
+        ),
+        Link(
+            text=u"Raumreservationen",
+            url=request.link(ResourceCollection(libres_context)),
+            subtitle=u"Turnhalle und Gesellschaftsräume"
+        ),
+    ]
+
+    daypasses = ResourceCollection(libres_context).by_name('ga-tageskarte')
+
+    if daypasses:
+        online_counter_links.append(
             Link(
                 text=u"GA-Tageskarte",
-                url="#",
+                url=request.link(daypasses),
                 subtitle=u"Günstige Tageskarten von der Gemeinde"
             )
-        ]
+        )
+
+    online_counter = LinkGroup(
+        title=_("Services"),
+        links=online_counter_links
     )
 
     latest_events = LinkGroup(
