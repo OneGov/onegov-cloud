cd8d739356d6772e9c4714e76a57a5db684e8d33
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-05 15:17:57 +0200

    Adds different views for different resources

--- src/onegov.town/onegov/town/assets/js/fullcalendar_custom.js
+++ src/onegov.town/onegov/town/assets/js/fullcalendar_custom.js
@@ -1,6 +1,14 @@
 var setup_calendar = function(calendar) {
     calendar.fullCalendar({
-        events: calendar.data('feed')
+        events: calendar.data('feed'),
+        header: {
+            left: calendar.data('header-left'),
+            center: calendar.data('header-center'),
+            right: calendar.data('header-right')
+        },
+        allDaySlot: false,
+        minTime: calendar.data('min-time'),
+        maxTime: calendar.data('max-time')
     });
 };
 
