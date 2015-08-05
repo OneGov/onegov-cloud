4890b1f33bba2fc7463eda58787ecc4843e5da0d
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-05 15:29:55 +0200

    Removes first/last hour setting.

--- src/onegov.town/onegov/town/forms/resource.py
+++ src/onegov.town/onegov/town/forms/resource.py
@@ -3,8 +3,6 @@ from onegov.form import Form, with_options
 from onegov.town import _
 from onegov.town.utils import mark_images
 from wtforms import StringField, TextAreaField, validators
-from wtforms.fields.html5 import IntegerField
-from wtforms.validators import InputRequired, NumberRange
 from wtforms.widgets import TextArea
 
 
@@ -22,18 +20,6 @@ class ResourceForm(Form):
         widget=with_options(TextArea, class_='editor'),
         filters=[sanitize_html, mark_images])
 
-    first_hour = IntegerField(
-        label=_("First hour shown in calendar"),
-        validators=[InputRequired(), NumberRange(0, 24)],
-        default=7
-    )
-
-    last_hour = IntegerField(
-        label=_("First hour shown in calendar"),
-        validators=[InputRequired(), NumberRange(0, 24)],
-        default=18
-    )
-
     def update_model(self, model):
         """ Stores the form values on the page. """
         model.title = self.title.data
@@ -43,13 +29,9 @@ class ResourceForm(Form):
         model.content = {
             'text': self.text.data
         }
-        model.first_hour = self.first_hour.data
-        model.last_hour = self.last_hour.data
 
     def apply_model(self, model):
         """ Stores the page values on the form. """
         self.title.data = model.title
         self.lead.data = model.meta.get('lead', '')
         self.text.data = model.content.get('text', '')
-        self.first_hour.data = model.first_hour
-        self.last_hour.data = model.last_hour
