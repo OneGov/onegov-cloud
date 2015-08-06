a23a1695b486f7ace84ae7716639a2386b66a5e7
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-06 13:00:16 +0200

    Adds a test for url_safe_token

---
+++ src/onegov.core/onegov/core/tests/test_request.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from freezegun import freeze_time
+from onegov.core.utils import Bunch
+from onegov.core.request import CoreRequest
+
+
+def test_url_safe_token():
+
+    request = CoreRequest(environ={
+        'PATH_INFO': '/',
+        'SERVER_NAME': '',
+        'SERVER_PORT': '',
+        'SERVER_PROTOCOL': 'https'
+    }, app=Bunch(identity_secret='asdf', lookup=None))
+
+    token = request.new_url_safe_token({'foo': 'bar'})
+
+    assert request.load_url_safe_token(token) == {'foo': 'bar'}
+    assert request.load_url_safe_token(token, salt='x') is None
+
+    with freeze_time(datetime.now() + timedelta(seconds=1)):
+        assert request.load_url_safe_token(token, max_age=1) is None
