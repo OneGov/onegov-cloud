420a494f480552fe4a0a8b87c36c37388559552e
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2015-08-20 14:00:54 +0200

    Onegov Event Release 0.0.1

--- src/onegov.event/HISTORY.rst
+++ src/onegov.event/HISTORY.rst
@@ -3,5 +3,7 @@ Changelog
 
 Unreleased
 ~~~~~~~~~~
+0.0.1 (2015-08-20)
+~~~~~~~~~~~~~~~~~~~
 
 - Initial Release
--- src/onegov.event/setup.py
+++ src/onegov.event/setup.py
@@ -6,7 +6,7 @@ name = 'onegov.event'
 description = (
     'Calender of events for OneGov.'
 )
-version = '0.0.0'
+version = '0.0.1'
 
 
 def get_long_description():
