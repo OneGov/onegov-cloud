9e82ae2dc686a7553174fb9475386f87103ff1ba
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-20 13:39:24 +0200

    Adds a test for transaction integration

--- src/onegov.reservation/onegov/libres/tests/test_session.py
+++ src/onegov.reservation/onegov/libres/tests/test_session.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from libres import new_scheduler
 from morepath import setup
 from onegov.core.framework import Framework
-from onegov.libres import LibresIntegration
+from onegov.libres import LibresIntegration, ResourceCollection
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from webtest import TestApp as Client
@@ -102,3 +102,54 @@ def test_libres_context(postgres_dsn):
 
     assert app.session_manager is app.libres_context.get_service(
         'session_provider')
+
+
+def test_transaction_integration(postgres_dsn):
+    Base = declarative_base()
+    config = setup()
+
+    class App(Framework, LibresIntegration):
+        testing_config = config
+
+    class Document(Base):
+        __tablename__ = 'documents'
+        id = Column(Integer, primary_key=True)
+
+    @App.path(path='/')
+    class Root(object):
+        pass
+
+    @App.json(model=Root)
+    def handle_root(self, request):
+        collection = ResourceCollection(request.app.libres_context)
+
+        resource = collection.add('Test', 'Europe/Zurich')
+        scheduler = resource.get_scheduler(request.app.libres_context)
+        scheduler.allocate(
+            (datetime(2015, 7, 30, 11), datetime(2015, 7, 30, 12))
+        )
+
+        # this will fail and then abort everything
+        request.app.session().add(Document(id=1))
+        request.app.session().add(Document(id=1))
+
+    # this is required for the transactions to actually work, usually this
+    # would be onegov.server's job
+    import more.transaction
+    config.scan(more.transaction)
+
+    config.commit()
+
+    app = App()
+    app.configure_application(dsn=postgres_dsn, base=Base)
+    app.namespace = 'libres'
+    app.set_application_id('libres/foo')
+
+    c = Client(app)
+    try:
+        c.get('/', expect_errors=True)
+    except:
+        pass
+
+    collection = ResourceCollection(app.libres_context)
+    assert collection.query().count() == 0
