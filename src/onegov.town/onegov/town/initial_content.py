defbab7b8d95c938df679a2709195dcd1dd5f47c
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-26 13:11:27 +0200

    Use simpler email check call from the latest onegov.form release

--- src/onegov.town/onegov/town/initial_content.py
+++ src/onegov.town/onegov/town/initial_content.py
@@ -10,8 +10,6 @@ from onegov.form import FormCollection
 from onegov.libres import LibresIntegration, ResourceCollection
 from onegov.page import PageCollection
 from onegov.town.models import Town
-from sedate import as_datetime
-from wtforms.fields.html5 import EmailField
 
 
 def add_initial_content(libres_registry, session_manager, town_name,
@@ -103,11 +101,9 @@ def add_builtin_forms(session, definitions=None):
                 type='builtin'
             )
 
-        assert form.form_class().match_fields(
-            include_classes=(EmailField, ),
-            required=True,
-            limit=1
-        ), "Each form must have at least one required email field"
+        assert form.form_class().has_required_email_field, (
+            "Each form must have at least one required email field"
+        )
 
 
 def builtin_form_definitions(path=None):
