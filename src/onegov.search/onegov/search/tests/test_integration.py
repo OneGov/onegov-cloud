e123fc12b96702381074fa0e49199f7868d948cf
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-09-08 16:07:26 +0200

    Make sure the testing es cluster is used

--- src/onegov.search/onegov/search/tests/test_integration.py
+++ src/onegov.search/onegov/search/tests/test_integration.py
@@ -8,6 +8,11 @@ def test_app_integration(es_url):
         pass
 
     app = App()
-    app.configure_application(elasticesarch_hosts=[es_url])
+    app.configure_application(elasticsearch_hosts=[es_url])
 
     assert app.es_client.ping()
+
+    # make sure we got the testing host
+    assert len(app.es_client.transport.hosts) == 1
+    assert app.es_client.transport.hosts[0]['port'] \
+        == int(es_url.split(':')[-1])
