0de00dbc96cd443faa7571dcc65e5fcb9329a345
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-09-08 11:08:36 +0200

    Onegov Search Initial commit

---
+++ src/onegov.search/README.rst
@@ -0,0 +1,49 @@
+Onegov Search
+=============
+
+Elasticsearch integration for OneGov Cloud
+
+Run the Tests
+-------------
+    
+Install tox and run it::
+
+    pip install tox
+    tox
+
+Limit the tests to a specific python version::
+
+    tox -e py27
+
+Conventions
+-----------
+
+Onegov Search follows PEP8 as close as possible. To test for it run::
+
+    tox -e pep8
+
+Onegov Search uses `Semantic Versioning <http://semver.org/>`_
+
+Build Status
+------------
+
+.. image:: https://travis-ci.org/OneGov/onegov.search.png
+  :target: https://travis-ci.org/OneGov/onegov.search
+  :alt: Build Status
+
+Coverage
+--------
+
+.. image:: https://coveralls.io/repos/OneGov/onegov.search/badge.png?branch=master
+  :target: https://coveralls.io/r/OneGov/onegov.search?branch=master
+  :alt: Project Coverage
+
+Latests PyPI Release
+--------------------
+.. image:: https://pypip.in/v/onegov.search/badge.png
+  :target: https://crate.io/packages/onegov.search
+  :alt: Latest PyPI Release
+
+License
+-------
+onegov.search is released under GPLv2
---
+++ src/onegov.search/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+name = 'onegov.search'
+description = (
+    'Elasticsearch integration for OneGov Cloud'
+)
+version = '0.0.0'
+
+
+def get_long_description():
+    readme = open('README.rst').read()
+    history = open('HISTORY.rst').read()
+
+    # cut the part before the description to avoid repetition on pypi
+    readme = readme[readme.index(description) + len(description):]
+
+    return '\n'.join((readme, history))
+
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=get_long_description(),
+    url='http://github.com/OneGov/onegov.search',
+    author='Seantis GmbH',
+    author_email='info@seantis.ch',
+    license='GPLv2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=name.split('.')[:-1],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'onegov.core>=0.4.0'
+    ],
+    extras_require=dict(
+        test=[
+            'coverage',
+            'onegov.testing',
+            'pytest',
+        ],
+    ),
+    entry_points={
+        'onegov': [
+            'upgrade = onegov.search.upgrade'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    ]
+)
