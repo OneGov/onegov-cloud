678af8a34ce16192683b9cbf44baa7e9096ddf67
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-10-08 15:05:57 +0200

    Fix upload fields not working correctly

--- src/onegov.election_day/onegov/election_day/forms/upload.py
+++ src/onegov.election_day/onegov/election_day/forms/upload.py
@@ -1,8 +1,9 @@
 from onegov.election_day import _
 from onegov.form import Form, with_options
+from onegov.form.fields import UploadField
 from onegov.form.parser.core import FieldDependency
 from onegov.form.widgets import UploadWidget
-from wtforms import RadioField, FileField
+from wtforms import RadioField
 from wtforms.validators import DataRequired, InputRequired
 from wtforms_components import If
 
@@ -12,7 +13,7 @@ class UploadForm(Form):
     type = RadioField(_("Type"), choices=[
         ('simple', _("Simple Vote")),
         ('complex', _("Vote with Counter-Proposal")),
-    ], validators=[InputRequired()])
+    ], validators=[InputRequired()], default='simple')
 
     # XXX make this easier with onegov.form
     complex_vote_depencdency = FieldDependency('type', 'complex')
@@ -22,19 +23,26 @@ class UploadForm(Form):
     )]
     complex_vote_validators[0].field_flags = ('required', )
 
-    proposal = FileField(
+    proposal = UploadField(
         label=_("Proposal"),
-        validators=[DataRequired()]
+        validators=[DataRequired()],
+        widget=with_options(UploadWidget, force_simple=True)
     )
 
-    counter_proposal = FileField(
+    counter_proposal = UploadField(
         label=_("Counter-Proposal"),
         validators=complex_vote_validators,
-        widget=with_options(UploadWidget, **complex_vote_depencdency.html_data)
+        widget=with_options(
+            UploadWidget, force_simple=True,
+            **complex_vote_depencdency.html_data
+        )
     )
 
-    tie_breaker = FileField(
+    tie_breaker = UploadField(
         label=_("Tie-Breaker"),
         validators=complex_vote_validators,
-        widget=with_options(UploadWidget, **complex_vote_depencdency.html_data)
+        widget=with_options(
+            UploadWidget, force_simple=True,
+            **complex_vote_depencdency.html_data
+        )
     )
