f47bb5d4b23b76914df99370e7d7df2fb3334d58
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-10-09 11:44:28 +0200

    Adds a cancel button to the upload view

--- src/onegov.election_day/onegov/election_day/views/upload.py
+++ src/onegov.election_day/onegov/election_day/views/upload.py
@@ -6,6 +6,7 @@ from onegov.election_day import _
 from onegov.election_day import ElectionDayApp
 from onegov.election_day.forms import UploadForm
 from onegov.election_day.layout import ManageLayout
+from onegov.election_day.models import Manage
 
 
 @ElectionDayApp.form(model=Vote, name='upload', template='upload.pt',
@@ -19,5 +20,6 @@ def view_upload(self, request, form):
         'layout': ManageLayout(self, request),
         'title': self.title,
         'subtitle': _("Upload results"),
-        'form': form
+        'form': form,
+        'cancel': request.link(Manage(request.app.session()))
     }
