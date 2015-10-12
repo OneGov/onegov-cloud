bdaad63098a8ebfde0794383e652f9c0bf2ab7bd
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-10-12 13:31:53 +0200

    Onegov Foundation Release 0.1.0

--- src/onegov.foundation/setup.py
+++ src/onegov.foundation/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.foundation'
 description = (
     'Provides a base for Zurb Foundation themes used in OneGov.'
 )
-version = '0.0.4'
+version = '0.1.0'
 
 
 def get_long_description():
