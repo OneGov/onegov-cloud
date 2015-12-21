a0a035e33fac630dfa0dd615123a81899986ac29
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-12-21 09:14:19 +0100

    Remove functions which have been moved to onegov.core

--- src/onegov_testing/onegov/testing/utils.py
+++ src/onegov_testing/onegov/testing/utils.py
@@ -1,7 +1,5 @@
-import morepath
 import onegov.core
 
-from importlib import import_module
 from io import BytesIO
 from morepath import setup
 from PIL import Image
@@ -40,37 +38,3 @@ def assert_explicit_permissions(module):
                 assert permission is not None, (
                     'view {}.{} has no permission'.format(
                         module, view.__name__))
-
-
-def morepath_modules(cls):
-    """ Returns all morepath modules which should be scanned for the given
-    morepath application class.
-
-    We can't reliably know the actual morepath modules that
-    need to be scanned, which is why we assume that each module has
-    one namespace (like 'more.transaction').
-
-    """
-    for base in cls.__mro__:
-        if not issubclass(base, morepath.App):
-            continue
-
-        if base is morepath.App:
-            continue
-
-        module = '.'.join(base.__module__.split('.')[:2])
-
-        if module.startswith('test'):
-            continue
-
-        yield module
-
-
-def scan_morepath_modules(cls, config):
-    """ Tries to scann all the morepath modules required for the given
-    application class. This is not guaranteed to stay reliable as there is
-    no sure way to discover all modules required by the application class.
-
-    """
-    for module in sorted(morepath_modules(cls)):
-        config.scan(import_module(module))
