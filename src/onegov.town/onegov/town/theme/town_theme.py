ebe3f453dfa6ecb0a3b4c0436e741b0bc0d8cc33
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-01-04 15:14:58 +0100

    Onegov Town Release 1.2.1

--- src/onegov.town/onegov/town/theme/town_theme.py
+++ src/onegov.town/onegov/town/theme/town_theme.py
@@ -17,7 +17,7 @@ class TownTheme(BaseTheme):
 
     # don't touch this number, it's incremented using bumpversion, so every
     # release will automatically trigger a rebuild of the theme
-    version = '1.2.0'
+    version = '1.2.1'
 
     @property
     def default_options(self):
