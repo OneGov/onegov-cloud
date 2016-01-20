f546b531ef805fae9bdadacf6be8b6e04dc95a24
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-01-20 17:43:55 +0100

    Onegov Core Release 0.14.0

--- src/onegov.core/HISTORY.rst
+++ src/onegov.core/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.14.0 (2016-01-20)
+~~~~~~~~~~~~~~~~~~~
 
 - Caches the result of po file compiles.
   [href]
--- src/onegov.core/setup.py
+++ src/onegov.core/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.core'
 description = (
     'Contains code shared by all OneGov applications.'
 )
-version = '0.13.4'
+version = '0.14.0'
 
 
 def get_long_description():
