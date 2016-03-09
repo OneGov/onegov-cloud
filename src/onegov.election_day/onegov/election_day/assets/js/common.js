ef2cbf00e5cac62de34759ca125a9ea0e43c8977
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2016-03-09 14:29:47 +0100

    Makes election details collapsible #34

--- src/onegov.election_day/onegov/election_day/assets/js/common.js
+++ src/onegov.election_day/onegov/election_day/assets/js/common.js
@@ -4,6 +4,13 @@ $(document).foundation();
 // stack tables wishing to be stacked
 $('.stackable').stacktable();
 
+// collapse tables whising to be collapsible
+$('.collapsible.collapsed tbody tr:not(.total):not(.sticky)').hide();
+$('.collapsible thead').click(function() {
+    $(this).parent().toggleClass('collapsed');
+    $(this).parent().children('tbody').children('tr:not(.total):not(.sticky)').toggle();
+});
+
 // force all dropdowns to be rendered in the direction specified in the
 // options of said dropdown (so if we say align:left, *always* align left)
 Foundation.libs.dropdown.small = function() { return false; };
