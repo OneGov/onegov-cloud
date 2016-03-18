630777bc3a6e038e41c2092ac965828dd6f4ea9e
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-03-18 11:13:45 +0100

    Proof of concept for youtube

--- src/onegov.town/onegov/town/assets/js/videoframe.js
+++ src/onegov.town/onegov/town/assets/js/videoframe.js
@@ -27,11 +27,23 @@ var getVideoId = function(url) {
 
 var getVideoThumbnail = function(host, id) {
     if (host.match(/youtube\.com/gi)) {
-        return "https://img.youtube.com/vi/" + id + "/hqdefault.jpg";
+        return "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
     }
 
     if (host.match(/vimeo\.com/gi)) {
-        return "https://i.vimecodn.com/video/" + id + "_640jpg";
+        return "https://i.vimecodn.com/video/" + id + "_1280jpg";
+    }
+
+    return null;
+};
+
+var getVideoUrl = function(host, id) {
+    if (host.match(/youtube\.com/gi)) {
+        return "https://www.youtube.com/embed/" + id + '?autoplay=1';
+    }
+
+    if (host.match(/vimeo\.com/gi)) {
+        return "https://player.vimeo.com/video/" + id;
     }
 
     return null;
@@ -42,7 +54,7 @@ var getVideoInfo = function(link) {
 
     if (info) {
         info.thumbnail = getVideoThumbnail(info.host, info.id);
-        info.url = link.attr('href').replace('http://', 'https://');
+        info.url = getVideoUrl(info.host, info.id);
         info.width = 640;
         info.height = 360;
 
@@ -56,20 +68,34 @@ var VideoFrame = function(link) {
     var info = getVideoInfo(link);
 
     var iframe = $('<iframe class="video-frame" src="' + info.url + '">')
-        .attr('autoplay', '1')
         .attr('frameborder', '0')
         .attr('allowfullscreen', true)
         .attr('width', info.width)
         .attr('height', info.height);
 
-    var thumb = $('<img class="video-thumbnail" src="' + info.thumbnail + '">')
-        .append('<div class="video-play />');
+    var thumb = $('<img class="video-thumbnail" />')
+        .on('error', function() {
+            $(this).removeAttr('src');
+        })
+        .on('load', function() {
+            $(this).parent().css('height', $(this).height());
+        });
+
+    var button = $('<i class="video-button fa fa-play"></i>');
+
+    button.click(function() {
+        button.remove();
+        thumb.replaceWith(iframe);
+    });
 
     thumb.click(function() {
+        button.remove();
         thumb.replaceWith(iframe);
     });
 
     link.replaceWith(thumb);
+    thumb.after(button);
+    thumb.attr('src', info.thumbnail);
 };
 
 jQuery.fn.videoframe = function() {
