1e03b4c70573da49d27832cbf8f2882e91282f64
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-03-24 16:04:09 +0100

    Simplify settings form handling

--- src/onegov.town/onegov/town/models/town.py
+++ src/onegov.town/onegov/town/models/town.py
@@ -1,8 +1,9 @@
 """ Contains the model describing the town proper. """
 
 from onegov.core.orm import Base
-from onegov.core.orm.mixins import TimestampMixin
+from onegov.core.orm.mixins import meta_property, TimestampMixin
 from onegov.core.orm.types import JSON, UUID
+from onegov.core.utils import linkify
 from onegov.town.theme import user_options
 from sqlalchemy import Column, Text
 from uuid import uuid4
@@ -31,3 +32,26 @@ class Town(Base, TimestampMixin):
 
     #: additional data associated with the town
     meta = Column(JSON, nullable=True, default=dict)
+
+    # meta bound values
+    contact = meta_property('contact')
+    contact_url = meta_property('contact_url')
+    opening_hours = meta_property('opening_hours')
+    opening_hours_url = meta_property('opening_hours_url')
+    reply_to = meta_property('reply_to')
+    facebook_url = meta_property('facebook_url')
+    twitter_url = meta_property('twitter_url')
+    analytics_code = meta_property('analytics_code')
+    online_counter_label = meta_property('online_counter_label')
+    reservations_label = meta_property('reservations_label')
+    daypass_label = meta_property('daypass_label')
+
+    @contact.setter
+    def contact(self, value):
+        self.meta['contact'] = value
+        self.meta['contact_html'] = linkify(value).replace('\n', '<br>')
+
+    @opening_hours.setter
+    def opening_hours(self, value):
+        self.meta['opening_hours'] = value
+        self.meta['opening_hours_html'] = linkify(value).replace('\n', '<br>')
--- src/onegov.town/onegov/town/views/settings.py
+++ src/onegov.town/onegov/town/views/settings.py
@@ -1,7 +1,6 @@
 """ The settings view, defining things like the logo or color of the town. """
 
 from onegov.core.security import Secret
-from onegov.core.utils import linkify
 from onegov.town import _
 from onegov.town.app import TownApp
 from onegov.town.elements import Link
@@ -23,43 +22,13 @@ def handle_settings(self, request, form):
 
     if form.submitted(request):
         with request.app.update_town() as town:
-            town.name = form.name.data
-            town.logo_url = form.logo_url.data
+            form.populate_obj(town, exclude={'primary_color', 'footer-height'})
             town.theme_options = form.theme_options
-            town.meta = {
-                'contact': form.contact.data,
-                'contact_html': linkify(
-                    form.contact.data).replace('\n', '<br>'),
-                'contact_url': form.contact_url.data,
-                'opening_hours': form.opening_hours.data,
-                'opening_hours_html': linkify(
-                    form.opening_hours.data).replace('\n', '<br>'),
-                'opening_hours_url': form.opening_hours_url.data,
-                'reply_to': form.reply_to.data,
-                'facebook_url': form.facebook_url.data,
-                'twitter_url': form.twitter_url.data,
-                'analytics_code': form.analytics_code.data,
-                'online_counter_label': form.online_counter_label.data,
-                'reservations_label': form.reservations_label.data,
-                'sbb_daypass_label': form.sbb_daypass_label.data,
-            }
 
         request.success(_("Your changes were saved"))
     elif request.method == 'GET':
-        form.name.data = self.name
-        form.logo_url.data = self.logo_url
+        form.process(obj=self)
         form.theme_options = self.theme_options
-        form.contact.data = self.meta.get('contact')
-        form.contact_url.data = self.meta.get('contact_url')
-        form.opening_hours.data = self.meta.get('opening_hours')
-        form.opening_hours_url.data = self.meta.get('opening_hours_url')
-        form.reply_to.data = self.meta.get('reply_to')
-        form.facebook_url.data = self.meta.get('facebook_url')
-        form.twitter_url.data = self.meta.get('twitter_url')
-        form.analytics_code.data = self.meta.get('analytics_code')
-        form.online_counter_label.data = self.meta.get('online_counter_label')
-        form.reservations_label.data = self.meta.get('reservations_label')
-        form.sbb_daypass_label.data = self.meta.get('sbb_daypass_label')
 
     layout.breadcrumbs = [
         Link(_("Homepage"), layout.homepage_url),
