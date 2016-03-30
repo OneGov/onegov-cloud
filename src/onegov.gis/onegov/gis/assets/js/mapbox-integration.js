86c2559f99ea875bfaab64c9b477b166075c7a53
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-03-30 11:58:54 +0200

    Implement crosshair input map

--- src/onegov.gis/onegov/gis/assets/js/mapbox-integration.js
+++ src/onegov.gis/onegov/gis/assets/js/mapbox-integration.js
@@ -1,17 +1,28 @@
-function utf8_to_b64(str) {
-    return window.btoa(escape(encodeURIComponent(str)));
-}
-
-function b64_to_utf8(str) {
-    return decodeURIComponent(unescape(window.atob(str)));
-}
-
 function getCoordinates(input) {
-    return JSON.parse(b64_to_utf8(input.val()));
+    return JSON.parse(window.atob(input.attr('value')));
 }
 
 function setCoordinates(input, coordinates) {
-    return input.val(utf8_to_b64(JSON.stringify(coordinates)));
+    return input.attr('value', window.btoa(JSON.stringify(coordinates)));
+}
+
+function mapToCoordinates(position, zoom) {
+    return {
+        'lat': position.lat,
+        'lon': position.lng,
+        'zoom': zoom
+    };
+}
+
+function asCrosshairMap(map, input) {
+    // a crosshair map updates the input coordinates with the center of
+    // the map as well as the zoom, whenever either changes
+
+    map.on('move', function(e) {
+        setCoordinates(input, mapToCoordinates(
+            e.target.getCenter(), e.target.getZoom()
+        ));
+    });
 }
 
 var MapboxInput = function(input) {
@@ -25,8 +36,10 @@ var MapboxInput = function(input) {
     var zoom = coordinates.zoom || 5;
 
     var el = $('<div class="map-wrapper">')
-        .css('min-height', '200px')
         .insertBefore(input.hide());
+
+    // the height depends on the width using the golden ratio
+    el.css('height', input.data('map-height') || $(el).width() / 1.618 + 'px');
 
     var map = new mapboxgl.Map({
         container: el[0],
@@ -34,6 +47,21 @@ var MapboxInput = function(input) {
         center: [lon, lat],
         zoom: zoom
     });
+
+    // hide the improve map link, it's not translated, looks ugly and doesn't
+    // *have* to be included (unlike the copyright labels)
+    map.on('load', function(e) {
+        $(e.target._container).find('.mapbox-improve-map').remove();
+    });
+
+    switch (input.data('map-type')) {
+        case 'crosshair':
+            asCrosshairMap(map, input);
+            break;
+
+        default:
+            break;
+    }
 };
 
 jQuery.fn.mapboxInput = function() {
