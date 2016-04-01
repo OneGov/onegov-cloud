d11ae05a86664e4865bb92c09b0c00617864142e
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-04-01 12:55:47 +0200

    Pave the way for morepath 0.13

--- src/onegov_testing/onegov/testing/utils.py
+++ src/onegov_testing/onegov/testing/utils.py
@@ -1,7 +1,6 @@
 import onegov.core
 
 from io import BytesIO
-from morepath import setup
 from PIL import Image
 from unittest.mock import patch
 
@@ -18,6 +17,37 @@ def create_image():
 
 
 def assert_explicit_permissions(module):
+
+    # support morepath 0.12 until 0.13 is out
+    try:
+        return assert_explicit_permissions_old(module)
+    except ImportError:
+        return assert_explicit_permissions_new(module)
+
+
+def assert_explicit_permissions_new(module):
+    with patch('morepath.view.ViewRegistry.register_view') as register_view:
+
+        import morepath
+
+        morepath.scan(onegov.core)
+        morepath.scan(module)
+        morepath.autocommit()
+
+        # make sure that all registered views have an explicit permission
+        for call in register_view.call_args_list:
+            view = call[0][1]
+            permission = call[0][4]
+
+            if view.__module__.startswith('onegov'):
+                assert permission is not None, (
+                    'view {}.{} has no permission'.format(
+                        module, view.__name__))
+
+
+def assert_explicit_permissions_old(module):
+
+    from morepath import setup
 
     with patch('morepath.directive.register_view') as register_view:
 
