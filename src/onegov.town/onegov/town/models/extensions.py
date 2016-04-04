ea76015e3cad47379d7c9efff50eb034e99bfca2
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-04-04 14:48:48 +0200

    Set the default town view on all coordinate inputs

--- src/onegov.town/onegov/town/models/extensions.py
+++ src/onegov.town/onegov/town/models/extensions.py
@@ -94,13 +94,19 @@ class CoordinatesExtension(ContentExtension):
 
     def extend_form(self, form_class, request):
 
+        render_kw = {'data-map-type': 'marker'}
+        default = request.app.town.default_map_view
+
+        if default:
+            render_kw['data-map-default-lat'] = default['lat']
+            render_kw['data-map-default-lon'] = default['lon']
+            render_kw['data-map-default-zoom'] = default['zoom']
+
         class CoordinatesForm(form_class):
             coordinates = CoordinatesField(
                 label=_("Coordinates"),
                 fieldset=_("Map"),
-                render_kw={
-                    'data-map-type': 'marker'
-                }
+                render_kw=render_kw
             )
 
         return CoordinatesForm
