01aad1208c5bc5a6cd6622fb726336cd12d434ee
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-07-06 14:54:36 +0200

    Adds a test ensuring proper separation between applications

--- src/onegov.file/onegov/file/tests/test_integration.py
+++ src/onegov.file/onegov/file/tests/test_integration.py
@@ -1,13 +1,16 @@
 import morepath
+import pytest
 import transaction
 
+from depot.manager import DepotManager
 from onegov.core import Framework
 from onegov.core.utils import scan_morepath_modules
 from onegov.file import DepotApp, FileCollection
 from webtest import TestApp as Client
 
 
-def test_serve_file(postgres_dsn, temporary_path):
+@pytest.fixture(scope='function')
+def app(postgres_dsn, temporary_path):
 
     class App(Framework, DepotApp):
         pass
@@ -25,11 +28,17 @@ def test_serve_file(postgres_dsn, temporary_path):
     app.namespace = 'apps'
     app.set_application_id('apps/my-app')
 
-    client = Client(app)
+    return app
 
+
+def ensure_correct_depot(app):
     # this will activate the correct depot storage - only required in these
     # tets because we are not storing the file *during* a request
-    client.get('/', expect_errors=True)
+    Client(app).get('/', expect_errors=True)
+
+
+def test_serve_file(app):
+    ensure_correct_depot(app)
 
     files = FileCollection(app.session())
     file_id = files.add('readme.txt', b'README').id
@@ -42,3 +51,38 @@ def test_serve_file(postgres_dsn, temporary_path):
     assert result.content_type == 'text/plain'
     assert result.content_length == 6
     assert 'filename=readme.txt' in result.content_disposition
+
+
+def test_application_separation(app):
+    app.set_application_id('apps/one')
+    ensure_correct_depot(app)
+
+    files = FileCollection(app.session())
+    first_id = files.add('readme.txt', b'README').id
+    transaction.commit()
+
+    app.set_application_id('apps/two')
+    ensure_correct_depot(app)
+
+    files = FileCollection(app.session())
+    second_id = files.add('readme.txt', b'README').id
+    transaction.commit()
+
+    assert len(DepotManager.get('apps-one').list()) == 1
+    assert len(DepotManager.get('apps-two').list()) == 1
+
+    client = Client(app)
+
+    app.set_application_id('apps/one')
+
+    assert client.get('/storage/{}'.format(first_id))\
+        .status_code == 200
+    assert client.get('/storage/{}'.format(second_id), expect_errors=True)\
+        .status_code == 404
+
+    app.set_application_id('apps/two')
+
+    assert client.get('/storage/{}'.format(first_id), expect_errors=True)\
+        .status_code == 404
+    assert client.get('/storage/{}'.format(second_id))\
+        .status_code == 200
