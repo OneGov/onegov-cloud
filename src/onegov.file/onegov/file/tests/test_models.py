16e2d30a210b677ee90f3d9f61d096ec90e8f64d
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-07-06 09:33:59 +0200

    Adds a test for fileset associations

--- src/onegov.file/onegov/file/tests/test_models.py
+++ src/onegov.file/onegov/file/tests/test_models.py
@@ -1,6 +1,7 @@
 import transaction
 
-from onegov.file import File
+from onegov.file import File, FileSet
+from onegov.file.models.fileset import file_to_set_associations
 
 
 def test_store_file_from_string(session):
@@ -31,3 +32,47 @@ def test_store_file_from_path(session, temporary_path):
     assert readme.reference.file.content_type == 'text/plain'
     assert readme.reference.file.read() == b'README\n======'
     assert readme.reference.file.name == 'readme.txt'
+
+
+def test_associate_files_with_sets(session):
+    session.add(File(name='readme.txt', reference=b'README'))
+    session.add(File(name='manual.txt', reference=b'MANUAL'))
+    session.add(FileSet(title='Textfiles'))
+    session.add(FileSet(title='Documents'))
+
+    transaction.commit()
+
+    readme, manual = session.query(File).order_by(File.name).all()
+    textfiles, documents = session.query(FileSet).order_by(FileSet.title).all()
+
+    assert session.query(file_to_set_associations).count() == 0
+    assert not readme.filesets
+    assert not manual.filesets
+    assert not textfiles.files
+    assert not documents.files
+
+    textfiles.files.append(readme)
+    textfiles.files.append(manual)
+    documents.files.append(readme)
+
+    transaction.commit()
+
+    readme, manual = session.query(File).order_by(File.name).all()
+    textfiles, documents = session.query(FileSet).order_by(FileSet.title).all()
+
+    assert session.query(file_to_set_associations).count() == 3
+    assert len(readme.filesets) == 2
+    assert len(manual.filesets) == 1
+    assert len(textfiles.files) == 2
+    assert len(documents.files) == 1
+
+    session.delete(manual)
+    transaction.commit()
+
+    readme = session.query(File).order_by(File.name).one()
+    textfiles, documents = session.query(FileSet).order_by(FileSet.title).all()
+
+    assert session.query(file_to_set_associations).count() == 2
+    assert len(readme.filesets) == 2
+    assert len(textfiles.files) == 1
+    assert len(documents.files) == 1
