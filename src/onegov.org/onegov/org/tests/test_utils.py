32f4bd40ed94e4906729506696cbbfa17d816996
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-08-22 14:00:19 +0200

    Move all utils from town to org

---
+++ src/onegov.org/onegov/org/tests/test_utils.py
@@ -0,0 +1,40 @@
+from onegov.org import utils
+
+
+def test_annotate_html():
+    html = "<p><img/></p><p></p>"
+    assert utils.annotate_html(html) == (
+        '<p class="has-img"><img class="lazyload-alt"></p><p></p>'
+    )
+
+    html = "<p class='x'><img/></p><p></p>"
+    assert utils.annotate_html(html) == (
+        '<p class="x has-img"><img class="lazyload-alt"></p><p></p>'
+    )
+
+    html = "<strong></strong>"
+    assert utils.annotate_html(html) == html
+
+    html = '<a href="http://www.seantis.ch"></a>'
+    assert utils.annotate_html(html) == html
+
+    html = 'no html'
+    assert utils.annotate_html(html) == 'no html'
+
+    html = '<p><a href="http://www.seantis.ch"></a></p>'
+    assert utils.annotate_html(html) == html
+
+    html = (
+        '<p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"></a></p>'
+    )
+    assert '<p class="has-video">' in utils.annotate_html(html)
+    assert 'class="has-video"></a>' in utils.annotate_html(html)
+
+    html = (
+        '<p>'
+        '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"></a>'
+        '<img />'
+        '</p>'
+    )
+    assert '<p class="has-img has-video">' in utils.annotate_html(html)
+    assert 'class="has-video"></a>' in utils.annotate_html(html)
