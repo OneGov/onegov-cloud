73e6ba204ba37f5a76ff9bba156b75e42bd9a5a1
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-08-22 11:53:38 +0200

    Setup logging and translations

--- src/onegov.org/onegov/org/__init__.py
+++ src/onegov.org/onegov/org/__init__.py
@@ -1,3 +1,10 @@
+import logging
+log = logging.getLogger('onegov.org')  # noqa
+log.addHandler(logging.NullHandler())  # noqa
+
+from translationstring import TranslationStringFactory  # noqa
+_ = TranslationStringFactory('onegov.org')  # noqa
+
 from onegov.org.app import OrgApp
 
 __all__ = ['OrgApp']
