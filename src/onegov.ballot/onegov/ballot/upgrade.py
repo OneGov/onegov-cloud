c6dc7a4047bc36a5ff40cd7554388305fb43f80a
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2016-09-16 08:57:23 +0200

    Add to last upgrade step and make it more robust

--- src/onegov.ballot/onegov/ballot/upgrade.py
+++ src/onegov.ballot/onegov/ballot/upgrade.py
@@ -6,6 +6,7 @@ from onegov.ballot import Vote
 from onegov.core.orm.types import HSTORE, JSON
 from onegov.core.upgrade import upgrade_task
 from sqlalchemy import Column, Enum, Integer, Text
+from sqlalchemy.engine.reflection import Inspector
 
 
 @upgrade_task('Rename yays to yeas', always_run=True)
@@ -80,6 +81,15 @@ def add_municipality_domain(context):
             context.operations.alter_column(table, old, new_column_name=new)
 
     # Add the new domain, see http://stackoverflow.com/a/14845740
+    table_names = []
+    inspector = Inspector(context.operations_connection)
+    if 'elections' in inspector.get_table_names(context.schema):
+        table_names.append('elections')
+    if 'votes' in inspector.get_table_names(context.schema):
+        table_names.append('votes')
+    if 'archived_results' in inspector.get_table_names(context.schema):
+        table_names.append('archived_results')
+
     old_type = Enum('federation', 'canton', name='domain_of_influence')
     new_type = Enum('federation', 'canton', 'municipality',
                     name='domain_of_influence')
@@ -87,25 +97,25 @@ def add_municipality_domain(context):
                     name='_domain_of_influence')
 
     tmp_type.create(context.operations.get_bind(), checkfirst=False)
-    context.operations.execute(
-        'ALTER TABLE votes ALTER COLUMN domain TYPE _domain_of_influence '
-        'USING domain::text::_domain_of_influence'
-    )
-    context.operations.execute(
-        'ALTER TABLE elections ALTER COLUMN domain TYPE _domain_of_influence '
-        'USING domain::text::_domain_of_influence'
-    )
+
+    for table_name in table_names:
+        context.operations.execute(
+            (
+                'ALTER TABLE {} ALTER COLUMN domain TYPE _domain_of_influence '
+                'USING domain::text::_domain_of_influence'
+            ).format(table_name)
+        )
 
     old_type.drop(context.operations.get_bind(), checkfirst=False)
 
     new_type.create(context.operations.get_bind(), checkfirst=False)
-    context.operations.execute(
-        'ALTER TABLE votes ALTER COLUMN domain TYPE domain_of_influence '
-        'USING domain::text::domain_of_influence'
-    )
-    context.operations.execute(
-        'ALTER TABLE elections ALTER COLUMN domain TYPE domain_of_influence '
-        'USING domain::text::domain_of_influence'
-    )
+
+    for table_name in table_names:
+        context.operations.execute(
+            (
+                'ALTER TABLE {} ALTER COLUMN domain TYPE domain_of_influence '
+                'USING domain::text::domain_of_influence'
+            ).format(table_name)
+        )
 
     tmp_type.drop(context.operations.get_bind(), checkfirst=False)
