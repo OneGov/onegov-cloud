5908213b36408eed3b761aa1c3eb5f7f3d2c52ad
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-09-20 16:33:56 +0200

    Adds a test for pagination

--- src/onegov.activity/onegov/activity/tests/test_collections.py
+++ src/onegov.activity/onegov/activity/tests/test_collections.py
@@ -30,3 +30,24 @@ def test_unique_activity(session, owner):
 
     collection.add("Möped Lads", username=owner.username)
     assert collection.get_unique_name("Möped Lads") == 'moeped-lads-2'
+
+
+def test_activity_pagination(session, owner):
+
+    collection = ActivityCollection(session)
+
+    for i in range(0, 20):
+        collection.add(
+            title='{:02d}'.format(i),
+            username=owner.username
+        )
+
+    collection = collection.page_by_index(0)
+    assert [a.title for a in collection.batch] == [
+        "00", "01", "02", "03", "04", "05", "06", "07", "08", "09"
+    ]
+
+    collection = collection.page_by_index(1)
+    assert [a.title for a in collection.batch] == [
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"
+    ]
