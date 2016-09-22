59d19f4bc5aeeef5d9ed3a76ba77ea378a198804
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-09-22 10:52:16 +0200

    Use the new onegov.testing create_app helper

--- src/onegov.town/onegov/town/tests/conftest.py
+++ src/onegov.town/onegov/town/tests/conftest.py
@@ -1,15 +1,14 @@
 import onegov.core
 import onegov.town
 import pytest
-import tempfile
 import transaction
-import shutil
 
-from onegov.core.utils import Bunch, module_path, scan_morepath_modules
-from onegov.town.initial_content import (
-    create_new_organisation, builtin_form_definitions)
+from onegov.core.utils import module_path
+from onegov.testing.utils import create_app
+from onegov.town import TownApp
+from onegov.town.initial_content import builtin_form_definitions
+from onegov.town.initial_content import create_new_organisation
 from onegov.user import User
-from uuid import uuid4
 
 
 @pytest.yield_fixture(scope='session')
@@ -19,72 +18,32 @@ def handlers():
 
 
 @pytest.yield_fixture(scope='session')
-def filestorage():
-    directory = tempfile.mkdtemp()
-    yield directory
-    shutil.rmtree(directory)
-
-
-@pytest.yield_fixture(scope='session')
 def forms():
     yield list(builtin_form_definitions(
         module_path('onegov.town', 'forms/builtin')))
 
 
 @pytest.yield_fixture(scope='function')
-def town_app(postgres_dsn, filestorage, test_password, smtp, forms):
-    yield new_town_app(postgres_dsn, filestorage, test_password, smtp, forms)
+def town_app(request):
+    yield create_town_app(request, use_elasticsearch=False)
 
 
 @pytest.yield_fixture(scope='function')
-def es_town_app(postgres_dsn, filestorage, test_password, smtp, forms, es_url):
-    yield new_town_app(
-        postgres_dsn, filestorage, test_password, smtp, forms, es_url)
+def es_town_app(request):
+    yield create_town_app(request, use_elasticsearch=True)
 
 
-def new_town_app(postgres_dsn, filestorage, test_password, smtp,
-                 forms, es_url=None):
-
-    scan_morepath_modules(onegov.town.TownApp)
-    onegov.town.TownApp.commit()
-
-    app = onegov.town.TownApp()
-    app.namespace = 'test_' + uuid4().hex
-    app.configure_application(
-        dsn=postgres_dsn,
-        filestorage='fs.osfs.OSFS',
-        filestorage_options={
-            'root_path': filestorage,
-            'create': True
-        },
-        depot_backend='depot.io.memory.MemoryFileStorage',
-        identity_secure=False,
-        disable_memcached=True,
-        enable_elasticsearch=es_url and True or False,
-        elasticsearch_hosts=[es_url]
-    )
-    app.set_application_id(app.namespace + '/' + 'test')
-    app.bind_depot()
-
-    create_new_organisation(app, 'Govikon', 'mails@govikon.ch', forms)
-
-    # cronjobs leave lingering sessions open, in real life this is not a
-    # problem, but in testing it leads to connection pool exhaustion
-    app.settings.cronjobs = Bunch(enabled=False)
-
+def create_town_app(request, use_elasticsearch):
+    app = create_app(TownApp, request, use_elasticsearch=False)
     session = app.session()
 
-    app.mail_host, app.mail_port = smtp.address
-    app.mail_sender = 'mails@govikon.ch'
-    app.mail_force_tls = False
-    app.mail_username = None
-    app.mail_password = None
-    app.mail_use_directory = False
-    app.smtp = smtp
+    forms = request.getfixturevalue('forms')
+    create_new_organisation(app, 'Govikon', 'mails@govikon.ch', forms)
 
     # usually we don't want to create the users directly, anywhere else you
     # *need* to go through the UserCollection. Here however, we can improve
     # the test speed by not hashing the password for every test.
+    test_password = request.getfixturevalue('test_password')
 
     session.add(User(
         username='admin@example.org',
