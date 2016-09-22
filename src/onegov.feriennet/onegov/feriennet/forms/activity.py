8f94dc261cb1cf1c68b71b214c636f6f7582ebe1
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-09-22 09:45:29 +0200

    Require title and lead in activity to be filled out

--- src/onegov.feriennet/onegov/feriennet/forms/activity.py
+++ src/onegov.feriennet/onegov/feriennet/forms/activity.py
@@ -3,17 +3,20 @@ from onegov.core.utils import sanitize_html
 from onegov.form import Form
 from onegov.org.utils import annotate_html
 from wtforms import TextField, TextAreaField
+from wtforms.validators import InputRequired
 
 
 class VacationActivityForm(Form):
 
     title = TextField(
         label=_("Title"),
-        description=_("The title of the activity"))
+        description=_("The title of the activity"),
+        validators=[InputRequired()])
 
     lead = TextAreaField(
         label=_("Lead"),
         description=_("Describes briefly what this activity is about"),
+        validators=[InputRequired()],
         render_kw={'rows': 4})
 
     text = TextAreaField(
