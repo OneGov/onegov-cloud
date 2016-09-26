e72890bcd0b999f0d00f31d8e1961b0d0c9a5d9b
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2016-09-26 08:08:04 +0200

    Update constraints

--- src/onegov.election_day/setup.py
+++ src/onegov.election_day/setup.py
@@ -37,7 +37,7 @@ setup(
         'cached_property',
         'cssmin',
         'onegov.core>=0.23.0',
-        'onegov.ballot>=0.9.0',
+        'onegov.ballot>=1.0.0',
         'onegov.form>=0.12.0',
         'onegov.foundation>=0.0.4',
         'onegov.shared>=0.2.0',
