62dbc2b9c35e0dee6d9d2adcf021da1bf9472594
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2016-11-18 09:17:07 +0100

    Onegov Election_Day Release 1.3.3

--- src/onegov.election_day/HISTORY.rst
+++ src/onegov.election_day/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+1.3.3 (2016-11-18)
+~~~~~~~~~~~~~~~~~~~
 
 - Update translations.
 [msom]
--- src/onegov.election_day/setup.py
+++ src/onegov.election_day/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.election_day'
 description = (
     'OneGov ballot results website used on election day.'
 )
-version = '1.3.2'
+version = '1.3.3'
 
 
 def get_long_description():
