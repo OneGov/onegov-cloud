7c01a933a603be725af08641bc9c0dde014eba6c
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-11-23 15:54:17 +0100

    Mark open bookings as denied during the confirmation

--- src/onegov.feriennet/onegov/feriennet/views/match.py
+++ src/onegov.feriennet/onegov/feriennet/views/match.py
@@ -27,7 +27,7 @@ def handle_matches(self, request, form):
         form.store_to_period(self.period)
 
         if form.confirm_period:
-            self.period.confirmed = True
+            self.confirm_period()
             request.success(_("The matching was confirmed successfully"))
         else:
             request.success(_("The matching run executed successfully"))
