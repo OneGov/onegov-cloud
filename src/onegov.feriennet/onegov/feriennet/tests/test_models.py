fc3592057b4802b57babaa9e3fc279e9926b839e
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2016-12-28 10:41:24 +0100

    Fix tests

--- src/onegov.feriennet/onegov/feriennet/tests/test_models.py
+++ src/onegov.feriennet/onegov/feriennet/tests/test_models.py
@@ -1,3 +1,4 @@
+from onegov.core.utils import Bunch
 from onegov.feriennet.models.notification_template import TemplateVariables
 from uuid import uuid4
 
@@ -5,6 +6,9 @@ from uuid import uuid4
 def test_template_variables():
 
     class MockRequest(object):
+
+        def __repr__(self):
+            return 'MockRequest'
 
         def translate(self, text):
             return text.upper()
@@ -15,6 +19,10 @@ def test_template_variables():
         def class_link(self, cls, *args, **kwargs):
             return cls.__name__
 
+        @property
+        def app(self):
+            return Bunch(org=self)
+
     class MockPeriod(object):
         id = uuid4()
         title = 'Foobar Pass'
@@ -22,7 +30,9 @@ def test_template_variables():
     t = TemplateVariables(MockRequest(), MockPeriod())
 
     assert sorted(t.bound.keys()) == [
+        "[ACTIVITIES]",
         "[BOOKINGS]",
+        "[HOMEPAGE]",
         "[INVOICES]",
         "[PERIOD]",
     ]
@@ -32,3 +42,7 @@ def test_template_variables():
         == 'Go to <a href="InvoiceItemCollection">INVOICES</a>'
     assert t.render("Go to [BOOKINGS]") \
         == 'Go to <a href="BookingCollection">BOOKINGS</a>'
+    assert t.render("Go to [ACTIVITIES]") \
+        == 'Go to <a href="VacationActivityCollection">ACTIVITIES</a>'
+    assert t.render("Go to [HOMEPAGE]") \
+        == 'Go to <a href="MockRequest">HOMEPAGE</a>'
