ac4466bbca1989d5204237049f095b15671a36b0
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-01-19 14:13:59 +0100

    Depend on latest onegov.org/onegov.town release

--- src/onegov.onboarding/setup.py
+++ src/onegov.onboarding/setup.py
@@ -36,8 +36,8 @@ setup(
         'onegov.core>=0.28.0',
         'onegov.foundation',
         'onegov.libres',
-        'onegov.org>=0.3.0',
-        'onegov.town>=1.12.0',
+        'onegov.org>=0.3.1',
+        'onegov.town>=1.12.1',
         'python-simple-hipchat',
     ],
     extras_require=dict(
