a4b327ed166f01d02f5e0e218f73f3c6cfb49cc1
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-01-30 16:41:49 +0100

    Depend on latest onegov.org release

--- src/onegov.town/setup.py
+++ src/onegov.town/setup.py
@@ -32,7 +32,7 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'onegov.org>=0.3.2',
+        'onegov.org>=0.3.3',
     ],
     extras_require=dict(
         test=[
