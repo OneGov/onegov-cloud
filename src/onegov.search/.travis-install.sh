188da2062e2b01d53f8eb6694937386d6ab16ff1
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-03-24 11:56:37 +0100

    Switch to the new elasticsearch testing regime

--- src/onegov.search/.travis-install.sh
+++ src/onegov.search/.travis-install.sh
@@ -1,11 +1,7 @@
-# /usr/bin/env sh
+#!/bin/bash
+
 pip install tox
 
 if [ "$TOXENV" = 'py34' ]; then
     pip install coveralls
-fi
-
-if [ "$ES_URL" != 'no' ]; then
-    mkdir /tmp/elasticsearch
-    wget -O - "$ES_URL" | tar xz --directory=/tmp/elasticsearch --strip-components=1
 fi
