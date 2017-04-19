a91bbfa233838d0c6df643292b68ec63f8a174c8
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-04-19 14:44:21 +0200

    Add tests

--- src/onegov.election_day/onegov/election_day/tests/test_collections.py
+++ src/onegov.election_day/onegov/election_day/tests/test_collections.py
@@ -2,10 +2,15 @@ from datetime import date, datetime, timezone
 from freezegun import freeze_time
 from onegov.ballot.models import Election, Vote
 from onegov.election_day.collections import ArchivedResultCollection
+from onegov.election_day.collections import DataSourceCollection
+from onegov.election_day.collections import DataSourceItemCollection
 from onegov.election_day.collections import NotificationCollection
 from onegov.election_day.collections import SubscriberCollection
 from onegov.election_day.models import ArchivedResult
+from onegov.election_day.models import DataSource
+from onegov.election_day.models import DataSourceItem
 from onegov.election_day.tests import DummyRequest
+from uuid import uuid4
 
 
 def test_archive(session):
@@ -410,3 +415,71 @@ def test_subscriber_collection_pagination(session):
         '+41791112299'
 
     assert len(SubscriberCollection(session, page=10).batch) == 0
+
+
+def test_data_source_collection(session):
+    collection = DataSourceCollection(session)
+
+    collection.add(DataSource(type='vote', name='ds_vote'))
+    data_source = collection.query().one()
+    assert data_source.type == 'vote'
+    assert data_source.name == 'ds_vote'
+    assert collection.by_id(data_source.id) == data_source
+
+    collection.delete(data_source)
+    assert collection.query().count() == 0
+
+
+def test_data_source_collection_pagination(session):
+    collection = DataSourceCollection(session)
+
+    for number in range(100):
+        collection.add(DataSource(type='vote', name='{:02}'.format(number)))
+    assert collection.query().count() == 100
+
+    assert DataSourceCollection(session, page=0).batch[0].name == '00'
+    assert DataSourceCollection(session, page=4).batch[4].name == '44'
+    assert DataSourceCollection(session, page=5).batch[5].name == '55'
+    assert DataSourceCollection(session, page=9).batch[9].name == '99'
+
+    assert len(DataSourceCollection(session, page=10).batch) == 0
+
+
+def test_data_source_item_collection(session):
+    DataSourceCollection(session).add(DataSource(type='vote', name='ds'))
+    data_source = session.query(DataSource).one()
+
+    collection = DataSourceItemCollection(session, id=data_source.id)
+    assert collection.source is data_source
+
+    collection.add(DataSourceItem(number='1', district='2'))
+    item = collection.query().one()
+    assert item.number == '1'
+    assert item.district == '2'
+    assert collection.by_id(item.id) == item
+
+    collection.delete(item)
+    assert collection.query().count() == 0
+
+
+def test_data_source_item_collection_pagination(session):
+    id_ = uuid4()
+    DataSourceCollection(session).add(
+        DataSource(type='vote', name='ds', id=id_)
+    )
+    collection = DataSourceItemCollection(session, id=id_)
+
+    for number in range(100):
+        collection.add(DataSourceItem(number='{:02}'.format(number)))
+    assert collection.query().count() == 100
+
+    assert DataSourceItemCollection(session, id=id_, page=0).batch[0].number \
+        == '00'
+    assert DataSourceItemCollection(session, id=id_, page=4).batch[4].number \
+        == '44'
+    assert DataSourceItemCollection(session, id=id_, page=5).batch[5].number \
+        == '55'
+    assert DataSourceItemCollection(session, id=id_, page=9).batch[9].number \
+        == '99'
+
+    assert len(DataSourceItemCollection(session, page=10).batch) == 0
