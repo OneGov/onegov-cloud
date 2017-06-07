ec13313e8e8e2104cce1474f509b3923b72930e2
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-06-07 16:58:11 +0200

    Move the payment processing to onegov.pay

--- src/onegov.form/onegov/form/models.py
+++ src/onegov.form/onegov/form/models.py
@@ -6,10 +6,10 @@ from onegov.core.orm.mixins import ContentMixin, TimestampMixin
 from onegov.core.orm.mixins import meta_property, content_property
 from onegov.core.orm.types import JSON, UUID
 from onegov.core.orm.types import UTCDateTime
-from onegov.form import log
 from onegov.form.display import render_field
 from onegov.form.parser import parse_form
-from onegov.pay import CARD_ERRORS, Payable, ManualPayment
+from onegov.pay import Payable
+from onegov.pay import process_payment
 from onegov.search import ORMSearchable
 from sedate import utcnow
 from sqlalchemy import Column, Enum, ForeignKey, Text
@@ -222,45 +222,17 @@ class FormSubmission(Base, TimestampMixin, Payable):
         by the provider that can be used to charge the credit card and creates
         a payment record if necessary.
 
-        Returns True if the payment was processed successfully. If there's no
-        price for the current form, the processing will be successful as well.
+        Returns True or a payment object if the payment was processed
+        successfully. That is, if there is a payment or if there is no payment
+        required the method returns truthy.
 
         """
 
         total = self.form_obj.total()
 
-        if not total:
-            return True
-
-        if self.form.payment_method == 'free':
-            payment_method = token and 'cc' or 'manual'
-        else:
-            payment_method = self.form.payment_method
-
-        if payment_method == 'manual':
-            self.payment = ManualPayment(amount=total[0], currency=total[1])
-        elif payment_method == 'cc':
-            if not token:
-                return False
-
-            try:
-                self.payment = provider.charge(
-                    amount=total[0],
-                    currency=total[1],
-                    token=token
-                )
-            except CARD_ERRORS:
-                log.exception(
-                    "Processing {} {} through {} with token {} failed".format(
-                        total[0],
-                        total[1],
-                        provider.title,
-                        token
-                    )
-                )
-                return False
-        else:
-            raise NotImplementedError
+        if total and total.amount > 0:
+            return process_payment(
+                self.form.payment_method, total, provider, token)
 
         return True
 
