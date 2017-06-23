005310e1ed1e6a545ad4f2c5a04ac40b92938a06
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-06-23 11:06:20 +0200

    Update dependency

--- src/onegov.election_day/setup.py
+++ src/onegov.election_day/setup.py
@@ -36,7 +36,7 @@ setup(
         'babel',
         'cached_property',
         'cssmin',
-        'onegov.core>=0.41.0',
+        'onegov.core>=0.41.1',
         'onegov.ballot>=1.7.0',
         'onegov.form>=0.12.0',
         'onegov.foundation>=0.0.4',
