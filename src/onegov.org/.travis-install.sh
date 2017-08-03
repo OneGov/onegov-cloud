13caa14f6380159e59825a310fbf5cdafef9d607
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-08-03 15:09:23 +0200

    Removes chromedriver 2.31 workaround

--- src/onegov.org/.travis-install.sh
+++ src/onegov.org/.travis-install.sh
@@ -5,7 +5,3 @@ if [ "$TOXENV" = 'py34' ]; then
     pip install coveralls
 fi
 
-# needed until chromedriver 2.31 is officially released
-mkdir -p /home/travis/.wdm/chromedriver/2.30
-curl -L 'https://github.com/davidthornton/chromedriver-2.31/blob/master/chromedriver?raw=true' -o /home/travis/.wdm/chromedriver/2.30/chromedriver
-chmod +x /home/travis/.wdm/chromedriver/2.30/chromedriver
