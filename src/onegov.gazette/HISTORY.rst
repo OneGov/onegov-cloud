cf9c0aaa964dcf1774946b2e02580d6ac6b69044
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-08-21 10:44:43 +0200

    Onegov Gazette Release 0.1.1

--- src/onegov.gazette/HISTORY.rst
+++ src/onegov.gazette/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.1.1 (2017-08-21)
+~~~~~~~~~~~~~~~~~~~
 
 - Fixes ordering by first issue.
   [msom]
--- src/onegov.gazette/setup.py
+++ src/onegov.gazette/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.gazette'
 description = (
     'OneGov official notices website.'
 )
-version = '0.1.0'
+version = '0.1.1'
 
 
 def get_long_description():
