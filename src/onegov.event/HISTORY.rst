d709d3815fdc706051ef96ec5726676b454d6f40
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-09-26 11:29:39 +0200

    Onegov Event Release 0.6.0

--- src/onegov.event/HISTORY.rst
+++ src/onegov.event/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.6.0 (2017-09-26)
+~~~~~~~~~~~~~~~~~~~
 
 - Switches to onegov.search's automatic language detection.
   [href]
--- src/onegov.event/setup.py
+++ src/onegov.event/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.event'
 description = (
     'Calendar of events for OneGov.'
 )
-version = '0.5.1'
+version = '0.6.0'
 
 
 def get_long_description():
