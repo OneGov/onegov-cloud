9d502f01e8811c19ae99e45c294cf5d0a0b3a9d2
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-10-13 13:34:36 +0200

    Fix join statements

--- src/onegov.notice/onegov/notice/collections.py
+++ src/onegov.notice/onegov/notice/collections.py
@@ -156,16 +156,16 @@ class OfficialNoticeCollection(OfficialNoticeCollectionPagination):
         if self.order in inspect(self.model_class).columns.keys():
             attribute = getattr(self.model_class, self.order)
         elif self.order == 'group.name':
-            query = query.join(UserGroup, isouter=True)
+            query = query.join(self.model_class.group, isouter=True)
             attribute = func.coalesce(UserGroup.name, '')
         elif self.order == 'user.realname':
-            query = query.join(User, isouter=True)
+            query = query.join(self.model_class.user, isouter=True)
             attribute = func.coalesce(User.realname, '')
         elif self.order == 'user.username':
-            query = query.join(User, isouter=True)
+            query = query.join(self.model_class.user, isouter=True)
             attribute = func.coalesce(User.username, '')
         elif self.order == 'user.name':
-            query = query.join(User, isouter=True)
+            query = query.join(self.model_class.user, isouter=True)
             attribute = func.coalesce(User.realname, User.username, '')
         else:
             attribute = self.model_class.first_issue
