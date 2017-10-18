2ed035fc993606433772f0c7d0fecfaddd79cba8
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-10-18 11:51:10 +0200

    Support latest changes to formcode syntax

--- src/onegov.org/onegov/org/assets/js/ace-mode-form.js
+++ src/onegov.org/onegov/org/assets/js/ace-mode-form.js
@@ -18,6 +18,10 @@ var FormHighlightRules = function() {
                 regex: /(___|\*\*\*|\.\.\.|@@@|YYYY.MM.DD|YYYY.MM.DD HH:MM|HH:MM|[0-9]+[0-9\.]+[0-9]+)/
             },
             {
+                token: 'support.type',
+                regex: /\/.*/
+            },
+            {
                 token: 'keyword',
                 regex: /[\[\(]{1}[x ]?[\]\)]{1}/
             },
