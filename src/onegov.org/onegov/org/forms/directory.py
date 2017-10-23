fcc950fdc85699fee50d0115d34f9bb28ad4012a
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-10-23 14:45:29 +0200

    Implicitly set some fields, limiting the configuration options of a directory for the sake of simplicity

--- src/onegov.org/onegov/org/forms/directory.py
+++ src/onegov.org/onegov/org/forms/directory.py
@@ -2,6 +2,7 @@ from onegov.directory import DirectoryConfiguration
 from onegov.form import Form
 from onegov.form.validators import ValidFormDefinition
 from onegov.org import _
+from onegov.core.utils import safe_format_keys
 from wtforms import StringField, TextAreaField, validators
 
 
@@ -23,16 +24,60 @@ class DirectoryForm(Form):
         ],
         render_kw={'rows': 32, 'data-editor': 'form'})
 
-    configuration = TextAreaField(
-        label=_("Configuration"),
-        validators=[validators.InputRequired()],
-        render_kw={'rows': 32, 'data-editor': 'yaml'})
+    title_format = StringField(
+        label=_("Title Format"),
+        validators=[validators.InputRequired()])
+
+    lead_format = StringField(
+        label=_("Lead Format"),
+        validators=[validators.InputRequired()])
+
+    content_fields = TextAreaField(
+        label=_("Content Fields"),
+        render_kw={'rows': 8})
+
+    address_fields = TextAreaField(
+        label=_("Address Fields"),
+        render_kw={'rows': 8})
+
+    keyword_fields = TextAreaField(
+        label=_("Keyword Fields"),
+        render_kw={'rows': 8})
+
+    def extract_field_ids(self, field):
+        for line in field.data.splitlines():
+            line = line.strip()
+            yield line
+
+    @property
+    def configuration(self):
+        content_fields = list(self.extract_field_ids(self.content_fields))
+        address_fields = list(self.extract_field_ids(self.address_fields))
+        keyword_fields = list(self.extract_field_ids(self.keyword_fields))
+
+        return DirectoryConfiguration(
+            title=self.title_format.data,
+            lead=self.lead_format.data,
+            order=safe_format_keys(self.title_format.data),
+            keywords=keyword_fields,
+            searchable=content_fields + address_fields,
+            display={
+                'content': content_fields,
+                'address': address_fields
+            }
+        )
+
+    @configuration.setter
+    def configuration(self, cfg):
+        self.title_format.data = cfg.title
+        self.lead_format.data = cfg.lead or ''
+        self.content_fields.data = '\n'.join(cfg.display.get('content', ''))
+        self.address_fields.data = '\n'.join(cfg.display.get('address', ''))
+        self.keyword_fields.data = '\n'.join(cfg.keywords)
 
     def populate_obj(self, obj):
         super().populate_obj(obj, exclude={'configuration'})
-        obj.configuration = DirectoryConfiguration.from_yaml(
-            self.configuration.data
-        )
+        obj.configuration = self.configuration
 
     def process_obj(self, obj):
-        self.configuration.data = obj.configuration.to_yaml()
+        self.configuration = obj.configuration
