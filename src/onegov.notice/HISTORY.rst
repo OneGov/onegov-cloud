ea01ca02a8c87ec7658fabed727efe982d64b4f2
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-10-26 15:42:49 +0200

    Onegov Notice Release 0.3.0

--- src/onegov.notice/HISTORY.rst
+++ src/onegov.notice/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.3.0 (2017-10-26)
+~~~~~~~~~~~~~~~~~~~
 
 - Adds categories and organizations to notices.
   [msom]
--- src/onegov.notice/setup.py
+++ src/onegov.notice/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.notice'
 description = (
     'Official notices for OneGov.'
 )
-version = '0.2.0'
+version = '0.3.0'
 
 
 def get_long_description():
