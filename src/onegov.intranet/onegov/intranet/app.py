4bfa94e87b1d8b1416ad7f51463c9bd706fa6c64
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-11-14 16:15:59 +0100

    Gets intranet working in principle

--- src/onegov.intranet/onegov/intranet/app.py
+++ src/onegov.intranet/onegov/intranet/app.py
@@ -3,3 +3,8 @@ from onegov.org import OrgApp
 
 class IntranetApp(OrgApp):
     pass
+
+
+@IntranetApp.setting(section='org', name='enable_yubikey')
+def get_enable_yubikey():
+    return True
--- src/onegov.intranet/onegov/intranet/security.py
+++ src/onegov.intranet/onegov/intranet/security.py
@@ -1,5 +1,8 @@
 from onegov.core.security import Public, Private, Personal, Secret
+from onegov.core.static import StaticFile
+from onegov.core.theme import ThemeFile
 from onegov.intranet.app import IntranetApp
+from onegov.user import Auth
 
 
 @IntranetApp.setting_section(section="roles")
@@ -50,3 +53,38 @@ def get_roles_setting():
         # the public has no access whatsoever
         'anonymous': set()
     }
+
+
+@IntranetApp.permission_rule(
+    model=StaticFile,
+    permission=Public,
+    identity=None)
+def may_view_static_files_not_logged_in(app, identity, model, permission):
+    """ Always allow to view static files.
+
+    Those files are public anyway, since we are open-source.
+
+    """
+    return True
+
+
+@IntranetApp.permission_rule(
+    model=ThemeFile,
+    permission=Public,
+    identity=None)
+def may_view_theme_files_not_logged_in(app, identity, model, permission):
+    """ Always allow to view theme files.
+
+    Those files are public anyway, since we are open-source.
+
+    """
+    return True
+
+
+@IntranetApp.permission_rule(
+    model=Auth,
+    permission=Public,
+    identity=None)
+def may_view_auth_not_logged_in(app, identity, model, permission):
+    """ Anonymous needs to be able to log in. """
+    return True
