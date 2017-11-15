a43e17623c4cf48bfdf7c2d7db36cc13ff540e4b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-11-15 13:44:36 +0100

    Better approach to daily ticket statistics field for members

--- src/onegov.org/onegov/org/forms/userprofile.py
+++ src/onegov.org/onegov/org/forms/userprofile.py
@@ -8,17 +8,28 @@ class UserProfileForm(Form):
 
     daily_ticket_statistics = BooleanField(_("Send a daily status e-mail."))
 
+    @property
+    def enable_daily_ticket_statistics(self):
+        roles = self.request.app.settings.org.status_mail_roles
+        return self.request.current_role in roles
+
+    def on_request(self):
+        if not self.enable_daily_ticket_statistics:
+            self.delete_field('daily_ticket_statistics')
+
     def populate_obj(self, model):
         super().populate_obj(model, exclude={
             'daily_ticket_statistics'
         })
 
-        model.data = model.data or {}
-        model.data['daily_ticket_statistics']\
-            = self.daily_ticket_statistics.data
+        if self.enable_daily_ticket_statistics:
+            model.data = model.data or {}
+            model.data['daily_ticket_statistics']\
+                = self.daily_ticket_statistics.data
 
     def process_obj(self, obj):
         super().process_obj(obj)
 
-        self.daily_ticket_statistics.data\
-            = (obj.data or {}).get('daily_ticket_statistics', True)
+        if self.enable_daily_ticket_statistics:
+            self.daily_ticket_statistics.data\
+                = (obj.data or {}).get('daily_ticket_statistics', True)
