7930136efa04b891f21616646ad9aad7ba0b28a6
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-11-16 12:51:04 +0100

    Optimizes generation of weekly PDFs somewhat

--- src/onegov.gazette/onegov/gazette/pdf.py
+++ src/onegov.gazette/onegov/gazette/pdf.py
@@ -58,47 +58,70 @@ class Pdf(PdfBase):
     @classmethod
     def from_issue(cls, issue, request, file=None):
         """ Generate a PDF for one issue. """
-        # Collect the data
-        session = request.app.session()
-        categories = session.query(Category).order_by(Category.order).all()
-        roots = session.query(Organization).filter_by(parent_id=None)
-        roots = roots.order_by(Organization.order)
 
+        # Collect the data
         data = []
-        for root in roots:
-            root_data = []
-            if not root.children:
-                for category in categories:
-                    notices = cls.query_notices(
-                        session, issue.name, root.name, category.name
-                    )
-                    if notices:
-                        root_data.append({
-                            'title': category.title,
-                            'notices': notices
-                        })
-            else:
-                for child in root.children:
-                    child_data = []
+
+        session = request.app.session()
+
+        used_categories = session.query(GazetteNotice._categories.keys())
+        used_categories = used_categories.filter(
+            GazetteNotice._issues.has_key(issue.name),  # noqa
+            GazetteNotice.state == 'published',
+        )
+        used_categories = [cat[0][0] for cat in used_categories]
+
+        used_organizations = session.query(GazetteNotice._organizations.keys())
+        used_organizations = used_organizations.filter(
+            GazetteNotice._issues.has_key(issue.name),  # noqa
+            GazetteNotice.state == 'published',
+        )
+        used_organizations = [cat[0][0] for cat in used_organizations]
+
+        if used_categories and used_organizations:
+            categories = session.query(Category)
+            categories = categories.filter(Category.name.in_(used_categories))
+            categories = categories.order_by(Category.order).all()
+
+            roots = session.query(Organization).filter_by(parent_id=None)
+            roots = roots.order_by(Organization.order)
+
+            for root in roots:
+                root_data = []
+                if not root.children:
                     for category in categories:
                         notices = cls.query_notices(
-                            session, issue.name, child.name, category.name
+                            session, issue.name, root.name, category.name
                         )
                         if notices:
-                            child_data.append({
+                            root_data.append({
                                 'title': category.title,
                                 'notices': notices
                             })
-                    if child_data:
-                        root_data.append({
-                            'title': child.title,
-                            'children': child_data
-                        })
-            if root_data:
-                data.append({
-                    'title': root.title,
-                    'children': root_data
-                })
+                else:
+                    for child in root.children:
+                        if child.name not in used_organizations:
+                            continue
+                        child_data = []
+                        for category in categories:
+                            notices = cls.query_notices(
+                                session, issue.name, child.name, category.name
+                            )
+                            if notices:
+                                child_data.append({
+                                    'title': category.title,
+                                    'notices': notices
+                                })
+                        if child_data:
+                            root_data.append({
+                                'title': child.title,
+                                'children': child_data
+                            })
+                if root_data:
+                    data.append({
+                        'title': root.title,
+                        'children': root_data
+                    })
 
         # Generate the PDF
         layout = Layout(None, request)
