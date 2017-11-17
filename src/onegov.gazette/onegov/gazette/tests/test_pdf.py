cf2976d6b0343a4be5e13d353a9a763a068eafdd
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-11-17 11:33:16 +0100

    Fix tests

--- src/onegov.gazette/onegov/gazette/tests/test_pdf.py
+++ src/onegov.gazette/onegov/gazette/tests/test_pdf.py
@@ -44,9 +44,13 @@ def test_pdf_h():
         pdf.h('h3', 3)
         assert h3.called
 
-    with patch.object(pdf, 'h3') as h3:
-        pdf.h('h3', 4)
-        assert h3.called
+    with patch.object(pdf, 'h4') as h4:
+        pdf.h('h4', 4)
+        assert h4.called
+
+    with patch.object(pdf, 'h4') as h4:
+        pdf.h('h5', 5)
+        assert h4.called
 
 
 def test_pdf_unfold_data():
@@ -57,19 +61,19 @@ def test_pdf_unfold_data():
                 {
                     'title': 'title-1-1',
                     'children': [{'title': 'title-1-1-1'}],
-                    'notices': [['title-1-1-a', 'text-1-1-a']]
+                    'notices': [['title-1-1-a', 'text-1-1-a', '1']]
                 },
                 {
                     'title': 'title-1-2',
                     'notices': [
-                        ['title-1-2-a', 'text-1-2-a'],
-                        ['title-1-2-b', 'text-1-2-b'],
-                        ['title-1-2-c', 'text-1-2-c'],
+                        ['title-1-2-a', 'text-1-2-a', '1'],
+                        ['title-1-2-b', 'text-1-2-b', '1'],
+                        ['title-1-2-c', 'text-1-2-c', '1'],
                     ],
                     'children': [{
                         'notices': [
-                            ['title-1-2-1-a', 'text-1-2-1-a'],
-                            ['title-1-2-1-b', 'text-1-2-1-b']
+                            ['title-1-2-1-a', 'text-1-2-1-a', '1'],
+                            ['title-1-2-1-b', 'text-1-2-1-b', '1'],
                         ]
                     }]
                 }
@@ -77,19 +81,20 @@ def test_pdf_unfold_data():
         },
         {
             'title': 'title-2',
-            'notices': [['title-2-a', 'text-2-a']],
+
+            'notices': [['title-2-a', 'text-2-a', '1']],
             'children': [
-                {'notices': [['title-2-1-a', 'text-2-1-a']]},
-                {'notices': [['title-2-2-a', 'text-2-2-a']]},
-                {'notices': [['title-2-3-a', 'text-2-3-a']]}
+                {'notices': [['title-2-1-a', 'text-2-1-a', '1']]},
+                {'notices': [['title-2-2-a', 'text-2-2-a', '1']]},
+                {'notices': [['title-2-3-a', 'text-2-3-a', '1']]}
             ]
         },
         {
             'notices': [
-                ['title-3-a', 'text-3-a'],
-                ['title-3-b', 'text-3-b'],
-                ['title-3-c', 'text-3-c'],
-                ['title-3-d', 'text-3-d'],
+                ['title-3-a', 'text-3-a', '1'],
+                ['title-3-b', 'text-3-b', '1'],
+                ['title-3-c', 'text-3-c', '1'],
+                ['title-3-d', 'text-3-d', '1'],
             ]
         }
     ]
@@ -99,27 +104,34 @@ def test_pdf_unfold_data():
     pdf.init_a4_portrait()
     pdf.unfold_data(data)
 
-    story = [x.text for x in pdf.story if hasattr(x, 'text')]
     expected = [
         'title-1',
         'title-1-1',
-        'title-1-1-a', 'text-1-1-a',
+        'title-1-1-a 1', 'text-1-1-a',
         'title-1-1-1',
         'title-1-2',
-        'title-1-2-a', 'text-1-2-a',
-        'title-1-2-b', 'text-1-2-b',
-        'title-1-2-c', 'text-1-2-c',
-        'title-1-2-1-a', 'text-1-2-1-a',
-        'title-1-2-1-b', 'text-1-2-1-b',
+        'title-1-2-a 1', 'text-1-2-a',
+        'title-1-2-b 1', 'text-1-2-b',
+        'title-1-2-c 1', 'text-1-2-c',
+        'title-1-2-1-a 1', 'text-1-2-1-a',
+        'title-1-2-1-b 1', 'text-1-2-1-b',
         'title-2',
-        'title-2-a', 'text-2-a',
-        'title-2-1-a', 'text-2-1-a',
-        'title-2-2-a', 'text-2-2-a',
-        'title-2-3-a', 'text-2-3-a',
-        'title-3-a', 'text-3-a',
-        'title-3-b', 'text-3-b',
-        'title-3-c', 'text-3-c',
-        'title-3-d', 'text-3-d'
+        'title-2-a 1', 'text-2-a',
+        'title-2-1-a 1', 'text-2-1-a',
+        'title-2-2-a 1', 'text-2-2-a',
+        'title-2-3-a 1', 'text-2-3-a',
+        'title-3-a 1', 'text-3-a',
+        'title-3-b 1', 'text-3-b',
+        'title-3-c 1', 'text-3-c',
+        'title-3-d 1', 'text-3-d',
+    ]
+
+    story = [
+        x.text.replace('<b>', '')
+              .replace('</b>', '')
+              .replace('<i><font size="9.84375">', '')
+              .replace('</font></i>', '')
+        for x in pdf.story if hasattr(x, 'text')
     ]
     assert story == expected
 
@@ -154,20 +166,20 @@ def test_pdf_query_notices(session, issues, organizations, categories):
     session.flush()
 
     assert Pdf.query_notices(session, '2017-40', '100', '10') == [
-        ('100-10', '2017-40-1, 2017-41-4'),
-        ('100-10', '2017-40-2, 2017-41-3'),
-        ('100-10', '2017-40-3, 2017-41-2'),
-        ('100-10', '2017-40-4, 2017-41-1')
+        ('100-10', '2017-40-1, 2017-41-4', '1'),
+        ('100-10', '2017-40-2, 2017-41-3', '2'),
+        ('100-10', '2017-40-3, 2017-41-2', '3'),
+        ('100-10', '2017-40-4, 2017-41-1', '4')
     ]
     assert Pdf.query_notices(session, '2017-41', '100', '10') == [
-        ('100-10', '2017-40-4, 2017-41-1'),
-        ('100-10', '2017-40-3, 2017-41-2'),
-        ('100-10', '2017-40-2, 2017-41-3'),
-        ('100-10', '2017-40-1, 2017-41-4'),
-        ('100-10', '2017-41-5, 2017-42-1')
+        ('100-10', '2017-40-4, 2017-41-1', '1'),
+        ('100-10', '2017-40-3, 2017-41-2', '2'),
+        ('100-10', '2017-40-2, 2017-41-3', '3'),
+        ('100-10', '2017-40-1, 2017-41-4', '4'),
+        ('100-10', '2017-41-5, 2017-42-1', '5')
     ]
     assert Pdf.query_notices(session, '2017-42', '100', '10') == [
-        ('100-10', '2017-41-5, 2017-42-1')
+        ('100-10', '2017-41-5, 2017-42-1', '1')
     ]
 
 
@@ -204,21 +216,21 @@ def test_pdf_from_issue(gazette_app):
         text = ''.join([page.extractText() for page in reader.pages])
         assert text == (
             '© 2017 Govikon\n1\n'
-            'Gazette No. 40, Freitag 06.10.2017\n'
+            'Gazette No. 40, 06.10.2017\n'
             'State Chancellery\n'
             'Complaints\n'
-            '100-10\n'
+            '100-10 1\n'
             '2017-40-1, 2017-41-4\n'
-            '100-10\n'
+            '100-10 3\n'
             '2017-40-3, 2017-41-2\n'
             'Civic Community\n'
             'Complaints\n'
-            '200-10\n'
+            '200-10 2\n'
             '2017-40-2, 2017-41-3\n'
             'Churches\n'
             'Evangelical Reformed Parish\n'
             'Education\n'
-            '410-11\n'
+            '410-11 4\n'
             '2017-40-4, 2017-41-1\n'
         )
 
@@ -228,25 +240,25 @@ def test_pdf_from_issue(gazette_app):
         text = ''.join([page.extractText() for page in reader.pages])
         assert text == (
             '© 2017 Govikon\n1\n'
-            'Gazette No. 41, Freitag 13.10.2017\n'
+            'Gazette No. 41, 13.10.2017\n'
             'State Chancellery\n'
             'Complaints\n'
-            '100-10\n'
+            '100-10 2\n'
             '2017-40-3, 2017-41-2\n'
-            '100-10\n'
+            '100-10 4\n'
             '2017-40-1, 2017-41-4\n'
             'Civic Community\n'
             'Complaints\n'
-            '200-10\n'
+            '200-10 3\n'
             '2017-40-2, 2017-41-3\n'
             'Churches\n'
             'Evangelical Reformed Parish\n'
             'Education\n'
-            '410-11\n'
+            '410-11 1\n'
             '2017-40-4, 2017-41-1\n'
             'Sikh Community\n'
             'Education\n'
-            '420-11\n'
+            '420-11 5\n'
             '2017-41-5, 2017-42-1\n'
         )
 
@@ -257,10 +269,10 @@ def test_pdf_from_issue(gazette_app):
         text = ''.join([page.extractText() for page in reader.pages])
         assert text == (
             '© 2018 Govikon\n1\n'
-            'Gazette No. 42, Freitag 20.10.2017\n'
+            'Gazette No. 42, 20.10.2017\n'
             'Churches\n'
             'Sikh Community\n'
             'Education\n'
-            '420-11\n'
+            '420-11 1\n'
             '2017-41-5, 2017-42-1\n'
         )
