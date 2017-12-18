78835dbd9f68168bc858cbd5abd204f9a01afbcb
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-12-18 09:55:44 +0100

    Add test

--- src/onegov.election_day/onegov/election_day/tests/models/test_notification.py
+++ src/onegov.election_day/onegov/election_day/tests/models/test_notification.py
@@ -4,8 +4,14 @@ from datetime import timezone
 from freezegun import freeze_time
 from json import loads
 from onegov.ballot import BallotResult
+from onegov.ballot import Candidate
+from onegov.ballot import CandidateResult
 from onegov.ballot import ComplexVote
 from onegov.ballot import Election
+from onegov.ballot import ElectionResult
+from onegov.ballot import List
+from onegov.ballot import ListResult
+from onegov.ballot import ProporzElection
 from onegov.ballot import Vote
 from onegov.election_day.models import EmailNotification
 from onegov.election_day.models import EmailSubscriber
@@ -314,6 +320,7 @@ def test_email_notification_vote(election_day_app, session):
             'Vote - Nouveaux résultats intermédiaires'
         ]
         contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "9 of 11" in contents
         assert "9 da 11" in contents
         assert "9 di 11" in contents
         assert "9 de 11" in contents
@@ -333,6 +340,7 @@ def test_email_notification_vote(election_day_app, session):
             'Vote avec contre-projet - Nouveaux résultats intermédiaires'
         ]
         contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "9 of 11" in contents
         assert "9 da 11" in contents
         assert "9 di 11" in contents
         assert "9 de 11" in contents
@@ -388,8 +396,301 @@ def test_email_notification_vote(election_day_app, session):
 
 
 def test_email_notification_election(election_day_app, session):
-    # todo:
-    pass
+    with freeze_time("2008-01-01 00:00"):
+        mock = Mock()
+        election_day_app.send_email = mock
+
+        principal = election_day_app.principal
+        principal.email_notification = True
+        election_day_app.cache.set('principal', principal)
+
+        session.add(
+            Election(
+                title_translations={
+                    'de_CH': "Majorzwahl",
+                    'fr_CH': "Election selon le système majoritaire",
+                    'it_CH': "Elezione secondo il sistema maggioritario",
+                    'rm_CH': "Elecziun da maiorz"
+                },
+                domain='federation',
+                date=date(2011, 1, 1),
+                number_of_mandates=1,
+                total_entities=12
+            )
+        )
+        majorz = session.query(Election).one()
+
+        session.add(
+            ProporzElection(
+                title_translations={
+                    'de_CH': "Proporzwahl",
+                    'fr_CH': "Election selon le système proportionnel",
+                    # 'it_CH' missing
+                    'rm_CH': "Elecziun da proporz"
+                },
+                domain='federation',
+                date=date(2011, 1, 1),
+                number_of_mandates=1,
+                total_entities=12
+            )
+        )
+        proporz = session.query(ProporzElection).one()
+
+        request = DummyRequest(app=election_day_app, session=session)
+        freezed = datetime(2008, 1, 1, 0, 0, tzinfo=timezone.utc)
+
+        session.add(EmailSubscriber(address='de@examp.le', locale='de_CH'))
+        session.add(EmailSubscriber(address='fr@examp.le', locale='fr_CH'))
+        session.add(EmailSubscriber(address='it@examp.le', locale='it_CH'))
+        session.add(EmailSubscriber(address='rm@examp.le', locale='rm_CH'))
+        session.add(EmailSubscriber(address='en@examp.le', locale='en'))
+
+        # No results yet
+        # ... majorz
+        notification = EmailNotification()
+        notification.trigger(request, majorz)
+        assert notification.type == 'email'
+        assert notification.election_id == majorz.id
+        assert notification.last_modified == freezed
+        assert mock.call_count == 5
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            (
+                'Election selon le système majoritaire - '
+                'Nouveaux résultats intermédiaires'
+            ),
+            'Elecziun da maiorz - Novs resultats intermediars',
+            (
+                'Elezione secondo il sistema maggioritario - '
+                'Nuovi risultati intermedi'
+            ),
+            'Majorzwahl - Neue Zwischenresultate',
+            'Majorzwahl - New intermediate results'
+        ]
+        assert sorted([call[2]['receivers'] for call in mock.mock_calls]) == [
+            ('de@examp.le',),
+            ('en@examp.le',),
+            ('fr@examp.le',),
+            ('it@examp.le',),
+            ('rm@examp.le',)
+        ]
+        assert set([call[2]['reply_to'] for call in mock.mock_calls]) == {
+            'mails@govikon.ch'
+        }
+        assert set([
+            call[2]['headers']['List-Unsubscribe-Post']
+            for call in mock.mock_calls
+        ]) == {'List-Unsubscribe=One-Click'}
+        assert sorted([
+            call[2]['headers']['List-Unsubscribe'] for call in mock.mock_calls
+        ]) == [
+            "<Principal/unsubscribe-email?opaque={'address': 'de@examp.le'}>",
+            "<Principal/unsubscribe-email?opaque={'address': 'en@examp.le'}>",
+            "<Principal/unsubscribe-email?opaque={'address': 'fr@examp.le'}>",
+            "<Principal/unsubscribe-email?opaque={'address': 'it@examp.le'}>",
+            "<Principal/unsubscribe-email?opaque={'address': 'rm@examp.le'}>"
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "Noch keine Resultate" in contents
+        assert "Pas de résultats à l'heure actuelle" in contents
+        assert "Ancora nessun risultato" in contents
+        assert "Anc nagins resultats avant maun" in contents
+
+        # ... proporz
+        mock.reset_mock()
+        notification = EmailNotification()
+        notification.trigger(request, proporz)
+        assert notification.type == 'email'
+        assert notification.election_id == proporz.id
+        assert notification.last_modified == freezed
+        assert mock.call_count == 5
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            (
+                'Election selon le système proportionnel - '
+                'Nouveaux résultats intermédiaires'
+            ),
+            'Elecziun da proporz - Novs resultats intermediars',
+            'Proporzwahl - Neue Zwischenresultate',
+            'Proporzwahl - New intermediate results',
+            'Proporzwahl - Nuovi risultati intermedi'
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "Noch keine Resultate" in contents
+        assert "Pas de résultats à l'heure actuelle" in contents
+        assert "Ancora nessun risultato" in contents
+        assert "Anc nagins resultats avant maun" in contents
+
+        # Intermediate results
+        proporz.lists.append(List(list_id='1', name='FDP'))
+        proporz.lists.append(List(list_id='2', name='SP'))
+        lids = {
+            '1': session.query(List).filter_by(list_id='1').one().id,
+            '2': session.query(List).filter_by(list_id='2').one().id
+        }
+
+        keys = ['candidate_id', 'first_name', 'family_name', 'elected']
+        for values in (
+            ('1', 'Peter', 'Maier', False),
+            ('2', 'Hans', 'Müller', False),
+        ):
+            kw = {key: values[index] for index, key in enumerate(keys)}
+            majorz.candidates.append(Candidate(**kw))
+            kw['list_id'] = lids[kw['candidate_id']]
+            proporz.candidates.append(Candidate(**kw))
+        mcids = {
+            '1': majorz.candidates.filter_by(candidate_id='1').one().id,
+            '2': majorz.candidates.filter_by(candidate_id='2').one().id,
+        }
+        pcids = {
+            '1': proporz.candidates.filter_by(candidate_id='1').one().id,
+            '2': proporz.candidates.filter_by(candidate_id='2').one().id,
+        }
+
+        keys = [
+            'entity_id', 'group', 'elegible_voters', 'received_ballots',
+            'blank_ballots', 'invalid_ballots', 'blank_votes', 'invalid_votes'
+        ]
+        for values in (
+            (1711, 'Zug', 16516, 8516, 80, 1, 0, 0),
+            (1706, 'Oberägeri', 3560, 1560, 18, 0, 0, 0),
+            (1709, 'Unterägeri', 5245, 2245, 18, 1, 0, 0),
+            (1704, 'Menzingen', 2917, 1917, 17, 0, 0, 0),
+            (1701, 'Baar', 13828, 6828, 54, 3, 0, 0),
+            (1702, 'Cham', 9687, 4687, 60, 0, 0, 0),
+            (1703, 'Hünenberg', 5842, 2842, 15, 1, 0, 0),
+            (1708, 'Steinhausen', 5989, 2989, 17, 0, 0, 0),
+            (1707, 'Risch', 6068, 3068, 17, 0, 0, 0),
+            (1710, 'Walchwil', 2016, 1016, 8, 0, 0, 0),
+            (1705, 'Neuheim', 1289, 689, 10, 1, 0, 0),
+        ):
+            kw = {key: values[index] for index, key in enumerate(keys)}
+            result = ElectionResult(**kw)
+            result.candidate_results.append(
+                CandidateResult(candidate_id=mcids['1'], votes=500)
+            )
+            result.candidate_results.append(
+                CandidateResult(candidate_id=mcids['2'], votes=200)
+            )
+            majorz.results.append(result)
+
+            result = ElectionResult(**kw)
+            result.candidate_results.append(
+                CandidateResult(candidate_id=pcids['1'], votes=500)
+            )
+            result.candidate_results.append(
+                CandidateResult(candidate_id=pcids['2'], votes=200)
+            )
+            result.list_results.append(
+                ListResult(list_id=lids['1'], votes=700)
+            )
+            result.list_results.append(
+                ListResult(list_id=lids['2'], votes=600)
+            )
+            proporz.results.append(result)
+        majorz.counted_entities = 11
+        proporz.counted_entities = 11
+
+        # ... majorz
+        mock.reset_mock()
+        notification = EmailNotification()
+        notification.trigger(request, majorz)
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            (
+                'Election selon le système majoritaire - '
+                'Nouveaux résultats intermédiaires'
+            ),
+            'Elecziun da maiorz - Novs resultats intermediars',
+            (
+                'Elezione secondo il sistema maggioritario - '
+                'Nuovi risultati intermedi'
+            ),
+            'Majorzwahl - Neue Zwischenresultate',
+            'Majorzwahl - New intermediate results'
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "11 of 12" in contents
+        assert "11 da 12" in contents
+        assert "11 di 12" in contents
+        assert "11 de 12" in contents
+        assert "11 von 12" in contents
+        assert "Maier Peter" in contents
+        assert "5'500" in contents
+        assert "5 500" in contents
+        assert "5,500" in contents
+        assert "Müller Hans" in contents
+        assert "2'200" in contents
+        assert "2 200" in contents
+        assert "2,200" in contents
+
+        # ... proporz
+        mock.reset_mock()
+        notification = EmailNotification()
+        notification.trigger(request, proporz)
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            (
+                'Election selon le système proportionnel - '
+                'Nouveaux résultats intermédiaires'
+            ),
+            'Elecziun da proporz - Novs resultats intermediars',
+            'Proporzwahl - Neue Zwischenresultate',
+            'Proporzwahl - New intermediate results',
+            'Proporzwahl - Nuovi risultati intermedi'
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "11 of 12" in contents
+        assert "11 da 12" in contents
+        assert "11 di 12" in contents
+        assert "11 de 12" in contents
+        assert "11 von 1" in contents
+        assert "FDP" in contents
+        assert "7'700" in contents
+        assert "7 700" in contents
+        assert "7,700" in contents
+        assert "SP" in contents
+        assert "6'600" in contents
+        assert "6 600" in contents
+        assert "6,600" in contents
+
+        # Final results
+        majorz.total_entities = 11
+        proporz.total_entities = 11
+        for candidate in session.query(Candidate).filter_by(candidate_id='1'):
+            candidate.elected = True
+        for list_ in session.query(List).filter_by(list_id='1'):
+            list_.number_of_mandates = 1
+
+        # ... majorz
+        mock.reset_mock()
+        notification = EmailNotification()
+        notification.trigger(request, majorz)
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            'Election selon le système majoritaire - Résultats finaux',
+            'Elecziun da maiorz - Resultats finals',
+            'Elezione secondo il sistema maggioritario - Risultati finali',
+            'Majorzwahl - Final results',
+            'Majorzwahl - Schlussresultate'
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "Maier Peter" in contents
+        assert "Müller Hans" not in contents
+        assert "49.83 %" in contents
+
+        # ... proporz
+        mock.reset_mock()
+        notification = EmailNotification()
+        notification.trigger(request, proporz)
+        assert sorted([call[2]['subject'] for call in mock.mock_calls]) == [
+            'Election selon le système proportionnel - Résultats finaux',
+            'Elecziun da proporz - Resultats finals',
+            'Proporzwahl - Final results',
+            'Proporzwahl - Risultati finali',
+            'Proporzwahl - Schlussresultate'
+        ]
+        contents = ''.join(call[2]['content'] for call in mock.mock_calls)
+        assert "FDP" in contents
+        assert "<span>1</span>" in contents
+        assert "Maier Peter" in contents
+        assert "Müller Hans" not in contents
+        assert "49.83 %" in contents
 
 
 def test_sms_notification(request, election_day_app, session):
