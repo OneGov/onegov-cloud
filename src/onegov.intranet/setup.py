3c7a05e14cf591ea87f48068741ae14e3fbe4283
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2017-12-29 10:11:11 +0100

    Requires Python 3.6

--- src/onegov.intranet/setup.py
+++ src/onegov.intranet/setup.py
@@ -31,6 +31,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
+    python_requires='>=3.6',
     install_requires=[
         'onegov.core',
         'onegov.org',
@@ -50,8 +51,8 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
     ]
