8093acfff63b45c5cdc07e5ba6e78aa0675af039
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-01-30 15:51:00 +0100

    Adds a street update cronjob

---
+++ src/onegov.winterthur/onegov/winterthur/cronjobs.py
@@ -0,0 +1,7 @@
+from onegov.winterthur.app import WinterthurApp
+from onegov.winterthur.collections import AddressCollection
+
+
+@WinterthurApp.cronjob(hour=15, minute=50, timezone='Europe/Zurich')
+def update_streets_directory(request):
+    AddressCollection(request.app.session()).update()
