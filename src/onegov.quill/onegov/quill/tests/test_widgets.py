31d735c7851f11d1f2eb2844cf9db39a87e537b6
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-02-07 10:34:20 +0100

    Fix tests

--- src/onegov.quill/onegov/quill/tests/test_widgets.py
+++ src/onegov.quill/onegov/quill/tests/test_widgets.py
@@ -7,16 +7,16 @@ def test_widget_initalization():
     input = QuillInput()
     assert input.formats == ["'bold'", "'italic'", "'list'"]
     assert input.toolbar == [
-        "'bold'", "'italic'", "{list: 'ordered'}", "{list: 'bullet'}"
+        "'bold'", "'italic'", "{'list': 'ordered'}", "{'list': 'bullet'}"
     ]
 
     input = QuillInput(tags=['strong', 'ul'])
     assert input.formats == ["'bold'", "'list'"]
-    assert input.toolbar == ["'bold'", "{list: 'bullet'}"]
+    assert input.toolbar == ["'bold'", "{'list': 'bullet'}"]
 
     input = QuillInput(tags=['em', 'ol'])
     assert input.formats == ["'italic'", "'list'"]
-    assert input.toolbar == ["'italic'", "{list: 'ordered'}"]
+    assert input.toolbar == ["'italic'", "{'list': 'ordered'}"]
 
     input = QuillInput(tags=['strong'])
     assert input.formats == ["'bold'"]
@@ -38,11 +38,13 @@ def test_widget_render():
     assert input.container_id in text
     assert input.input_id in text
     assert "['bold', 'italic', 'list']" in text
-    assert "['bold', 'italic', {list: 'ordered'}, {list: 'bullet'}]}" in text
+    assert (
+        "['bold', 'italic', {'list': 'ordered'}, {'list': 'bullet'}]}"
+    ) in text
 
     input = QuillInput(tags=['em', 'ul'])
     text = input(field)
     assert input.container_id in text
     assert input.input_id in text
     assert "['italic', 'list']" in text
-    assert "['italic', {list: 'bullet'}]}" in text
+    assert "['italic', {'list': 'bullet'}]}" in text
