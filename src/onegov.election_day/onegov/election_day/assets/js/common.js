8eedeea3de43d9fca6a056893edefdf89411c137
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-03-12 11:22:30 +0100

    Move table totals and buttons to the foot of the table

--- src/onegov.election_day/onegov/election_day/assets/js/common.js
+++ src/onegov.election_day/onegov/election_day/assets/js/common.js
@@ -2,10 +2,13 @@
 $(document).foundation();
 
 // collapse tables whising to be collapsible
-$('.collapsible.collapsed tbody tr:not(.total):not(.sticky-row):not(.more)').hide();
+$('.collapsible.collapsed tbody tr:not(.sticky-row):not(.more)').hide();
+$('.collapsible.collapsed tfoot tr:not(.more)').hide();
 $('.collapsible .more, .collapsible .less').click(function() {
     $(this).parents('table').toggleClass('collapsed');
-    $(this).parents('table').children('tbody').children('tr:not(.total):not(.sticky-row)').toggle();
+    $(this).parents('table').children('tbody').children('tr:not(.sticky-row)').toggle();
+    $(this).parents('table').children('tfoot').children('tr.less').toggle();
+    $(this).parents('table').children('tfoot').children('tr.more').toggle();
 });
 
 // sort tables wishing to be sorted
