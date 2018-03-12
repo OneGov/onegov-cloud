8540b71264c080e3479e445f2c24877b47d16654
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-03-12 12:56:50 +0100

    Merge branch 'v1.12'

--- src/onegov.gazette/setup.py
+++ src/onegov.gazette/setup.py
@@ -42,6 +42,7 @@ setup(
         'onegov.form',
         'onegov.foundation',
         'onegov.notice>=0.6.0',
+        'onegov.pdf>=0.3.3',
         'onegov.quill>=0.2.3',
         'onegov.shared',
         'onegov.user>=0.17.0',
