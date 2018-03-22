97f48a955a77b90a61320e9d109b72f9a39c8f5b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-03-22 13:17:40 +0100

    Onegov Onboarding Release 0.4.5

--- src/onegov.onboarding/HISTORY.rst
+++ src/onegov.onboarding/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.4.5 (2018-03-22)
+~~~~~~~~~~~~~~~~~~~
 
 - Fixes url in zulip chat.
   [href]
--- src/onegov.onboarding/setup.py
+++ src/onegov.onboarding/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.onboarding'
 description = (
     'Onboarding for OneGov Cloud applications.'
 )
-version = '0.4.4'
+version = '0.4.5'
 
 
 def get_long_description():
