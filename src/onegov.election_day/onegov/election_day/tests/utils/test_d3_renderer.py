67ad6098cdd989ba5b529a70008826e916ea8b6c
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-03-27 14:49:24 +0200

    Add tests

--- src/onegov.election_day/onegov/election_day/tests/utils/test_d3_renderer.py
+++ src/onegov.election_day/onegov/election_day/tests/utils/test_d3_renderer.py
@@ -1,4 +1,8 @@
 from base64 import b64encode
+from datetime import date
+from onegov.ballot import Election
+from onegov.ballot import ElectionCompound
+from onegov.ballot import Vote
 from onegov.election_day import _
 from onegov.election_day.utils.d3_renderer import D3Renderer
 from unittest.mock import patch, MagicMock
@@ -96,3 +100,56 @@ def test_d3_renderer_get_chart(election_day_app):
         generator.get_chart('map', 'pdf', data).read().decode() == 'PDF'
         generator.get_map('pdf', data, 2015).read().decode() == 'PDF'
         assert post.call_args[0] == ('http://localhost:1337/d3/pdf',)
+
+
+def test_d3_renderer_get_charts(election_day_app):
+    election = Election(
+        title="Election",
+        domain='federation',
+        date=date(2011, 1, 1),
+    )
+    compound = ElectionCompound(
+        title="ElectionCompound",
+        domain='canton',
+        date=date(2011, 1, 1),
+    )
+    vote = Vote(
+        title="Vote",
+        domain='federation',
+        date=date(2011, 1, 1),
+    )
+
+    session = election_day_app.session()
+    session.add(election)
+    session.add(compound)
+    session.add(vote)
+
+    generator = D3Renderer(election_day_app)
+
+    assert generator.get_lists_chart(election, 'svg') is None
+    assert generator.get_lists_chart(compound, 'svg') is None
+    assert generator.get_lists_chart(vote, 'svg') is None
+
+    assert generator.get_candidates_chart(election, 'svg') is None
+    assert generator.get_candidates_chart(compound, 'svg') is None
+    assert generator.get_candidates_chart(vote, 'svg') is None
+
+    assert generator.get_connections_chart(election, 'svg') is None
+    assert generator.get_connections_chart(compound, 'svg') is None
+    assert generator.get_connections_chart(vote, 'svg') is None
+
+    assert generator.get_party_strengths_chart(election, 'svg') is None
+    assert generator.get_party_strengths_chart(compound, 'svg') is None
+    assert generator.get_party_strengths_chart(vote, 'svg') is None
+
+    assert generator.get_lists_panachage_chart(election, 'svg') is None
+    assert generator.get_lists_panachage_chart(compound, 'svg') is None
+    assert generator.get_lists_panachage_chart(vote, 'svg') is None
+
+    assert generator.get_parties_panachage_chart(election, 'svg') is None
+    assert generator.get_parties_panachage_chart(compound, 'svg') is None
+    assert generator.get_parties_panachage_chart(vote, 'svg') is None
+
+    assert generator.get_map_chart(election, 'svg') is None
+    assert generator.get_map_chart(compound, 'svg') is None
+    assert generator.get_map_chart(vote, 'svg') is None
--- src/onegov.election_day/onegov/election_day/tests/utils/test_filenames.py
+++ src/onegov.election_day/onegov/election_day/tests/utils/test_filenames.py
@@ -2,6 +2,7 @@ from datetime import date
 from freezegun import freeze_time
 from onegov.ballot import Ballot
 from onegov.ballot import Election
+from onegov.ballot import ElectionCompound
 from onegov.ballot import Vote
 from onegov.election_day.utils import pdf_filename
 from onegov.election_day.utils import svg_filename
@@ -14,30 +15,33 @@ def test_pdf_filename(session):
             domain='federation',
             date=date(2011, 1, 1),
         )
+        compound = ElectionCompound(
+            title="ElectionCompound",
+            domain='canton',
+            date=date(2011, 1, 1),
+        )
         vote = Vote(
             title="Vote",
             domain='federation',
             date=date(2011, 1, 1),
         )
         session.add(election)
+        session.add(compound)
         session.add(vote)
         session.flush()
 
+        ts = 1388577600
         he = '4b9e99d2bd5e48d9a569e5f82175d1d2ed59105f8d82a12dc51b673ff12dc1f2'
+        assert pdf_filename(election, 'de') == f'election-{he}.{ts}.de.pdf'
+        assert pdf_filename(election, 'rm') == f'election-{he}.{ts}.rm.pdf'
+
+        hc = '2ef359817c8f8a7354e201f891cd7c11a13f4e025aa25239c3ad0cabe58bc49b'
+        assert pdf_filename(compound, 'de') == f'election-{hc}.{ts}.de.pdf'
+        assert pdf_filename(compound, 'rm') == f'election-{hc}.{ts}.rm.pdf'
+
         hv = 'ab274474a6aa82c100dddca63977facb556f66f489fb558c044a456f9ba919ce'
-        template = '{name}-{hash}.1388577600.{locale}.pdf'
-        assert pdf_filename(election, 'de') == template.format(
-            name='election', hash=he, locale='de'
-        )
-        assert pdf_filename(election, 'rm') == template.format(
-            name='election', hash=he, locale='rm'
-        )
-        assert pdf_filename(vote, 'de') == template.format(
-            name='vote', hash=hv, locale='de'
-        )
-        assert pdf_filename(vote, 'rm') == template.format(
-            name='vote', hash=hv, locale='rm'
-        )
+        assert pdf_filename(vote, 'de') == f'vote-{hv}.{ts}.de.pdf'
+        assert pdf_filename(vote, 'rm') == f'vote-{hv}.{ts}.rm.pdf'
 
 
 def test_svg_filename(session):
@@ -45,6 +49,11 @@ def test_svg_filename(session):
         election = Election(
             title="Election",
             domain='federation',
+            date=date(2011, 1, 1),
+        )
+        compound = ElectionCompound(
+            title="ElectionCompound",
+            domain='canton',
             date=date(2011, 1, 1),
         )
         vote = Vote(
@@ -55,36 +64,39 @@ def test_svg_filename(session):
         ballot = Ballot(type="proposal")
         vote.ballots.append(ballot)
         session.add(election)
+        session.add(compound)
         session.add(vote)
         session.flush()
 
+        ts = 1388577600
         he = '4b9e99d2bd5e48d9a569e5f82175d1d2ed59105f8d82a12dc51b673ff12dc1f2'
+        assert svg_filename(election, 'chart') == \
+            f'election-{he}.{ts}.chart.any.svg'
+        assert svg_filename(election, 'chart', 'de') == \
+            f'election-{he}.{ts}.chart.de.svg'
+        assert svg_filename(election, 'chart', 'rm') == \
+            f'election-{he}.{ts}.chart.rm.svg'
+
+        hc = '2ef359817c8f8a7354e201f891cd7c11a13f4e025aa25239c3ad0cabe58bc49b'
+        assert svg_filename(compound, 'chart') == \
+            f'election-{hc}.{ts}.chart.any.svg'
+        assert svg_filename(compound, 'chart', 'de') == \
+            f'election-{hc}.{ts}.chart.de.svg'
+        assert svg_filename(compound, 'chart', 'rm') == \
+            f'election-{hc}.{ts}.chart.rm.svg'
+
         hv = 'ab274474a6aa82c100dddca63977facb556f66f489fb558c044a456f9ba919ce'
-        template = '{name}-{hash}.1388577600.chart.{locale}.svg'
-        assert svg_filename(election, 'chart') == template.format(
-            name='election', hash=he, locale='any'
-        )
-        assert svg_filename(election, 'chart', 'de') == template.format(
-            name='election', hash=he, locale='de'
-        )
-        assert svg_filename(election, 'chart', 'rm') == template.format(
-            name='election', hash=he, locale='rm'
-        )
-        assert svg_filename(vote, 'chart') == template.format(
-            name='vote', hash=hv, locale='any'
-        )
-        assert svg_filename(vote, 'chart', 'de') == template.format(
-            name='vote', hash=hv, locale='de'
-        )
-        assert svg_filename(vote, 'chart', 'rm') == template.format(
-            name='vote', hash=hv, locale='rm'
-        )
-        assert svg_filename(ballot, 'chart') == template.format(
-            name='ballot', hash=str(ballot.id), locale='any'
-        )
-        assert svg_filename(ballot, 'chart', 'de') == template.format(
-            name='ballot', hash=str(ballot.id), locale='de'
-        )
-        assert svg_filename(ballot, 'chart', 'rm') == template.format(
-            name='ballot', hash=str(ballot.id), locale='rm'
-        )
+        assert svg_filename(vote, 'chart') == \
+            f'vote-{hv}.{ts}.chart.any.svg'
+        assert svg_filename(vote, 'chart', 'de') == \
+            f'vote-{hv}.{ts}.chart.de.svg'
+        assert svg_filename(vote, 'chart', 'rm') == \
+            f'vote-{hv}.{ts}.chart.rm.svg'
+
+        hb = str(ballot.id)
+        assert svg_filename(ballot, 'chart') == \
+            f'ballot-{hb}.{ts}.chart.any.svg'
+        assert svg_filename(ballot, 'chart', 'de') == \
+            f'ballot-{hb}.{ts}.chart.de.svg'
+        assert svg_filename(ballot, 'chart', 'rm') == \
+            f'ballot-{hb}.{ts}.chart.rm.svg'
