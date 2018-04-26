a13dedbc2ba78ade3eab4a0c6d92a08b138cafa5
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-04-26 13:01:02 +0200

    Onegov Ballot Release 3.5.0

--- src/onegov.ballot/HISTORY.rst
+++ src/onegov.ballot/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+3.5.0 (2018-04-26)
+~~~~~~~~~~~~~~~~~~~
 
 - Adds a distinct property to elections.
   [msom]
--- src/onegov.ballot/setup.py
+++ src/onegov.ballot/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.ballot'
 description = (
     'Models ballot results for OneGov.'
 )
-version = '3.4.2'
+version = '3.5.0'
 
 
 def get_long_description():
