5142b0913cc9f830c9f91262b97a727db9144dd5
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-05-18 09:58:58 +0200

    Gets the hierarchy out of the vollzug export

--- src/onegov.winterthur/scripts/__main__.py
+++ src/onegov.winterthur/scripts/__main__.py
@@ -3,16 +3,27 @@ import click
 from .vollzug import transform_vollzug
 
 
+POST_MORTEM = False
+
+
 @click.group()
-def cli():
-    pass
+@click.option('--pdb', default=False, is_flag=True)
+def cli(pdb):
+    global POST_MORTEM
+    POST_MORTEM = pdb
 
 
 @cli.command(name='transform-vollzug')
 @click.argument('path', type=click.Path(exists=True, file_okay=False))
 @click.argument('prefix')
-def transform_vollzug_cli(path, prefix):
-    return transform_vollzug(path, prefix)
+@click.option('--output', type=click.Path(), default=None)
+def transform_vollzug_cli(path, prefix, output):
+    try:
+        return transform_vollzug(path, prefix, output)
+    except Exception:
+        if POST_MORTEM:
+            import pdb; pdb.post_mortem()  # noqa
+        raise
 
 
 # run cli handler when running this thorugh pyhton -m
--- src/onegov.winterthur/scripts/vollzug.py
+++ src/onegov.winterthur/scripts/vollzug.py
@@ -15,13 +15,15 @@ from pathlib import Path
 @attr.s(auto_attribs=True)
 class Institution(object):
     name: str
-    department: str
     branch: str
-    chapter: str
+    department: str
     address: str
     phone: str = None
     email: str = None
     website: str = None
+
+    topics: set = attr.Factory(set)
+    chapters: set = attr.Factory(set)
     tasks: set = attr.Factory(set)
 
     @property
@@ -30,6 +32,48 @@ class Institution(object):
             self.name,
             self.branch
         ) if part)
+
+    @classmethod
+    def from_description(cls, uid, description):
+
+        def clean(description):
+            return description.replace('Tel.:', '')
+
+        def split(description):
+            for desc in description.split('</td'):
+                if html_to_text(desc).strip():
+                    yield desc
+
+        for desc in split(description):
+
+            lines = html_to_text(desc).split('\n\n')
+
+            if len(lines) < 6:
+                print(f"Could not parse a description for {uid}:")
+                print(desc)
+                continue
+
+            department = lines.pop(0).strip()
+            name = lines.pop(0).strip()
+            branch = lines.pop(0).strip()
+            website = lines.pop(-1).strip()
+            email = lines.pop(-1).strip()
+            phone = lines.pop(-1).strip()
+            address = '\n'.join(lines)
+
+            if re.search(r'[0-9]+', branch):
+                address = branch + '\n' + address
+                branch = ''
+
+            yield cls(
+                name=name,
+                branch=branch,
+                department=department,
+                website=website,
+                email=email,
+                phone=phone,
+                address=address
+            )
 
 
 def get_metadata(title, structure):
@@ -63,12 +107,13 @@ def load_files(path, prefix):
     chapters, links, institutions = None, None, None
 
     for p in Path(path).glob(f'{prefix}*'):
-        if 'chapter_vollzugsbehoerden' in p.name:
+        if 'chapter_theme' in p.name:
             chapters = CSVFile(p.open('rb'), (
-                'uid',
-                'chapter'
+                'chapter_uid',
+                'chapter',
+                'topic',
+                'topic_pid'
             ))
-            continue
 
         if 'chapter_mm_vollzugsbehoerden' in p.name:
             links = CSVFile(p.open('rb'), (
@@ -91,78 +136,67 @@ def load_files(path, prefix):
     return chapters, links, institutions
 
 
-def parse_description(uid, description):
-    description = description.replace('Tel.:', '')
-
-    for desc in description.split('</td>'):
-        lines = html_to_text(desc).split('\n\n')
-
-        if len(lines) <= 1:
-            continue
-
-        if len(lines) < 6:
-            print(f"Could not parse a description for {uid}:")
-            print(desc)
-            continue
-
-        department = lines.pop(0).strip()
-        name = lines.pop(0).strip()
-        branch = lines.pop(0).strip()
-        website = lines.pop(-1).strip()
-        email = lines.pop(-1).strip()
-        phone = lines.pop(-1).strip()
-        address = '\n'.join(lines)
-
-        if re.search(r'[0-9]+', branch):
-            address = branch + '\n' + address
-            branch = ''
-
-        yield {
-            'department': department,
-            'name': name,
-            'branch': branch,
-            'website': website,
-            'email': email,
-            'phone': phone,
-            'address': address
-        }
-
-
 def get_institutions(path, prefix):
     chapters, links, institutions = load_files(path, prefix)
 
-    chapters = {c.uid: c.chapter for c in chapters.lines}
+    # topics by chapter
+    topics = {c.chapter: c.topic for c in chapters.lines}
+
+    # chapters by institution id
+    chapters = {c.chapter_uid: c.chapter for c in chapters.lines}
     chapters = {l.uid_local: chapters.get(l.uid_foreign) for l in links.lines}
 
+    # resulting institutions
     result = {}
 
-    for record in institutions.lines:
-        task = record.institution
+    for r in institutions.lines:
+        task = r.institution
 
-        for data in parse_description(record.uid, record.description):
-            data['chapter'] = chapters[record.uid]
+        for i in Institution.from_description(r.uid, r.description):
+            i = result[i.title] = result.get(i.title, i)
+            i.tasks.add(task)
 
-            institution = Institution(**data)
+            if chapters[r.uid]:
+                i.chapters.add(chapters[r.uid])
 
-            if institution.title in result:
-                institution = result[institution.title]
-            else:
-                result[institution.title] = institution
+            if chapters[r.uid] in topics:
+                i.topics.add(topics[chapters[r.uid]])
 
-            institution.tasks.add(task)
+    # hierarchy of categories
+    hierarchy = []
 
-    return list(result.values())
+    for i in institutions.lines:
+        task = i.institution
+        chapter = chapters[i.uid]
+
+        if task and chapter and chapter in topics:
+            topic = topics[chapter]
+            hierarchy.append(f'{topic} :: {chapter} :: {task}')
+
+    institutions = sorted([i for i in result.values()], key=lambda i: i.title)
+    return sorted(hierarchy), institutions
 
 
-def transform_vollzug(path, prefix, output_file='vollzug.zip'):
-    institutions = get_institutions(path, prefix)
+def transform_vollzug(path, prefix, output_file):
+    hierarchy, institutions = get_institutions(path, prefix)
 
-    chapters = sorted(list({i.chapter for i in institutions}))
-    chapters = textwrap.indent(
-        '\n'.join(f'[ ] {chapter}' for chapter in chapters),
-        ' ' * 16,
-        lambda line: not chapters[0].endswith(line.split(']')[-1].strip())
-    )
+    def unique_values(values, attr):
+        unique = set()
+
+        for value in values:
+            unique.update(getattr(value, attr))
+
+        return sorted(list(unique))
+
+    def as_choices(values, indent=16):
+        return textwrap.indent(
+            '\n'.join(f'[ ] {v}' for v in values),
+            ' ' * 16,
+            lambda line: not values[0].endswith(line.split(']')[-1].strip())
+        )
+
+    topics = as_choices(unique_values(institutions, 'topics'))
+    chapters = as_choices(unique_values(institutions, 'chapters'))
 
     metadata = get_metadata(
         title='Vollzugsbehörden',
@@ -179,7 +213,10 @@ def transform_vollzug(path, prefix, output_file='vollzug.zip'):
             Webseite = ___
 
             # Kategorie
-            Vollzugsbereich =
+            Themen =
+                {topics}
+
+            Vollzugsbereiche =
                 {chapters}
 
             Vollzugsaufgaben = ...[16]
@@ -194,7 +231,8 @@ def transform_vollzug(path, prefix, output_file='vollzug.zip'):
             'Kontakt/E-Mail': i.email,
             'Kontakt/Telefon': i.phone,
             'Kontakt/Webseite': i.website,
-            'Kategorie/Vollzugsbereich': [i.chapter],
+            'Kategorie/Themen': list(i.topics),
+            'Kategorie/Vollzugsbereiche': list(i.chapters),
             'Kategorie/Vollzugsaufgaben': '\n'.join(
                 f'- {t}' for t in sorted(list(i.tasks))
             )
@@ -208,5 +246,8 @@ def transform_vollzug(path, prefix, output_file='vollzug.zip'):
 
         with (Path(directory) / 'data.json').open('w') as f:
             f.write(json.dumps(data))
+
+        with (Path(directory) / 'hierarchy.json').open('w') as f:
+            f.write(json.dumps(hierarchy))
 
         shutil.make_archive(output_file.split('.')[0], 'zip', directory)
