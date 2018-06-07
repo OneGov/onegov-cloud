16f5dd995dd87b84a4185e0e3b6296ffba16bc4c
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-06-07 13:41:51 +0200

    Switches to the custom winterthur map

--- src/onegov.winterthur/onegov/winterthur/assets/js/winterthur.js
+++ src/onegov.winterthur/onegov/winterthur/assets/js/winterthur.js
@@ -1,3 +1,14 @@
+function spawnDefaultMap(target, options) {
+    var service = 'http://stadtplan.winterthur.ch/wms/Hintergrundkarte_LK_AV_Situationsplan?';
+    var layers = 'Hintergrundkarte_LK_AV_Situationsplan';
+    var projection = L.CRS.EPSG4326
+
+    var map = L.map(target, $.extend(options, {'crs': projection}));
+    var tiles = L.tileLayer.wms(service, {layers: layers}).addTo(map);
+
+    return map
+}
+
 $(document).ready(function() {
     $('.field-display dd').html(function() {
         return $(this).html().split('<br>').map(function(html) {
