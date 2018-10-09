ce123d5ce3b413e8c0483969ca389c64976cd7cf
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-10-09 09:19:00 +0200

    Moves importscan to onegov-testing

--- src/onegov.election_day/onegov/election_day/tests/conftest.py
+++ src/onegov.election_day/onegov/election_day/tests/conftest.py
@@ -9,30 +9,6 @@ from onegov.user import User
 from onegov_testing.utils import create_app
 
 
-def pytest_addoption(parser):
-    """ Adds a command line argument to scans all the onegov.* sources to
-    make sure that the database tables are created.
-
-    Set this option if you run into  sqlalchemy erorrs like:
-        "relation XXX does not exist"
-
-    Run it like this:
-        py.test src/onegov.election-day/onegov/election_day/tests/
-            -k test_view_clear_results
-            --import-scan
-
-    """
-    parser.addoption('--import-scan', action="store_true")
-
-
-def pytest_cmdline_main(config):
-    option = config.getoption('--import-scan')
-    if option:
-        import importscan
-        import onegov
-        importscan.scan(onegov, ignore=['.test', '.tests'])
-
-
 @pytest.fixture(scope='session')
 def election_day_password():
     # only hash the password for the test users once per test session
