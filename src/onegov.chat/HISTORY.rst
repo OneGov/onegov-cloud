05d6b35920d7419fd5ba05dcea2d55938707c36f
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-10-10 10:15:57 +0200

    Onegov Chat Release 0.1.2

--- src/onegov.chat/HISTORY.rst
+++ src/onegov.chat/HISTORY.rst
@@ -1,6 +1,9 @@
 Changelog
 ---------
 
+0.1.2 (2018-10-10)
+~~~~~~~~~~~~~~~~~~~~~
+
 - Uses session instead of request for bound_messages.
   [href]
 
--- src/onegov.chat/setup.py
+++ src/onegov.chat/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.chat'
 description = (
     'Chat messages for OneGov Cloud.'
 )
-version = '0.1.1'
+version = '0.1.2'
 
 
 def get_long_description():
