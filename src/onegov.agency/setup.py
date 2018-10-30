69d8925f851a7ca080d4e679da4084e126841dcf
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-10-30 09:26:50 +0100

    Onegov Agency Release 0.0.2

--- src/onegov.agency/setup.py
+++ src/onegov.agency/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.agency'
 description = (
     'Administrative units and relationships for administrative directories.'
 )
-version = '0.0.1'
+version = '0.0.2'
 
 
 def get_long_description():
