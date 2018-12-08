a530b61765d2eb2c3040a7a94bb992ee1e1b4beb
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-12-08 11:00:08 +0100

    Onegov Event Release 1.1.0

--- src/onegov.event/HISTORY.rst
+++ src/onegov.event/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+1.1.0 (2018-12-08)
+~~~~~~~~~~~~~~~~~~~
 
 - Allows to filter occurrences by locations.
   [msom]
--- src/onegov.event/setup.py
+++ src/onegov.event/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.event'
 description = (
     'Calendar of events for OneGov.'
 )
-version = '1.0.0'
+version = '1.1.0'
 
 
 def get_long_description():
