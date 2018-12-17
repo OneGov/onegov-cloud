5370614b184f9149afc3799540ca3d4847520f12
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2018-12-17 15:16:41 +0100

    Extends test

--- src/onegov.swissvotes/onegov/swissvotes/tests/test_views_principal.py
+++ src/onegov.swissvotes/onegov/swissvotes/tests/test_views_principal.py
@@ -1,8 +1,17 @@
+from onegov.swissvotes.models import TranslatablePage
+from transaction import commit
 from webtest import TestApp as Client
 
 
 def test_view_home(swissvotes_app):
     client = Client(swissvotes_app)
-    home = client.get('/').follow()
+    home = client.get('/').maybe_follow()
     assert "Startseite" in home
     assert home.request.url.endswith('page/home')
+
+    session = swissvotes_app.session()
+    session.delete(session.query(TranslatablePage).filter_by(id='home').one())
+    commit()
+
+    home = client.get('/').maybe_follow()
+    assert not home.request.url.endswith('page/home')
