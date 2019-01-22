b5d20f7209bcc1c50994e262199b2d45d0b5b753
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2019-01-22 11:05:56 +0100

    Loosen ticket attachment restrictions

--- src/onegov.org/onegov/org/forms/ticketnotes.py
+++ src/onegov.org/onegov/org/forms/ticketnotes.py
@@ -3,7 +3,6 @@ from onegov.form import Form
 from onegov.form.fields import UploadFileWithORMSupport
 from onegov.form.filters import strip_whitespace
 from onegov.form.validators import FileSizeLimit
-from onegov.form.validators import WhitelistedMimeType
 from onegov.org import _
 from wtforms import TextAreaField
 from wtforms import validators
@@ -21,16 +20,7 @@ class TicketNoteForm(Form):
     file = UploadFileWithORMSupport(
         label=_("Attachment"),
         file_class=MessageFile,
-        render_kw={'accept': '.pdf,.gif,.jpeg,.jpg,.png,.svg,.txt'},
         validators=[
             validators.Optional(),
-            WhitelistedMimeType({
-                'application/pdf',
-                'image/gif',
-                'image/jpeg',
-                'image/png',
-                'image/svg+xml',
-                'text/plain',
-            }),
-            FileSizeLimit(5 * 1000 * 1000)
+            FileSizeLimit(10 * 1000 * 1000)
         ])
