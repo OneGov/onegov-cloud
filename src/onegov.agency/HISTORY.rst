7777f1a82413ac91eca2bc782f3e4a85079b314c
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-02-05 16:41:08 +0100

    Onegov Agency Release 1.8.1

--- src/onegov.agency/HISTORY.rst
+++ src/onegov.agency/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+1.8.1 (2019-02-05)
+~~~~~~~~~~~~~~~~~~~~
 
 - Fixes vCard with special characters not working on Windows.
   [msom]
--- src/onegov.agency/setup.py
+++ src/onegov.agency/setup.py
@@ -4,7 +4,7 @@ name = 'onegov.agency'
 description = (
     'Administrative units and relationships for administrative directories.'
 )
-version = '1.8.0'
+version = '1.8.1'
 
 
 def get_long_description():
