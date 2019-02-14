f88cf51653aff1aa0fcc7c22f0c4fb2ada42197d
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-02-14 14:41:49 +0100

    Onegov Swissvotes Release 0.0.31

--- src/onegov.swissvotes/onegov/swissvotes/theme/swissvotes_theme.py
+++ src/onegov.swissvotes/onegov/swissvotes/theme/swissvotes_theme.py
@@ -4,7 +4,7 @@ from onegov.foundation import BaseTheme
 
 class SwissvotesTheme(BaseTheme):
     name = 'onegov.swissvotes.foundation'
-    version = '0.0.30'
+    version = '0.0.31'
 
     @property
     def pre_imports(self):
