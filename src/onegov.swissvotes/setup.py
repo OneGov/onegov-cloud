fb7668f783dec776fdc9cbf43bd9b481cd5e78f9
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-02-27 09:58:54 +0100

    Onegov Swissvotes Release 0.0.37

--- src/onegov.swissvotes/setup.py
+++ src/onegov.swissvotes/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.swissvotes'
 description = 'Database for federal votes'
-version = '0.0.36'
+version = '0.0.37'
 
 
 def get_long_description():
