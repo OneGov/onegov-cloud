a6d9a4041aefc40e78f48b66dbf672cc04c0c6b1
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-03-13 14:49:31 +0100

    Onegov Swissvotes Release 0.0.45

--- src/onegov.swissvotes/HISTORY.rst
+++ src/onegov.swissvotes/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.0.45 (2019-03-13)
+~~~~~~~~~~~~~~~~~~~
 
 - Adds brief description attachments.
   [msom]
--- src/onegov.swissvotes/onegov/swissvotes/theme/swissvotes_theme.py
+++ src/onegov.swissvotes/onegov/swissvotes/theme/swissvotes_theme.py
@@ -4,7 +4,7 @@ from onegov.foundation import BaseTheme
 
 class SwissvotesTheme(BaseTheme):
     name = 'onegov.swissvotes.foundation'
-    version = '0.0.44'
+    version = '0.0.45'
 
     @property
     def pre_imports(self):
