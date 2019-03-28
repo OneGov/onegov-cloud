61eacbb3d39e60309340087fa0b9b8bba49bd22f
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2019-03-28 14:17:51 +0100

    Onegov Wtfs Release 0.0.5

--- src/onegov.wtfs/HISTORY.rst
+++ src/onegov.wtfs/HISTORY.rst
@@ -1,5 +1,7 @@
 Changelog
 ---------
+0.0.5 (2019-03-28)
+~~~~~~~~~~~~~~~~~~~
 
 - Adds datetime picker.
   [msom]
--- src/onegov.wtfs/setup.py
+++ src/onegov.wtfs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 name = 'onegov.wtfs'
 description = 'Tax form scanning app for the city of Winterthur'
-version = '0.0.4'
+version = '0.0.5'
 
 
 def get_long_description():
