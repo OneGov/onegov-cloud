2793ddb065a0ca778f50d6988e31d542d645245b
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2019-05-13 11:47:13 +0200

    Remove test-implementation detail

--- src/onegov.core/onegov/core/tests/test_datamanager.py
+++ src/onegov.core/onegov/core/tests/test_datamanager.py
@@ -9,13 +9,10 @@ def test_file_data_manager_commit(temporary_directory):
     data = 'data'.encode('utf-8')
     path = '{}/a.txt'.format(temporary_directory)
     FileDataManager.write_file(data, path)
-    manager = transaction.get()._resources[0]
-
-    assert manager.data == data
-    assert manager.path == path
     assert not os.path.exists(path)
 
     transaction.commit()
+
     assert os.path.exists(path)
     with open(path) as file:
         assert file.read() == 'data'
@@ -28,11 +25,8 @@ def test_file_data_manager_abort(temporary_directory):
     data = 'data'.encode('utf-8')
     path = '{}/b.txt'.format(temporary_directory)
     FileDataManager.write_file(data, path)
-    manager = transaction.get()._resources[0]
-
-    assert manager.data == data
-    assert manager.path == path
     assert not os.path.exists(path)
 
     transaction.abort()
+
     assert not os.path.exists(path)
