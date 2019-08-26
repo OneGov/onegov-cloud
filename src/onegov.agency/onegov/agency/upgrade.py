6579370b10f1fdc151b2404f00185384d59936f9
Lukas Burkhard <lukas.burkhard@seantis.ch>
2019-08-26 14:27:34 +0200

    Adds upgrade script

--- src/onegov.agency/onegov/agency/upgrade.py
+++ src/onegov.agency/onegov/agency/upgrade.py
@@ -2,3 +2,18 @@
 upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
 
 """
+from onegov.core.upgrade import upgrade_task
+from onegov.org.models import Organisation
+
+
+@upgrade_task("Add default values for page breaks of PDFs")
+def add_default_value_for_pagebreak_pdf(context):
+
+    """ Adds the elected candidates to the archived results,
+
+    """
+    session = context.session
+    if context.has_column('organisations', 'meta'):
+        for org in session.query(Organisation).all():
+            org.meta['page_break_on_level_root_pdf'] = 1
+            org.meta['page_break_on_level_orga_pdf'] = 1
