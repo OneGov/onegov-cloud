ca5315edf75e2dbdae6a7e87bfb9b32bff63b5b7
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2019-09-05 14:55:38 +0200

    Automatically retry LDAP connection

--- src/onegov/user/auth/clients/ldap.py
+++ src/onegov/user/auth/clients/ldap.py
@@ -1,6 +1,37 @@
 from attr import attrs, attrib
 from cached_property import cached_property
+from contextlib import suppress
 from ldap3 import Connection, Server, NONE
+from ldap3.core.exceptions import LDAPCommunicationError
+from time import sleep
+
+
+def auto_retry(fn, max_tries=3, pause=0.1):
+    """ Retries the decorated function if a LDAP connection error occurs, up
+    to a given set of retries, using linear backoff.
+
+    """
+    tried = 0
+
+    def retry(self, *args, **kwargs):
+        nonlocal tried
+
+        try:
+            return fn(self, *args, **kwargs)
+        except LDAPCommunicationError:
+            tried += 1
+
+            if tried >= max_tries:
+                raise
+
+            sleep(tried * 0.1)
+
+            with suppress(ValueError):
+                self.try_configuration()
+
+            return retry(self, *args, **kwargs)
+
+    return retry
 
 
 @attrs()
@@ -48,6 +79,7 @@ class LDAPClient():
         if not self.connection.rebind(self.username, self.password):
             raise ValueError(f"Failed to connect to {self.url}")
 
+    @auto_retry
     def search(self, query, attributes={}):
         """ Runs an LDAP query against the server and returns a dictionary
         with the distinguished name as key and the given attributes as values
