ed4a1b6fdb46e22485d3d5dea85d4a7449f6525a
Lukas <lukas.burkhard@seantis.ch>
2019-09-05 15:36:07 +0200

    initial commit for query and test

--- src/onegov/election_day/utils/election/lists.py
+++ src/onegov/election_day/utils/election/lists.py
@@ -1,8 +1,36 @@
 from collections import OrderedDict
-from onegov.ballot import List
+from onegov.ballot import List, Candidate
 from onegov.election_day import _
 from sqlalchemy import desc
 from sqlalchemy.orm import object_session
+
+
+def get_aggregated_list_results(election, session):
+    if election.type == 'majorz':
+        return {}
+
+    candidates_list = []
+    total_accounted_votes = election.accounted_votes
+
+    result = get_list_results(election, session)
+    result = result.join(
+        Candidate.first_name,
+
+    ).all()
+    print(result)
+
+
+    return {
+        'results': [
+            {
+                'name': lst.name,
+                'number_of_mandates': lst.number_of_mandates,
+                'votes': lst[1],
+                'perc_to_total_votes': lst[1] / total_accounted_votes,
+                'candidates': candidates_list,
+            } for lst in result
+        ]
+    }
 
 
 def get_list_results(election, session):
