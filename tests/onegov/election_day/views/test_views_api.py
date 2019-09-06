64b0a7cf1a9c1c0ddc1b1d8715fb4048f1a945e2
Lukas <lukas.burkhard@seantis.ch>
2019-09-06 14:50:28 +0200

    comment tests for now since done visually and with screen shots
    - we lack a good dataset at the moment

--- tests/onegov/election_day/views/test_views_api.py
+++ tests/onegov/election_day/views/test_views_api.py
@@ -13,29 +13,29 @@ from tests.onegov.election_day.common import import_wabstic_data
 #     module_path('tests.onegov.election_day',
 #                 'fixtures/WP-Dateien_NR2019_Zwischenresultate.tar.gz'),
 # ])
-def test_get_aggregated_list_results(election_day_app_sg, tar_file):
-    session = election_day_app_sg.session_manager.session()
-    session.add(
-        ProporzElection(
-            title='NR2019',
-            domain='federation',
-            date=date(2019, 10, 20),
-            number_of_mandates=12,
-            expats=True
-        )
-    )
-    session.flush()
-    election = session.query(Election).one()
-    data = get_aggregated_list_results(election, session)['results']
-    assert data == []
-
-    # Load results
-    # import_wabstic_data(
-    #     election, tar_file, election_day_app_sg.principal, expats=True
-    # )
-
-    print(json.dumps(data, indent=2))
-    assert False
+# def test_get_aggregated_list_results(election_day_app_sg, tar_file):
+#     session = election_day_app_sg.session_manager.session()
+#     session.add(
+#         ProporzElection(
+#             title='NR2019',
+#             domain='federation',
+#             date=date(2019, 10, 20),
+#             number_of_mandates=12,
+#             expats=True
+#         )
+#     )
+#     session.flush()
+#     election = session.query(Election).one()
+#     data = get_aggregated_list_results(election, session)['results']
+#     assert data == []
+#
+#     # Load results
+#     # import_wabstic_data(
+#     #     election, tar_file, election_day_app_sg.principal, expats=True
+#     # )
+#
+#     print(json.dumps(data, indent=2))
+#     assert False
 
 
 
