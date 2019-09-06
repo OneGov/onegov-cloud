c2fde98902b511a6d96ebb3cf0b71965053ccc22
Lukas <lukas.burkhard@seantis.ch>
2019-09-06 10:37:49 +0200

    Final test of query filtering for one election

--- tests/onegov/election_day/queries/aggregated_list_results.sql
+++ tests/onegov/election_day/queries/aggregated_list_results.sql
@@ -1,60 +1,80 @@
 WITH
 -- aggregated
-election_result_accounted_votes AS (
+agg_election_results AS (
   SELECT
+    election_id,
     sum(er.received_ballots) as received_ballots,
     sum(er.eligible_voters) as eligible_voters,
+
     sum(e.number_of_mandates * (
         er.received_ballots - er.blank_ballots - er.invalid_ballots
             - er.blank_votes - er.invalid_votes)) as accounted_votes
-
     FROM election_results er LEFT JOIN elections e on er.election_id = e.id
+    GROUP BY
+             er.election_id
 
-    WHERE er.election_id = 'erneuerungswahl-des-nationalrates-2'
-    GROUP BY er.election_id
     ),
 
 list_results AS (
     SELECT
-        array_agg(distinct l.id) as id,
-        array_agg(distinct l.number_of_mandates) as number_of_mandates,
-        array_agg(distinct l.name) as name,
-        sum(lr.votes) as total_votes
+        l.election_id,
+        l.id,
+        number_of_mandates,
+        name,
+        sum(lr.votes) as votes
     FROM lists l
     LEFT JOIN list_results lr ON l.id = lr.list_id
-    GROUP BY lr.list_id
+    GROUP BY
+            lr.list_id,
+            l.election_id,
+            l.id,
+            l.number_of_mandates,
+            l.name
 ),
+
 candidate_results as (
-    SELECT DISTINCT
-           array_agg(distinct c.list_id) as list_id,
-           array_agg(distinct c.family_name) as family_name,
-           array_agg(distinct c.first_name)as first_name,
+    SELECT
+           c.election_id,
+           c.list_id,
+           c.family_name,
+           c.first_name,
            sum(cr.votes) as candidate_votes
     FROM candidates c
     LEFT JOIN candidate_results cr on c.id = cr.candidate_id
-    GROUP BY cr.candidate_id    -- candidate has an election id, so is unique every election
+    GROUP BY
+            cr.candidate_id,
+            c.election_id,
+            c.list_id,
+            c.family_name,
+            c.first_name
 )
-SELECT * from election_result_accounted_votes;
--- SELECT
---     l.name,
---     l.total_votes,
---     l.number_of_mandates,
---     c.family_name,
---     c.first_name,
---     c.candidate_votes
---
---  from list_results l
--- LEFT JOIN candidate_results c ON l.id = c.list_id
--- ORDER BY
---          total_votes DESC ,
---          candidate_votes DESC
 
+-- SELECT * FROM candidate_results;
 
--- SELECT
---     lists.list_id,
---     lists.name as list_name,
---     c.party,
---     c.family_name,
---     c.first_name
--- FROM candidates as c RIGHT JOIN lists ON c.list_id = lists.id
--- WHERE lists.election_id = 'erneuerungswahl-des-nationalrates-2';
+SELECT
+    CASE
+        WHEN ar.accounted_votes < 0
+            THEN 0
+    ELSE ar.accounted_votes
+    END as accounted_votes,
+    l.election_id,
+    l.name,
+    l.votes,
+    l.number_of_mandates,
+    c.family_name,
+    c.first_name,
+    c.candidate_votes,
+    round(l.votes::DECIMAL / ar.accounted_votes, 1) as per_to_total_votes,
+    CASE WHEN l.votes = 0
+        THEN 0
+    ELSE
+        round(c.candidate_votes::DECIMAL / l.votes * 100, 1)
+    END as perc_to_list_votes
+
+FROM list_results l
+LEFT JOIN candidate_results c ON l.id = c.list_id
+LEFT JOIN agg_election_results ar ON l.election_id = ar.election_id
+ORDER BY
+    l.votes DESC,
+    c.candidate_votes DESC
+
