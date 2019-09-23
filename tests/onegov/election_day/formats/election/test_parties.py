d9eab72ab5fa177afbf365cb16e8615c6643f004
Lukas <lukas.burkhard@seantis.ch>
2019-09-23 11:13:58 +0200

    Adds test to evaluate error handling for invalid panachage headers

--- tests/onegov/election_day/formats/election/test_parties.py
+++ tests/onegov/election_day/formats/election/test_parties.py
@@ -1,11 +1,14 @@
 import tarfile
 from datetime import date
 from io import BytesIO
+
+import pytest
+from onegov.election_day import _
 from onegov.ballot import Election
 from onegov.ballot import ProporzElection
-from onegov.core.utils import module_path
 from onegov.election_day.formats import import_party_results
-from tests.onegov.election_day.common import get_tar_file_path
+from onegov.election_day.formats.common import FileImportError
+from tests.onegov.election_day.common import get_tar_file_path, print_errors
 
 
 def test_import_party_results_fixtures(session):
@@ -28,6 +31,7 @@ def test_import_party_results_fixtures(session):
             'Nationalratswahlen_2019_sesam-test_Parteien.csv').read()
 
     errors = import_party_results(election, BytesIO(csv), 'text/plain')
+    print_errors(errors)
     assert not errors
     total_votes = 1750
 
@@ -44,11 +48,12 @@ def test_import_party_results_fixtures(session):
         ('VERDA', total_votes, 0, 300),
     ]
 
-    # Validates if not somebody opened csv file with excel and converting
-    # 01 to 1, thus not matching panachage_results
+
     assert election.panachage_results
     for pana_r in election.panachage_results:
         # assert len(pana_r.target) > 10, 'target must be casted list.id'
+        if not pana_r.votes == 0:
+            print(pana_r.source, pana_r.target)
         assert pana_r.votes == 0, "Panachage votes from own list don't count"
 
 
@@ -95,6 +100,50 @@ def test_import_party_results(session):
         (2015, 'P1', '#123456', 5000, 10000, 1),
         (2015, 'P2', '#aabbcc', 5000, 10000, 0)
     ]
+
+
+@pytest.mark.parametrize('wrong_id,error', [
+    ('01', "Panachage results ids and id not consistent"),
+])
+def test_import_party_results_panachage_invalid_values(
+        session, wrong_id, error):
+
+    session.add(
+        ProporzElection(
+            title='election',
+            domain='canton',
+            date=date(2015, 10, 18),
+            number_of_mandates=6,
+        )
+    )
+    session.flush()
+    election = session.query(Election).one()
+
+    errors = import_party_results(
+        election,
+        BytesIO((
+                    '\n'.join((
+                        ','.join((
+                            'year',
+                            'total_votes',
+                            'id',
+                            'name',
+                            'color',
+                            'mandates',
+                            'votes',
+                            f'panachage_votes_from_{wrong_id}',
+                            'panachage_votes_from_2',
+                            'panachage_votes_from_999'
+                        )),
+                        '2015,10000,1,P1,#123456,1,5000,10,11,12',
+                        '2011,10000,1,P1,#123456,0,3000,13,14,15',
+                        '2015,10000,2,P2,#aabbcc,0,5000,20,21,22',
+                        '2011,10000,2,P2,#aabbcc,1,7000,23,24,25',
+                    ))
+                ).encode('utf-8')), 'text/plain'
+    )
+
+    assert errors[0].error.interpolate() == error
 
 
 def test_import_party_results_with_panachage(session):
