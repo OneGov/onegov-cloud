cb5cbc94a0eb6ccd31a848576a1c997c7d31d0a5
Lukas <lukas.burkhard@seantis.ch>
2019-09-26 10:33:14 +0200

    Adds test with verified data from canton gr

--- tests/onegov/election_day/utils/test_connections.py
+++ tests/onegov/election_day/utils/test_connections.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from onegov.election_day.utils.common import LastUpdatedOrderedDict
 from onegov.election_day.utils.election.connections import \
     get_connection_results_api
 
@@ -17,7 +18,43 @@ def test_get_connection_results(import_test_datasets, session):
         app_session=session
     )
     assert not errors
+    # These results have been verified by T. Hardegger
     results = get_connection_results_api(election, session)
 
-    print(results)
-    assert False
+    assert results['1']['total_votes'] == 23141
+    assert results['1']['subconns']['1'] == LastUpdatedOrderedDict({
+        'total_votes': 7520,
+        'lists': LastUpdatedOrderedDict({
+            'BDP Graubünden - Junge BDP Graubünden': 3861,
+            'BDP Graubünden': 3659
+        })
+    })
+    assert results['1']['subconns']['2'] == LastUpdatedOrderedDict({
+        'total_votes': 4668,
+        'lists': LastUpdatedOrderedDict({
+            'FDP.Die Liberalen Graubünden - Hauptliste': 1627,
+            'Jungfreisinnige Graubünden - FDP-Futuro': 1101,
+            'FDP.Die Liberalen GR - Support Gewerbe': 1100,
+            'FDP.Die Liberalen GR - Support Gemeindepolitik': 840
+        })
+    })
+    assert results['1']['subconns']['3'] == LastUpdatedOrderedDict({
+        'total_votes': 10953,
+        'lists': LastUpdatedOrderedDict({
+            'CVP Graubünden': 8510,
+            'CVP - Die Jungen A': 1380,
+            'CVP - Die Jungen 1': 1063
+        })
+    })
+    assert results['2']['total_votes'] == 27497
+    assert results['2']['subconns']['1'] == LastUpdatedOrderedDict({
+        'total_votes': 20420,
+        'lists': LastUpdatedOrderedDict({
+            'SP Sozialdemokratische Partei': 15969,
+            'SP JUSO Frauen*': 3000,
+            'SP JUSO Männer*': 1451
+        })
+    })
+    assert results['2']['lists'] == LastUpdatedOrderedDict({
+        'Verda - Grüne Graubünden': 1652
+    })
