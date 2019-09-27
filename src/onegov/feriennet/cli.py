e532bb4c67926c3ec3617b5e7e3508f2f8b39af2
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2019-09-27 09:44:04 +0200

    Feriennet: Introduces a command to delete periods
    This is a semi-regular support request we have to fulfill, which is
    going to be much easier to fulfill with this command.
    
    TYPE: Feature

--- src/onegov/feriennet/cli.py
+++ src/onegov/feriennet/cli.py
@@ -1,117 +1,111 @@
 import click
-import random
+import sys
 
-from datetime import timedelta
-from faker import Faker
-from functools import partial
-from onegov.activity import OccasionCollection
-from onegov.activity.models.activity import ACTIVITY_STATES
 from onegov.core.cli import command_group
-from onegov.feriennet.collections import VacationActivityCollection
-from onegov.feriennet.forms.activity import TAGS
-from onegov.user import UserCollection
+from onegov.activity.models import Period
 
 
 cli = command_group()
 
 
-@cli.command(name='generate-activities', context_settings={'singular': True})
-@click.option('--count', default=1000)
-def generate_activities(count):
-    """ Randomly generates activities/occasions and bookings for
-    performance tests.
+@cli.command(name='delete-period', context_settings={'singular': True})
+@click.argument('title')
+def delete_period(title):
+    """ Deletes all the data associated with a period, including:
+
+    - Payments
+    - Bookings
+    - Occasions
+    - Publication Requests
+    - Tickets
+
+    We usually don't allow for this, but there tends to be a request here and
+    there about this, where a Ferienpass created a period for testing and
+    tries to return to a semi-clean state.
+
+    Example:
+
+        onegov-feriennet --select /foo/bar delete-period "Ferienpass Test"
 
     """
 
-    fake = Faker()
-    tags = [t[0] for t in TAGS]
+    def delete_period(request, app):
+        period = request.session.query(Period).filter_by(title=title).first()
 
-    def maybe():
-        return random.choice((True, False))
+        if not period:
+            print(f"Could not find period «{title}»")
+            sys.exit(1)
 
-    def maybe_repeat(fn):
-        result = fn()
-
-        def wrapper():
-            nonlocal result
-
-            if maybe():
-                result = fn()
-
-            return result
-
-        return wrapper
-
-    def random_age():
-        min_age = random.randint(0, 15)
-        max_age = random.randint(min_age, 20)
-
-        return min_age, max_age
-
-    def random_spots():
-        min_spots = random.randint(0, 10)
-        max_spots = random.randint(min_spots, 30)
-
-        return min_spots, max_spots
-
-    def random_dates():
-        start = fake.date_time_this_year(before_now=True)
-        start = start.replace(
-            second=0,
-            microsecond=0,
-            minute=start.minute - (start.minute % 15)
-        )
-
-        end = start + timedelta(hours=random.randint(0, 96))
-
-        return start, end
-
-    def generate_activity(request, app, owner, activities, occasions):
-        activity = activities.add(
-            title=fake.text(50).rstrip('.'),
-            lead=fake.text(150),
-            text=fake.text(500),
-            username=owner,
-            tags=random.sample(tags, k=random.randint(0, 8))
-        )
-        activity.state = random.choice(ACTIVITY_STATES)
-
-        dates = maybe_repeat(random_dates)
-        age = maybe_repeat(random_age)
-        spots = maybe_repeat(random_spots)
-
-        for i in range(0, random.randint(0, 8)):
-
-            start, end = dates()
-
-            occasions.add(
-                activity=activity,
-                start=start,
-                end=end,
-                age=age(),
-                spots=spots(),
-                timezone='Europe/Zurich',
-                location=maybe() and fake.city() or None,
-                note=maybe() and fake.text(random.randint(5, 100)) or None,
+        request.session.execute(f"""
+            DELETE FROM payments WHERE payments.id IN (
+                SELECT payment_id FROM payments_for_invoice_items_payments
+                WHERE invoice_items_id IN (
+                    SELECT id FROM invoice_items
+                    WHERE invoice_id IN (
+                        SELECT id FROM invoices WHERE period_id = '{period.id}'
+                    )
+                )
             )
+        """)
 
-    def generate(request, app):
-        owners = [
-            u.username for u in
-            UserCollection(app.session()).by_roles('admin', 'editor')
-        ]
+        request.session.execute(f"""
+            DELETE FROM payments_for_invoice_items_payments
+            WHERE invoice_items_id IN (
+                SELECT id FROM invoice_items
+                WHERE invoice_id IN (
+                    SELECT id FROM invoices WHERE period_id = '{period.id}'
+                )
+            )
+        """)
 
-        session = app.session()
+        request.session.execute(f"""
+            DELETE FROM invoice_items where invoice_id IN (
+                SELECT id FROM invoices WHERE period_id = '{period.id}'
+            )
+        """)
 
-        bound_generate_activity = partial(
-            generate_activity,
-            request=request,
-            app=app,
-            activities=VacationActivityCollection(session, identity=None),
-            occasions=OccasionCollection(session),
-        )
+        request.session.execute(f"""
+            DELETE FROM invoice_references where invoice_id IN (
+                SELECT id FROM invoices WHERE period_id = '{period.id}'
+            )
+        """)
 
-        for n in range(0, count):
-            bound_generate_activity(owner=random.choice(owners))
+        request.session.execute(f"""
+            DELETE FROM invoices WHERE period_id = '{period.id}'
+        """)
 
-    return generate
+        request.session.execute(f"""
+            DELETE FROM tickets WHERE handler_id::uuid IN (
+                SELECT id FROM publication_requests
+                WHERE period_id = '{period.id}'
+            )
+        """)
+
+        request.session.execute(f"""
+            DELETE FROM publication_requests WHERE period_id = '{period.id}'
+        """)
+
+        request.session.execute(f"""
+            DELETE FROM bookings WHERE period_id = '{period.id}'
+        """)
+
+        request.session.execute(f"""
+            DELETE FROM occasion_dates WHERE occasion_id IN (
+                SELECT id FROM occasions WHERE period_id = '{period.id}'
+            )
+        """)
+
+        request.session.execute(f"""
+            DELETE FROM occasion_needs WHERE occasion_id IN (
+                SELECT id FROM occasions WHERE period_id = '{period.id}'
+            )
+        """)
+
+        request.session.execute(f"""
+            DELETE FROM occasions WHERE period_id = '{period.id}'
+        """)
+
+        # triggers a cache update
+        request.session.delete(period)
+
+    return delete_period
