d024c569935c65d651a1d18a95eba173c6350f2d
Lukas <lukas.burkhard@seantis.ch>
2019-10-22 15:27:31 +0200

    Adds defaults for the hiding elements per tenant

---
+++ src/onegov/election_day/defaults.py
@@ -0,0 +1,5 @@
+
+# Defaults for hiding elements if not value is provided in principal.yml
+always_hide_candidate_by_entity_chart_percentages = False
+hide_connections_chart_intermediate_results = False
+hide_candidates_chart_intermediate_results = False
