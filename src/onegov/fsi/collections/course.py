cb244cc908a0bdde8cdebd035260f87ef696b3e9
Lukas <lukas.burkhard@seantis.ch>
2019-11-04 15:11:30 +0100

    Improves collection by using SearchableCollection

--- src/onegov/fsi/collections/course.py
+++ src/onegov/fsi/collections/course.py
@@ -1,24 +1,47 @@
 from sqlalchemy import desc
 
-from onegov.core.collection import Pagination
+from onegov.core.collection import Pagination, SearcheableCollection
 from onegov.fsi.models.course import Course
 
 
-class CourseCollection(Pagination):
+class CourseCollection(SearcheableCollection, Pagination):
 
-    def __init__(self, session, page=0, creator_id=None):
+    """
+    Inherits from GenericCollection - SearchableCollection and add
+    pagination mixing which does not inherit from any class.
+
+    Only the GenericCollection defines __init__ method.
+
+    """
+
+    def __init__(
+            self, session, page=0, creator_id=None, term=None, locale=None):
+        super().__init__(session)
         self.session = session
         self.page = page
         self.creator_id = creator_id    # to filter courses of a creator
+        self.term = term
+        self.locale = locale
 
     def __eq__(self, other):
-        return self.page == other.page and self.creator_id == other.creator_id
+        return (self.page == other.page
+                and self.creator_id == other.creator_id
+                and self.term == other.term
+                and self.locale == other.local)
+
+    @property
+    def model_class(self):
+        return Course
+
+    @property
+    def term_filter_cols(self):
+        return 'name', 'presenter_name', 'description'
 
     def query(self):
-        query = self.session.query(Course).order_by(desc(Course.created))
+        query = super().query()
         if self.creator_id:
             query = query.filter_by(user_id=self.creator_id)
-        return query
+        return query.order_by(desc(Course.created))
 
     def subset(self):
         return self.query()
@@ -28,4 +51,10 @@ class CourseCollection(Pagination):
         return self.page
 
     def page_by_index(self, index):
-        return self.__class__(self.session, index, self.creator_id)
+        return self.__class__(
+            self.session,
+            index,
+            creator_id=self.creator_id,
+            term=self.term,
+            locale=self.locale
+        )
