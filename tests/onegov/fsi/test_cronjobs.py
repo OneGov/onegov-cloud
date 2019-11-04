f67761f2a3579537105dfc8daa5fe52f02dfb519
Lukas <lukas.burkhard@seantis.ch>
2019-11-04 17:35:31 +0100

    Uses now session to invoke fixture

--- tests/onegov/fsi/test_cronjobs.py
+++ tests/onegov/fsi/test_cronjobs.py
@@ -6,14 +6,12 @@ from onegov.fsi.collections.reservation import ReservationCollection
 from tests.onegov.org.common import get_cronjob_by_name, get_cronjob_url
 
 
-@pytest.mark.skip('Session merging is not working')
-def test_send_reminder_mails(
-        session, fsi_app, smtp, future_course_reservation):
-    reservation, data = future_course_reservation
+@pytest.mark.skip('last statement fails still')
+def test_send_reminder_mails(fsi_app, smtp, future_course_reservation):
+    session = fsi_app.session()
+    reservation, data = future_course_reservation(session)
 
     client = Client(fsi_app)
-    app_session = fsi_app.session()
-    session.merge(app_session)
     collection = ReservationCollection(session)
     reservations = collection.for_reminder_mails()
     assert collection.query().count() == 1
