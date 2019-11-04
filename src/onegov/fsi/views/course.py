1686d69ab0cd2ab0314464504f9047a8de66d974
Lukas <lukas.burkhard@seantis.ch>
2019-11-04 13:05:53 +0100

    Makes variable desc more clear

---
+++ src/onegov/fsi/views/course.py
@@ -0,0 +1,18 @@
+from onegov.fsi import FsiApp
+from onegov.fsi.collections.course import CourseCollection
+from onegov.fsi.layout import DefaultLayout
+from onegov.fsi import _
+
+
+@FsiApp.html(
+    model=CourseCollection,
+    template='course_collection.pt',
+    form=object)
+def view_course_collection(self, request):
+    layout = DefaultLayout(self, request)
+    return {
+            'title': _('Courses'),
+            'layout': layout,
+            'model': self,
+            'courses': self.query()
+    }
