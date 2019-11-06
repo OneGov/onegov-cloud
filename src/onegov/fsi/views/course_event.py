8866085a0bd225649cccf2d41ac2f96ffd24955d
Lukas <lukas.burkhard@seantis.ch>
2019-11-06 09:02:04 +0100

    Adds course event views

--- src/onegov/fsi/views/course_event.py
+++ src/onegov/fsi/views/course_event.py
@@ -3,6 +3,15 @@ from onegov.fsi.collections.course_event import CourseEventCollection
 from onegov.fsi.layout import CourseEventsLayout
 
 
+@FsiApp.html(model=CourseEventCollection, template='event_collection.pt')
+def get_course_events_view(self, request):
+    layout = CourseEventsLayout(self, request)
+    return {
+        'title': layout.title,
+        'layout': layout,
+        'events': self.query().all()
+    }
+
 # @FsiApp.form(
 #     model=CourseEventCollection,
 #     template='form.pt',
