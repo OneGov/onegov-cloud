6f55d0f17379e551b4fb56163a21bf4f0c220934
Lukas <lukas.burkhard@seantis.ch>
2019-11-12 14:51:19 +0100

    Completes reservation views with delete view

--- src/onegov/fsi/views/reservation.py
+++ src/onegov/fsi/views/reservation.py
@@ -1,19 +1,18 @@
 from onegov.fsi import FsiApp
 from onegov.fsi.collections.reservation import ReservationCollection
-from onegov.fsi.layouts.reservation import ReservationLayout
+from onegov.fsi.layouts.reservation import ReservationLayout, \
+    ReservationCollectionLayout
+from onegov.fsi.models.reservation import Reservation
 from onegov.org.forms import ReservationForm
 from onegov.fsi import _
 
 
-@FsiApp.html(
-    model=ReservationCollection,
-    template='reservation.pt')
+@FsiApp.html(model=ReservationCollection, template='reservations.pt')
 def view_reservations(self, request):
-    layout = ReservationLayout(self, request)
+    layout = ReservationCollectionLayout(self, request)
     return {
         'title': _('Reservations'),
         'layout': layout,
-        'model': self,
         'reservations': self.query().all()
     }
 
@@ -24,7 +23,7 @@ def view_reservations(self, request):
     name='add',
     form=ReservationForm
 )
-def view_add_reservation_confirmation(self, request):
+def view_add_reservation(self, request):
     layout = ReservationLayout(self, request)
     return {
         'title': _('Add Reservation'),
@@ -41,6 +40,22 @@ def view_add_reservation_confirmation(self, request):
 def view_add_from_course_event(self, request):
     request.assert_valid_csrf_token()
     self.add(
-        attendee_id=self.attendee_id,
+        attendee_id=self.attendee_id or request.attendee_id,
         course_event_id=self.course_event_id)
     request.success(_('New reservation successfully added'))
+
+
+@FsiApp.html(model=Reservation)
+def view_reservation(self, request):
+    pass
+
+
+@FsiApp.html(
+    model=Reservation,
+    request_method='DELETE',
+    name='delete'
+)
+def view_delete_reservation(self, request):
+    request.assert_valid_csrf_token()
+    ReservationCollection(request.session).delete(self)
+    request.success(_('Reservation successfully deleted'))
