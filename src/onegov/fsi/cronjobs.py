e7071368b277f07b6092abcd8517822ca1265143
Lukas <lukas.burkhard@seantis.ch>
2019-12-09 17:45:30 +0100

    Finally adds working cronjob for reminder emails

--- src/onegov/fsi/cronjobs.py
+++ src/onegov/fsi/cronjobs.py
@@ -2,35 +2,37 @@ from sedate import utcnow
 
 from onegov.core.templates import render_template
 from onegov.fsi.collections.course_event import CourseEventCollection
-from onegov.fsi.collections.reservation import ReservationCollection
 from onegov.fsi import _, FsiApp
 from onegov.fsi.layouts.notification import MailLayout
-from onegov.fsi.models.course_notification_template import \
-    CourseNotificationTemplate
 
 
 def send_scheduled_reminders(request):
-    events = CourseEventCollection(request.session)
 
-    for event in events:
-        pass
-        # template = res.course_event.reminder_template
-        # assert template
-        # assert res.attendee
-        # title = _('Reminder for course: ${name}',
-        #           mapping={'name': res.course.name})
-        # content = render_template('mail_notification.pt', request, {
-        #         'layout': MailLayout(res.course_event, request),
-        #         'title': title,
-        #         'notification': template.text_html,
-        #         'attendee': res.attendee
-        #     })
-        # request.app.send_marketing_email(
-        #     receivers=(res.attendee.email, ),
-        #     subject=template.subject,
-        #     content=content
-        # )
-        # template.last_sent = utcnow()
+    events = CourseEventCollection(request.session).get_past_reminder_date()
+
+    for course_event in events:
+        if not course_event.attendees.count():
+            continue
+        template = course_event.reminder_template
+
+        if not template or template.last_sent:
+            continue
+
+        title = _('Reminder for course event: ${name}',
+                  mapping={'name': course_event.course.name})
+        for attendee in course_event.attendees:
+            content = render_template('mail_notification.pt', request, {
+                    'layout': MailLayout(template, request),
+                    'title': title,
+                    'notification': template.text_html,
+                    'attendee': attendee
+                })
+            request.app.send_marketing_email(
+                receivers=(attendee.email, ),
+                subject=template.subject,
+                content=content
+            )
+        template.last_sent = utcnow()
 
 
 @FsiApp.cronjob(hour=8, minute=30, timezone='Europe/Zurich')
