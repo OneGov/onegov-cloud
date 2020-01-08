8adb2d785fe4b99f77844b6a1c756baa5131e53f
Lukas <lukas.burkhard@seantis.ch>
2020-01-08 10:51:39 +0100

    Testing code with real data

--- src/onegov/fsi/ims_import.py
+++ src/onegov/fsi/ims_import.py
@@ -6,6 +6,12 @@ Files:
 - Ausführungen: the course events
 - Kurse: courses, ignore everything you can also in the event
 
+Some edge cases are to handle
+
+- Missing start, end of a course event, they generally have BUCHUNGSTATUS = Keine Buchung
+- Missing presenter names, can not be nullable in our case
+- in Teilnehmer, ANWESEND can be missing
+
 """
 from uuid import uuid4
 import dateutil.parser
@@ -31,25 +37,35 @@ def with_open(func):
 
 @with_open
 def parse_courses(csvfile):
+    errors = {}
     courses = {}
     for line in csvfile.lines:
-        name = line.kurzbeschreibung
-        courses[line.vorgangsnr] = Course(
-                id=uuid4(),
-                name=name,
-                description=name        # Where is the long description?
-            )
-    return courses
+        try:
+            name = line.kurzbeschreibung
+            courses[line.vorgangsnr] = Course(
+                    id=uuid4(),
+                    name=name,
+                    description=name        # Where is the long description?
+                )
+        except Exception as e:
+            errors[line.rownumber] = e.args[0]
+            continue
+
+    return errors, courses
 
 
 def parse_date(val):
+    if not val:
+        raise TypeError('Emtpy value provided')
     date_ = dateutil.parser.parse(val)
     return replace_timezone(date_, 'UTC')
 
 
 def parse_status(val):
     mapping = {
-        'Abgeschlossen': 'confirmed'
+        'Abgeschlossen': 'confirmed',
+        'Abgesagt': 'canceled',
+        'Erfasst': 'created'
     }
     return mapping[val]
 
@@ -57,14 +73,33 @@ def parse_status(val):
 @with_open
 def parse_events(csvfile, courses):
     events = {}
+    errors = {}
     for line in csvfile.lines:
+
+        # Block accepts empty end values, but fails if the start is not set
         parent_course = courses[line.proc_id_kv]
+        date_lacking = not line.kurs_von or not line.kurs_bis
+        if line.buchungsstatus == 'Keine Buchung' and date_lacking:
+            print(f'Skipping line {line.rownumber}: '
+                  f'course not done and lacking date')
+            continue
+
+        try:
+            start = parse_date(line.kurs_von)
+        except Exception as e:
+            errors[line.rownumber] = e.args[0]
+            continue
+        try:
+            end = parse_date(line.kurs_bis)
+        except TypeError:
+            end = start
+
         event = CourseEvent(
             id=uuid4(),
             course_id=parent_course.id,
             location=line.kurslokal,
-            start=parse_date(line.kurs_von),
-            end=parse_date(line.kurs_bis),
+            start=start,
+            end=end,
             presenter_name=line.referent_name,
             presenter_company=line.referent_firma,
             min_attendees=line.teilnehmer_min,
@@ -72,10 +107,12 @@ def parse_events(csvfile, courses):
             status=parse_status(line.status),
         )
         events[line.proc_id] = event
-    return events
+    return errors, events
 
 
 def parse_completed(val):
+    if not val:
+        raise TypeError('Empty Value in parse completed')
     return True if val == 'J' else False
 
 
@@ -119,23 +156,67 @@ def parse_reservations(csvfile, session, events):
     return errors, reservations
 
 
+@with_open
+def parse_persons(csvfile, session):
+    errors = {}
+    persons = {}
+    for line in csvfile.lines:
+        obj_id = line.obj_id
+        assert obj_id
+        code = line.p_userid
+        mail = line.p_email
+
+        if not mail:
+            errors[line.rownumber] = 'User with no email'
+            continue
+
+        # Find the user, if more than one exists, code is needed to compare
+        persons.setdefault(obj_id, {
+            'email': mail,
+            'code': code,
+            'company': line.teilnehmer_firma
+        })
+    return errors, persons
+
+
 def import_ims_data(
         session,
         attendees_file,
         events_file,
         courses_file,
+        persons_file,
+        write_log=False
 ):
+    gathered_errors = {}
 
-    courses = parse_courses(courses_file)
-    events = parse_events(events_file, courses)
-    errors, reservations = parse_reservations(attendees_file, session, events)
+    errors, persons = parse_persons(persons_file, session)
+    assert False
 
     if errors:
-        return errors
+        gathered_errors['parse_persons'] = errors
+
+    errors, courses = parse_courses(courses_file)
+
+    if errors:
+        gathered_errors['parse_courses'] = errors
+        return gathered_errors
+
+    errors, events = parse_events(events_file, courses)
+    if errors:
+        gathered_errors['parse_events'] = errors
+        return gathered_errors
+
+    errors, reservations = parse_reservations(attendees_file, session, events)
+    if errors:
+        gathered_errors['parse_reservations'] = errors
+
+    if gathered_errors:
+        return gathered_errors
 
     session.add_all(courses.values())
     session.add_all(events.values())
     session.add_all(reservations)
     session.flush()
 
-    return []
+    return {}
+
--- tests/onegov/fsi/test_import_ims.py
+++ tests/onegov/fsi/test_import_ims.py
@@ -1,8 +1,9 @@
 from onegov.core.utils import module_path
 from onegov.fsi.ims_import import import_ims_data
 
-data_path = module_path('tests.onegov.fsi', 'ims_test_data')
+data_path = module_path('tests.onegov.fsi', 'temp_data')
 
+persons_file = f'{data_path}/Personen.txt'
 attendees_file = f'{data_path}/Teilnehmer.txt'
 events_file = f'{data_path}/Ausführungen.txt'
 courses_file = f'{data_path}/Kurse.txt'
@@ -17,6 +18,9 @@ def test_import_ims(session, attendee):
         session,
         attendees_file,
         events_file,
-        courses_file
+        courses_file,
+        persons_file
     )
+    print(errors)
+    assert not errors
     print('errors in reservation parsing: ', errors)
