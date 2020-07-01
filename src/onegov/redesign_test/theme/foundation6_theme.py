080ca2f44047b0b286ccc8d1bca039c0ee0597ff
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-07-01 09:03:05 +0200

    Adds pre and post imports and check if they work

--- src/onegov/redesign_test/theme/foundation6_theme.py
+++ src/onegov/redesign_test/theme/foundation6_theme.py
@@ -1,5 +1,22 @@
+from onegov.core.utils import module_path
 from onegov.foundation6 import BaseTheme
 
 
 class RedesignTheme(BaseTheme):
     name = 'onegov.test.foundation6'
+
+    @property
+    def extra_search_paths(self):
+        return [
+            module_path('onegov.redesign_test.theme', 'styles'),
+        ]
+
+    @property
+    def pre_imports(self):
+        return ['foundation-mods']
+
+    @property
+    def post_imports(self):
+        return [
+            'redesign',
+        ]
