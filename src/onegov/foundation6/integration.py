80b6c0209f1dbddfb40cc82ef16c3cc6a7a69078
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-07-01 12:52:23 +0200

    Adds Layout for seamless integration

--- src/onegov/foundation6/integration.py
+++ src/onegov/foundation6/integration.py
@@ -1,11 +1,11 @@
 from onegov.core import Framework
+from onegov.core.layout import ChameleonLayout
 from onegov.core.request import CoreRequest
 
 
 class FoundationRequest(CoreRequest):
     def __init__(self, *args, **kwargs):
         super(FoundationRequest, self).__init__(*args, **kwargs)
-        # self.include('foundation-js')
 
 
 class FoundationApp(Framework):
@@ -20,3 +20,9 @@ def get_foundation_js_path():
 @FoundationApp.webasset('foundation-js')
 def get_foundation_js_assets():
     yield 'foundation.min.js'
+
+
+class FoundationLayout(ChameleonLayout):
+    def __init__(self, model, request):
+        super().__init__(model, request)
+        self.request.include('foundation.min.js')
