1d336879b2730d46623259b8061c8811d68b9438
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-07-16 15:10:08 +0200

    Adds orbit slider component for homepage

--- src/onegov/org/theme/org_theme.py
+++ src/onegov/org/theme/org_theme.py
@@ -104,6 +104,7 @@ class OrgTheme(BaseTheme):
             'sortable',
             'search',
             'forms',
+            'sliders',
             'settings',
             'helpers',
             'footer',
