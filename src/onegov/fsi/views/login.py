cb5aedac48611d3dfa6cf75d177ff8bf2e294307
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-08-24 12:48:42 +0200

    Modifies LoginForm for FSI for school users
    - so that auth providers for school users are skipped

--- src/onegov/fsi/views/login.py
+++ src/onegov/fsi/views/login.py
@@ -1,13 +1,36 @@
 from onegov.core.security import Public
 from onegov.fsi import _
 from onegov.fsi.app import FsiApp
+from onegov.fsi.cli import schools
 from onegov.org.views.auth import handle_login as handle_login_base
 from onegov.user import Auth
 from onegov.user.forms import LoginForm
 
 
+class FsiLoginForm(LoginForm):
+
+    @property
+    def login_data(self):
+        """
+        Skips auth providers for school users are just indexed by the LDAP but
+        not can bot be logged in to. The are authenticated with the user and
+        password in our database, so we pass skip_providers to the login data.
+        """
+        login_data = super().login_data
+        username = self.username.data
+        if not username or '@' not in username:
+            return login_data
+        domain = f"@{username.split('@', 1)[1]}"
+        if domain in schools:
+            return {
+                'skip_providers': True,
+                **login_data
+            }
+        return login_data
+
+
 @FsiApp.form(model=Auth, name='login', template='login.pt', permission=Public,
-             form=LoginForm)
+             form=FsiLoginForm)
 def handle_login(self, request, form):
 
     # custom default redirect
