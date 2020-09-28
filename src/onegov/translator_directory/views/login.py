4169fad94a56c7f4139ede6cf22b94587a9dab64
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-09-28 10:20:11 +0200

    Use login form of fsi to skip auth providers for school teachers

--- src/onegov/translator_directory/views/login.py
+++ src/onegov/translator_directory/views/login.py
@@ -1,7 +1,8 @@
+from onegov.fsi import _
 from onegov.core.security import Public
+from onegov.fsi.views.login import FsiLoginForm
 from onegov.translator_directory import TranslatorDirectoryApp
 from onegov.user import Auth
-from onegov.user.forms import LoginForm
 from onegov.org.views.auth import handle_login as handle_login_base
 
 
@@ -10,11 +11,15 @@ from onegov.org.views.auth import handle_login as handle_login_base
     name='login',
     template='login.pt',
     permission=Public,
-    form=LoginForm
+    form=FsiLoginForm
 )
 def handle_login(self, request, form):
     # custom default redirect
     if self.to == '/':
         self.to = '/translators'
+
+    # custom username handle
+    form.username.label.text = request.translate(
+        _("E-Mail Address / Username / Shortcut"))
 
     return handle_login_base(self, request, form)
