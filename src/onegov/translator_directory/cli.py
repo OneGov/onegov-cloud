fad7bfd35db410b23e50b550791f1014712b56b7
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-11-06 09:40:41 +0100

    Adds distance cli

--- src/onegov/translator_directory/cli.py
+++ src/onegov/translator_directory/cli.py
@@ -13,6 +13,8 @@ from onegov.translator_directory.models.certificate import \
     LanguageCertificate
 from onegov.translator_directory.models.language import Language
 from onegov.translator_directory.models.translator import Translator
+from onegov.translator_directory.utils import parse_geocode_result, \
+    parse_directions_result
 
 cli = command_group()
 
@@ -277,6 +279,90 @@ def fetch_users_cli(ldap_server, ldap_username, ldap_password):
     return execute
 
 
+@cli.command(name='update-drive-distance', context_settings={'singular': True})
+@click.option('--dry-run', is_flag=True)
+@click.option('--only-empty', is_flag=True, default=True)
+@click.option(
+    '--tolerance-factor',
+    help='Do not overwrite existing distances if off by +- a factor',
+    default=0.1,
+    type=float
+)
+def drive_distances_cli(dry_run, only_empty, tolerance_factor):
+
+    def to_tuple(coordinate):
+        return coordinate.lat, coordinate.lon
+
+    def found_route(response):
+        return response.status_code == 200 and response.json()['code'] == 'Ok'
+
+    def out_of_tolerance(old_distance, new_distance):
+        if not old_distance or not new_distance:
+            return False
+        too_big = new_distance > old_distance + old_distance * tolerance_factor
+        too_sml = new_distance < old_distance - old_distance * tolerance_factor
+        return too_big or too_sml
+
+    def get_distances(request, app):
+        routes_not_found = []
+        tolerance_failed = []
+        distance_changed = 0
+        routes_found = 0
+        total = 0
+
+        directions_api = MapboxRequests(
+            app.mapbox_token,
+            endpoint='directions',
+            profile='driving'
+        )
+        query = request.session.query(Translator)
+        if only_empty:
+            query = query.filter(Translator.drive_distance == None)
+
+        for trs in query:
+            if not trs.coordinates:
+                continue
+            total += 1
+            response = directions_api.directions([
+                to_tuple(app.coordinates),
+                to_tuple(trs.coordinates)
+            ])
+            if found_route(response):
+                routes_found += 1
+                dist = parse_directions_result(response)
+                if not out_of_tolerance(trs.drive_distance, dist):
+                    trs.drive_distance = dist
+                    distance_changed += 1
+                else:
+                    tolerance_failed.append(trs)
+            else:
+                routes_not_found.append(trs)
+
+        click.secho(f'Directions not found: {len(routes_not_found)}/{total}',
+                    fg='yellow')
+
+        click.secho(f'Over tolerance: {len(tolerance_failed)}/{routes_found}',
+                    fg='yellow')
+
+        if routes_not_found:
+            click.secho(
+                'Listing all translators whose directions could not be found')
+            for trs in routes_not_found:
+                click.secho(f'- {request.link(trs, name="edit")}')
+
+        if tolerance_failed:
+            click.secho(
+                'Listing all translators who failed distance check')
+
+            for trs in tolerance_failed:
+                click.secho(f'- {request.link(trs, name="edit")}')
+
+        if dry_run:
+            transaction.abort()
+
+    return get_distances
+
+
 @cli.command(name='geocode', context_settings={'singular': True})
 @click.option('--dry-run', is_flag=True)
 @click.option('--only-empty', is_flag=True, default=True)
@@ -315,7 +401,6 @@ def geocode_cli(dry_run, only_empty):
                 skipped += 1
                 continue
 
-            total += 1
             response = api.geocode(
                 street=trs.address,
                 zip_code=trs.zip_code,
