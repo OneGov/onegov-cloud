e8f50ac8a02cfb4ca5d0fa91b38abdf89863708e
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-11-19 16:43:33 +0100

    Adds markdown rendering for field description
    - Render as markdown if the string really contains any markdown elements
    - Will put markdown into div.long-field-help if it was markdown

---
+++ tests/onegov/org/test_macros.py
@@ -0,0 +1,62 @@
+from webob.multidict import MultiDict
+from wtforms import StringField
+
+from onegov.core.templates import render_macro
+from onegov.core.utils import Bunch
+from onegov.form import Form
+from onegov.org.layout import DefaultLayout
+from pyquery import PyQuery as pq
+
+
+class DummyRequest:
+
+    is_manager = False
+    is_admin = False
+
+    def __init__(self, app):
+        self.app = app
+
+    def include(self, value):
+        pass
+
+    def translate(self, value, **kwargs):
+        return value
+
+    def get_translate(self, for_chameleon=None):
+        return self.translate
+
+    def link(self, *args, **kwargs):
+        return '#'
+
+    def new_csrf_token(self):
+        return ''
+
+
+def test_form_field_long_description_markdown(org_app):
+
+    model = Bunch()
+    request = DummyRequest(org_app)
+
+    layout = DefaultLayout(model, request)
+
+    class LongDescriptionForm(Form):
+        text = StringField(
+            label='Text',
+            description='Short text and md elem is enough [link](#).')
+
+    form = LongDescriptionForm(MultiDict([
+        ('text', 'Example')
+    ]))
+    form.request = DummyRequest(org_app)
+
+    html = render_macro(
+        layout.macros['form'],
+        form.request,
+        {
+            'form': form,
+            'layout': layout,
+            'year': layout.today().year,
+        }
+    )
+    assert pq(html)('.long-field-help a').attr('href') == '#'
+    assert pq(html)('.long-field-help a').text() == 'link'
