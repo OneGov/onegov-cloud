30ca4785fb03ae7f54707bd281b3f006ee64300d
Lukas Burkhard <lukas.burkhard@seantis.ch>
2020-12-02 12:12:55 +0100

    Remove fields from content extension since they are part of DirectoryEntry model

--- src/onegov/org/models/extensions.py
+++ src/onegov/org/models/extensions.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 from onegov.core.orm.mixins import meta_property, content_property
 from onegov.core.utils import linkify, normalize_for_url
-from onegov.directory.mixins import PublicationMixin, TimezonePublicationMixin
 from onegov.form import FieldDependency, WTFormsClassBuilder
 from onegov.gis import CoordinatesMixin
 from onegov.org import _
@@ -381,7 +380,7 @@ class ResourceValidationExtension(ContentExtension):
         return WithResourceValidation
 
 
-class PublicationExtension(ContentExtension, TimezonePublicationMixin):
+class PublicationExtension(ContentExtension):
 
     def extend_form(self, form_class, request):
         return PublicationFormExtension(form_class).create()
