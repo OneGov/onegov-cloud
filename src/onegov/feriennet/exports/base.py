4c96652e687fef4247f7fd8475e56c69006447b1
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-01-12 17:06:28 +0100

    Feriennet: Include all acitvity tags in export

--- src/onegov/feriennet/exports/base.py
+++ src/onegov/feriennet/exports/base.py
@@ -136,13 +136,7 @@ class FeriennetExport(Export):
         yield _("Need Description"), need.description
 
     def activity_tags(self, tags):
-        # A WWF feature, which uses one of the relevant tags for VAT.
-        relevant = [
-            "Family Camp", "Camp", "Camp in House",
-            "Tent Camp", "Trecking Camp"
-        ]
-        tags = [t for t in tags or [] if t in relevant]
-        yield _("Activity Tags"), "\n".join(sorted(tags))
+        yield _("Activity Tags"), "\n".join(sorted(tags or []))
 
     def user_fields(self, user):
         user_data = user.data or {}
