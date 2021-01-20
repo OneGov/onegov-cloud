876d720a521c69c945e5530afdf62e1752f020c6
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-01-20 19:33:05 +0100

    Removes double call of function self.generate_style

--- src/onegov/pdf/pdf.py
+++ src/onegov/pdf/pdf.py
@@ -93,8 +93,6 @@ class Pdf(PDFDocument):
         ])
         self.story.append(NextPageTemplate('Later'))
 
-        # Todo: calling the same function twice, maybe a workaround?
-        self.generate_style(font_size=font_size, font_name=font)
         self.adjust_style(font_size=font_size, font_name=font)
 
     def adjust_style(self, font_size=10, font_name=None):
