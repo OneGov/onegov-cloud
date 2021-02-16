dab3c0c3ee0502e286e8e41d8032c139e97aa398
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-02-16 17:40:45 +0100

    Fixes widgets

--- src/onegov/town6/homepage_widgets/widgets.py
+++ src/onegov/town6/homepage_widgets/widgets.py
@@ -1,9 +1,13 @@
 from collections import namedtuple
 from onegov.event import OccurrenceCollection
+from onegov.form import FormCollection
 from onegov.org.elements import Link, LinkGroup
 from onegov.org.layout import EventBaseLayout
 
 from onegov.org.homepage_widgets.widgets import NewsWidget as OrgNewsWidget
+from onegov.org.models import ImageSetCollection, PublicationCollection
+from onegov.people import PersonCollection
+from onegov.reservation import ResourceCollection
 
 from onegov.town6 import TownApp
 from onegov.town6 import _
@@ -188,3 +192,125 @@ class PartnerWidget(object):
                 lead=getattr(org, f'partner_{ix}_name'),
             ) for ix in range(1, partner_count + 1)
         ]}
+
+
+@TownApp.homepage_widget(tag='services')
+class ServicesWidget(object):
+    template = """
+        <xsl:template match="services">
+            <div class="services-panel">
+                <h3 tal:content="services_panel.title"></h3>
+
+                <metal:block use-macro="layout.macros['panel-links']"
+                    tal:define="panel services_panel; show_subtitle False;
+                     as_callout True"
+                />
+            </div>
+        </xsl:template>
+    """
+
+    def get_service_links(self, layout):
+        yield Link(
+            text=_("Online Counter"),
+            url=layout.request.class_link(FormCollection),
+            subtitle=(
+                    layout.org.meta.get('online_counter_label')
+                    or _("Forms and applications")
+            ),
+            classes=('online-counter', 'h5-size')
+        )
+
+        # only if there are publications, will we enable the link to them
+        if not layout.org.hide_publications and layout.app.publications_count:
+            yield Link(
+                text=_("Publications"),
+                url=layout.request.class_link(PublicationCollection),
+                subtitle=_(
+                    layout.org.meta.get('publications_label')
+                    or _("Official Documents")
+                ),
+                classes=('publications', 'h5-size')
+            )
+
+        yield Link(
+            text=_("Reservations"),
+            url=layout.request.class_link(ResourceCollection),
+            subtitle=(
+                    layout.org.meta.get('reservations_label')
+                    or _("Daypasses and rooms")
+            ),
+            classes=('reservations', 'h5-size')
+        )
+
+        if layout.org.meta.get('e_move_url'):
+            yield Link(
+                text=_("E-Move"),
+                url=layout.org.meta.get('e_move_url'),
+                subtitle=(
+                    layout.org.meta.get('e_move_label')
+                    or _("Move with eMovingCH")
+                ),
+                classes=('e-move', 'h5-size')
+            )
+
+
+        resources = ResourceCollection(layout.app.libres_context)
+
+        # ga-tageskarte is the legacy name
+        sbb_daypass = resources.by_name('sbb-tageskarte') \
+            or resources.by_name('ga-tageskarte')
+
+        if sbb_daypass:
+            yield Link(
+                text=_("SBB Daypass"),
+                url=layout.request.link(sbb_daypass),
+                subtitle=(
+                    layout.org.meta.get('daypass_label')
+                    or _("Generalabonnement for Towns")
+                ),
+                classes=('sbb-daypass', 'h5-size')
+            )
+
+    def get_variables(self, layout):
+        return {
+            'services_panel': LinkGroup(_("Services"), links=tuple(
+                self.get_service_links(layout)
+            ))
+        }
+
+
+@TownApp.homepage_widget(tag='contacts_and_albums')
+class ContactsAndAlbumsWidget(object):
+
+    template = """
+           <xsl:template match="contacts_and_albums">
+               <h3 tal:content="contacts_and_albums_panel.title"></h3>
+
+               <metal:block use-macro="layout.macros['panel-links']"
+                   tal:define="panel contacts_and_albums_panel"
+               />
+           </xsl:template>
+       """
+
+    def get_variables(self, layout):
+        request = layout.request
+
+        return {
+            'contacts_and_albums_panel': LinkGroup(
+                title=_("Contacts and Photos"),
+                links=[
+                    Link(
+                        text=_("People"),
+                        url=request.class_link(PersonCollection),
+                        subtitle=_("All contacts"),
+                        classes=('h5-size',)
+                    ),
+                    Link(
+                        text=_("Photo Albums"),
+                        url=request.class_link(ImageSetCollection),
+                        subtitle=_("Impressions"),
+                        classes=('h5-size',)
+                    ),
+                ]
+            )
+        }
