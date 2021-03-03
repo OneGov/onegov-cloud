11493b43f074aa21b29782442894e567d6770012
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-03-03 13:51:59 +0100

    Reworks homepage (for mobile)
    - Keyboard shortcuts to open sidenav
    - Focus on search field when opening offCanvas

--- src/onegov/town6/assets/js/animate.js
+++ src/onegov/town6/assets/js/animate.js
@@ -27,3 +27,17 @@ $('#nav-bar').on('sticky.zf.stuckto:top', function() {
     globals.toggleClass('hide')
     el.data('modified', true)
 });
+
+$('#offCanvas').on('opened.zf.offCanvas', function (e) {
+    $(this).find('#search').focus()
+}).on('close.zf.offCanvas', function (e) {
+    $(this).find('#search').blur()
+})
+
+$(document).on("keypress", function(e) {
+    var code = e.keyCode || e.which
+    // Ctrl+Shift+F or Ctrl+Shift+S
+    if(e.ctrlKey && e.shiftKey && (code === 6 || code === 19) ) {
+        $('#offCanvas').foundation('toggle', e)
+    }
+});
