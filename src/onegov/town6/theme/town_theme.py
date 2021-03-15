67efa9418140143ab5dcb392aa23c57c36e05e1c
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-03-15 11:13:28 +0100

    Adds newsletter scss

--- src/onegov/town6/theme/town_theme.py
+++ src/onegov/town6/theme/town_theme.py
@@ -135,7 +135,8 @@ class TownTheme(BaseTheme):
             'directories',
             'daypicker',
             'payment',
-            'person'
+            'person',
+            'newsletter'
         ]
 
     @property
