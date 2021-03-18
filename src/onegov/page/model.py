01a692f5157bd7d17c3663c655278e0e4f4bc457
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-03-18 17:07:35 +0100

    Page: Adds publication dates

--- src/onegov/page/model.py
+++ src/onegov/page/model.py
@@ -7,9 +7,10 @@ See also: `<http://docs.sqlalchemy.org/en/rel_0_9/orm/self_referential.html>`_
 
 from onegov.core.orm.abstract import AdjacencyList
 from onegov.core.orm.mixins import ContentMixin, TimestampMixin
+from onegov.form.mixins import UTCPublicationMixin
 
 
-class Page(AdjacencyList, ContentMixin, TimestampMixin):
+class Page(AdjacencyList, ContentMixin, TimestampMixin, UTCPublicationMixin):
     """ Defines a generic page. """
 
     __tablename__ = 'pages'
