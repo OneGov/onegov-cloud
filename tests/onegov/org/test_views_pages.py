581c7dca6ba44cd41fb376c0183b4d0eeee39b8b
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-03-23 13:51:48 +0100

    Refactor page test

--- tests/onegov/org/test_views_pages.py
+++ tests/onegov/org/test_views_pages.py
@@ -110,6 +110,31 @@ def test_hide_page(client):
     assert 'Test' not in page
 
 
+def test_links(client):
+    root_url = client.get('/').pyquery('.top-bar-section a').attr('href')
+    client.login_admin()
+    root_page = client.get(root_url)
+
+    new_link = root_page.click("Verknüpfung")
+    assert "Neue Verknüpfung" in new_link
+
+    new_link.form['title'] = 'Google'
+    new_link.form['url'] = 'https://www.google.ch'
+    link = new_link.form.submit().follow()
+
+    assert "Sie wurden nicht automatisch weitergeleitet" in link
+    assert 'https://www.google.ch' in link
+
+    client.get('/auth/logout')
+
+    root_page = client.get(root_url)
+    assert "Google" in root_page
+    google = root_page.click("Google", index=0)
+
+    assert google.status_code == 302
+    assert google.location == 'https://www.google.ch'
+
+
 def test_copy_pages_to_news(client):
     client.login_admin()
 
@@ -127,3 +152,35 @@ def test_copy_pages_to_news(client):
     page = edit.form.submit().follow()
 
     assert '/news/organisation' in page.request.url
+
+
+def test_clipboard(client):
+    client.login_admin()
+
+    page = client.get('/topics/organisation')
+    assert 'paste-link' not in page
+
+    page = page.click(
+        'Kopieren',
+        extra_environ={'HTTP_REFERER': page.request.url}
+    ).follow()
+
+    assert 'paste-link' in page
+
+    page = page.click('Einf').form.submit().follow()
+    assert '/organisation/organisation' in page.request.url
+
+
+def test_clipboard_separation(client):
+    client.login_admin()
+
+    page = client.get('/topics/organisation')
+    page = page.click('Kopieren')
+
+    assert 'paste-link' in client.get('/topics/organisation')
+
+    # new client (browser) -> new clipboard
+    client = client.spawn()
+    client.login_admin()
+
+    assert 'paste-link' not in client.get('/topics/organisation')
