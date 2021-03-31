918cc5921ea60c1850cc6d2f114c514625157e47
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-03-31 09:49:18 +0200

    Reproduces flowable error for long texts [skip ci]

--- tests/onegov/org/test_pdf.py
+++ tests/onegov/org/test_pdf.py
@@ -4,12 +4,62 @@ from PyPDF2 import PdfFileReader
 from webob.multidict import MultiDict
 from onegov.core.utils import Bunch
 from onegov.form import FormCollection
-from onegov.org.models import TicketMessage
+from onegov.org.models import TicketMessage, TicketChatMessage
 from onegov.org.pdf.ticket import TicketPdf
 from onegov.reservation import ResourceCollection
 from onegov.ticket import TicketCollection
 from tests.shared.utils import open_pdf
 
+
+long_text = """
+OpenID is an open standard and decentralized authentication protocol. 
+Promoted by the non-profit OpenID Foundation, it allows users to be 
+authenticated by co-operating sites (known as relying parties, or RP) 
+using a third-party service, eliminating the need for webmasters to 
+provide their own ad hoc login systems, and allowing users to log into 
+multiple unrelated websites without having to have a separate identity 
+and password for each.[1] Users create accounts by selecting an OpenID 
+identity provider[1] and then use those accounts to sign onto any website that 
+accepts OpenID authentication. Several large organizations either issue or 
+accept OpenIDs on their websites, according to the OpenID Foundation.[2]
+
+The OpenID standard provides a framework for the communication that must take 
+place between the identity provider and the OpenID acceptor 
+(the "relying party").[3] An extension to the standard 
+(the OpenID Attribute Exchange) facilitates the transfer of user attributes, 
+such as name and gender, from the OpenID identity provider to the relying party 
+(each relying party may request a different set of attributes, depending on 
+its requirements).[4] The OpenID protocol does not rely on a central authority 
+to authenticate a user's identity. Moreover, neither services nor the OpenID 
+standard may mandate a specific means by which to authenticate users, 
+allowing for approaches ranging from the common (such as passwords) to the 
+novel (such as smart cards or biometrics).
+
+The final version of OpenID is OpenID 2.0, finalized and published in December 
+2007.[5] The term OpenID may also refer to an identifier as specified in the 
+OpenID standard; these identifiers take the form of a unique Uniform Resource 
+Identifier (URI), and are managed by some "OpenID provider" that handles 
+authentication.[1] 
+
+The OpenID standard provides a framework for the communication that must take 
+place between the identity provider and the OpenID acceptor 
+(the "relying party").[3] An extension to the standard 
+(the OpenID Attribute Exchange) facilitates the transfer of user attributes, 
+such as name and gender, from the OpenID identity provider to the relying party 
+(each relying party may request a different set of attributes, depending on 
+its requirements).[4] The OpenID protocol does not rely on a central authority 
+to authenticate a user's identity. Moreover, neither services nor the OpenID 
+standard may mandate a specific means by which to authenticate users, 
+allowing for approaches ranging from the common (such as passwords) to the 
+novel (such as smart cards or biometrics).
+
+The final version of OpenID is OpenID 2.0, finalized and published in December 
+2007.[5] The term OpenID may also refer to an identifier as specified in the 
+OpenID standard; these identifiers take the form of a unique Uniform Resource 
+Identifier (URI), and are managed by some "OpenID provider" that handles 
+authentication.[1] 
+
+"""
 
 def open_ticket(request, token, handler_code, create_message=True):
     with request.session.no_autoflush:
@@ -40,6 +90,17 @@ def update_submission(session, submission, form):
     forms = FormCollection(session)
     if submission:
         forms.submissions.update(submission, form)
+
+
+def add_ticket_message(request, ticket, text):
+    message = TicketChatMessage.create(
+        ticket, request,
+        text=text,
+        owner='info@example.org',
+        recipient=None,
+        notify=None,
+        origin='internal')
+    return message
 
 
 def test_ticket_pdf(org_app, handlers):
@@ -96,6 +157,8 @@ def test_ticket_pdf(org_app, handlers):
     reservation = scheduler.reservations_by_token(token).one()
     assert reservation
     ticket = open_ticket(request, token, 'RSV', owner)
+
+    add_ticket_message(request, ticket, long_text)
 
     assert ticket.handler.resource
 
