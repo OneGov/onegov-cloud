9a8663c6919adf47cb09b4dea5da9d98691ecdff
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-04-15 15:57:38 +0200

    Refactors site collection for re-use

--- src/onegov/org/models/sitecollection.py
+++ src/onegov/org/models/sitecollection.py
@@ -11,52 +11,64 @@ class SiteCollection(object):
     def __init__(self, session):
         self.session = session
 
-    def get(self):
-        # get the topics
-        topics = self.session.query(Topic)
+    @staticmethod
+    def get_topics(session):
+        topics = session.query(Topic)
         topics = topics.options(defer(Topic.meta))
         topics = topics.options(defer(Topic.content))
         topics = topics.options(defer(Topic.order))
         topics = topics.order_by(Topic.title)
+        return topics
 
-        # get the news
-        news = self.session.query(News)
+    @staticmethod
+    def get_news(session):
+        news = session.query(News)
         news = news.options(defer(News.meta))
         news = news.options(defer(News.content))
         news = news.options(defer(News.order))
         news = news.order_by(News.title)
+        return news
 
-        # get the forms
-        forms = self.session.query(FormDefinition)
+    @staticmethod
+    def get_forms(session):
+        forms = session.query(FormDefinition)
         forms = forms.options(defer(FormDefinition.definition))
         forms = forms.options(defer(FormDefinition.checksum))
         forms = forms.order_by(FormDefinition.title)
+        return forms
 
-        # get the resources
-        resources = self.session.query(Resource)
+    @staticmethod
+    def get_resources(session):
+        resources = session.query(Resource)
         resources = resources.options(defer(Resource.timezone))
         resources = resources.order_by(Resource.title)
+        return resources
 
-        # get the directories
-        directories = self.session.query(Directory)
+    @staticmethod
+    def get_directories(session):
+        directories = session.query(Directory)
         directories = directories.options(defer(Directory.meta))
         directories = directories.options(defer(Directory.content))
         directories = directories.options(defer(Directory.lead))
         directories = directories.options(defer(Directory.structure))
         directories = directories.options(defer(Directory.configuration))
         directories = directories.order_by(Directory.order)
+        return directories
 
-        # get the foto albums
-        imagesets = self.session.query(ImageSet)
+    @staticmethod
+    def get_imagesets(session):
+        imagesets = session.query(ImageSet)
         imagesets = imagesets.options(defer(ImageSet.meta))
         imagesets = imagesets.options(defer(ImageSet.content))
         imagesets = imagesets.order_by(ImageSet.title)
+        return imagesets
 
+    def get(self):
         return {
-            'topics': tuple(topics),
-            'news': tuple(news),
-            'forms': tuple(forms),
-            'resources': tuple(resources),
-            'directories': tuple(directories),
-            'imagesets': tuple(imagesets),
+            'topics': tuple(self.get_topics(self.session)),
+            'news': tuple(self.get_news(self.session)),
+            'forms': tuple(self.get_forms(self.session)),
+            'resources': tuple(self.get_resources(self.session)),
+            'directories': tuple(self.get_directories(self.session)),
+            'imagesets': tuple(self.get_imagesets(self.session)),
         }
