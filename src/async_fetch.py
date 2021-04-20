16b5d7e39cfcd0bd303e91f35ee8c779e687f939
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-04-20 23:26:47 +0200

    Extends url type to be an object or namedtuple

--- src/async_fetch.py
+++ src/async_fetch.py
@@ -1,18 +1,19 @@
-from typing import Union, Callable, Tuple, List, Any
+from typing import Union, Callable, Tuple, List, Any, Sequence, NamedTuple
 
 import aiohttp
 import asyncio
 
 ErrFunc = Callable[[str], Any]
 HandleExceptionType = Union[Tuple[ErrFunc], List[ErrFunc], ErrFunc]
+UrlType = Union[str, object]
 
 
-def raise_by_default(exception):
+def raise_by_default(url, exception):
     raise exception
 
 
 async def fetch(
-        url: str,
+        url: UrlType,
         session,
         response_attr: str = 'json',
         handle_exceptions: HandleExceptionType = raise_by_default
@@ -20,9 +21,16 @@ async def fetch(
     """
     Asynchronous get request. Pass handle_exceptions in order to get your
     desired error handling.
+
+    :param url: object that has an attribute url or a string
+    :param session: instance of aiohttp.ClientSession()
+    :param response_attr: valid, (awaitable) attribute for response object
+    :param handle_exceptions: list of callables of same length than urls
+    :return: response_attr or handled exception return for each url
     """
     try:
-        async with session.get(url) as response:
+        url_ = url if isinstance(url, str) else getattr(url, 'url')
+        async with session.get(url_) as response:
             response_attr = response_attr or 'json'
             attr = getattr(response, response_attr)
             if callable(attr):
@@ -39,38 +47,33 @@ async def fetch(
 
 async def fetch_many(
         loop,
-        urls: Union[list, tuple],
+        urls: Sequence[UrlType],
         response_attr: str = 'json',
         fetch_func: Callable = fetch,
         handle_exceptions: HandleExceptionType = raise_by_default
 ):
     """ Registers a task per url using the coroutine fetch_func with correct
-    signature.
-
-
-
-     """
+        signature. """
     async with aiohttp.ClientSession() as session:
 
-        def exception_handler(url):
+        def exception_handler(ix):
             if callable(handle_exceptions):
                 return handle_exceptions
-            return handle_exceptions[urls.index(url)]
+            return handle_exceptions[urls.index(ix)]
 
         return await asyncio.gather(*(
             loop.create_task(
-                fetch_func(url, session, response_attr, exception_handler(url))
+                fetch_func(url, session, response_attr, exception_handler(ix))
             )
-            for url in urls))
+            for ix, url in enumerate(urls)))
 
 
 def async_aiohttp_get_all(
-        urls: Union[list, tuple],
+        urls: Sequence[UrlType],
         response_attr: str = 'json',
         handle_exceptions: HandleExceptionType = raise_by_default
 ):
-    """
-    Performs asynchronous get requests.
+    """ Performs asynchronous get requests.
 
     Example only checking the status without awaiting content:
         result = async_aiohttp_get_all(test_urls, response_attr='status')
@@ -78,9 +81,8 @@ def async_aiohttp_get_all(
     Other valid response attributes are 'json' and 'text', which are awaited.
 
     If the callable of the exception handler function returns, it will be
-    part of the returned results.
+    part of the returned results. """
 
-    """
     loop = asyncio.get_event_loop()
     return loop.run_until_complete(
         fetch_many(
--- tests/test_async_fetch.py
+++ tests/test_async_fetch.py
@@ -2,6 +2,7 @@ import pytest
 from aiohttp import InvalidURL
 
 from async_fetch import async_aiohttp_get_all
+from onegov.core.utils import Bunch
 
 valid_urls = [
     'https://seantis.ch',
@@ -9,7 +10,8 @@ valid_urls = [
 ]
 
 invalid_urls = [
-    'invalid.url.com'
+    'invalid.url.com',
+    Bunch(url='xxx', name='Failed')
 ]
 
 
@@ -18,17 +20,20 @@ def test_fetch_all_invalid(url):
 
     def handle_invalid(url, exception):
         if isinstance(exception, InvalidURL):
+            if isinstance(url, Bunch):
+                return url.name
             return str(exception)
         else:
             raise exception
 
     with pytest.raises(InvalidURL):
-        async_aiohttp_get_all([url])
+        async_aiohttp_get_all(invalid_urls[0])
 
     results = async_aiohttp_get_all(
-        [url], handle_exceptions=handle_invalid)
+        invalid_urls, handle_exceptions=handle_invalid)
 
     assert isinstance(results[0], str)
+    assert results[1] == 'Failed'
 
 
 def test_fetch_all_valid():
