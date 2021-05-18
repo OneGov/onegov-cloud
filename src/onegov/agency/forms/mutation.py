4c1553ec7ed2dace76307f384a6d2bc914191943
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2021-05-18 07:52:12 +0200

    Agency: Use honeypots in mutuation form.
    TYPE: Feature
    LINK: SEA-288

--- src/onegov/agency/forms/mutation.py
+++ src/onegov/agency/forms/mutation.py
@@ -3,10 +3,13 @@ from onegov.form import Form
 from wtforms import validators
 from wtforms import TextAreaField
 from wtforms.fields.html5 import EmailField
+from onegov.form.fields import HoneyPotField
 
 
 class MutationForm(Form):
     """ Form to report a mutation of an organization. """
+
+    delay = HoneyPotField()
 
     email = EmailField(
         label=_("E-Mail"),
