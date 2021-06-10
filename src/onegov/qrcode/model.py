16607079f199af57705ce9dd3a7972c95c6fa4d6
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-06-10 17:19:12 +0200

    Adds support for encoding of the image

--- src/onegov/qrcode/model.py
+++ src/onegov/qrcode/model.py
@@ -1,3 +1,4 @@
+from base64 import b64encode
 from io import BytesIO
 
 from qrcode import QRCode, ERROR_CORRECT_H
@@ -10,6 +11,7 @@ class QrCodeMixin:
     _fill_color = 'black'
     _back_color = 'white'
     _format = 'png'
+    _encoding = None
 
     @property
     def image(self):
@@ -33,18 +35,30 @@ class QrCodeMixin:
         file.seek(0)
         return file
 
+    @property
+    def encoded_image(self):
+        if self.encoding == 'base64':
+            return b64encode(self.image.read())
+        return self.image.read()
+
+    @property
+    def content_type(self):
+        encoding = self.encoding and f';{self.encoding}' or ''
+        return f'image/{self.img_format}{encoding}'
+
 
 class QrCode(QrCodeMixin):
     """ Generates QR Codes """
 
     def __init__(self, payload, border=None, box_size=None, fill_color=None,
-                 back_color=None, img_format=None):
+                 back_color=None, img_format=None, encoding=None):
         self.payload = payload
         self.border = border or self._border
         self.box_size = box_size or self._box_size
         self.fill_color = fill_color or self._fill_color
         self.back_color = back_color or self._back_color
         self.img_format = img_format or self._format
+        self.encoding = encoding or self._encoding
 
     @classmethod
     def from_payload(cls, payload):
