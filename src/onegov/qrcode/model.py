954e757dfe0f122465cea5125d0c15b67817b210
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-06-10 13:32:20 +0200

    Extends QRCode class to be more flexible

--- src/onegov/qrcode/model.py
+++ src/onegov/qrcode/model.py
@@ -18,18 +18,18 @@ class QrCodeMixin:
         """
         qr = QRCode(
             error_correction=ERROR_CORRECT_H,
-            box_size=self._box_size,
-            border=self._border
+            box_size=self.box_size,
+            border=self.border
         )
         qr.add_data(self.payload)
         # Fit will determine the version depending on the size of the payload
         qr.make(fit=True)
         img = qr.make_image(
-            fill_color=self._fill_color,
-            back_color=self._back_color
+            fill_color=self.fill_color,
+            back_color=self.back_color
         )
         file = BytesIO()
-        img.save(file, format=self._format)
+        img.save(file, format=self.img_format)
         file.seek(0)
         return file
 
@@ -37,8 +37,14 @@ class QrCodeMixin:
 class QrCode(QrCodeMixin):
     """ Generates QR Codes """
 
-    def __init__(self, payload):
+    def __init__(self, payload, border=None, box_size=None, fill_color=None,
+                 back_color=None, img_format=None):
         self.payload = payload
+        self.border = border or self._border
+        self.box_size = box_size or self._box_size
+        self.fill_color = fill_color or self._fill_color
+        self.back_color = back_color or self._back_color
+        self.img_format = img_format or self._format
 
     @classmethod
     def from_payload(cls, payload):
