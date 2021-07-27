fad0bc5fbc6bb5d164415730afc755ad75d70801
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-07-27 14:58:30 +0200

    tmp

--- src/onegov/foundation6/cli.py
+++ src/onegov/foundation6/cli.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 from pathlib import Path
 import click
-
+from subprocess import check_output
 from onegov.core.cli import command_group
 from onegov.core.utils import module_path
 
@@ -11,7 +11,8 @@ cli = command_group()
 
 
 def pre_checks():
-    if 'v10' not in os.system('node --version'):
+    node_version = check_output('node --version', shell=True)
+    if 'v10' not in node_version.decode('utf-8'):
         click.secho('Foundation CLI currently works with node version 10')
         sys.exit()
 
@@ -38,11 +39,14 @@ def update():
      """
 
     pre_checks()
-    module = Path(module_path('onegov.foundation6', 'foundation'))
-    src = module / 'foundation'
-    vendor_src = module / 'vendor'
-    src_bckp = module / 'foundation.bak'
+    module = Path(module_path('onegov.foundation6', 'foundation')).parent
     assets = module / 'assets'
+    src = module / 'foundation'
+    vendor_src = module / 'foundation' / 'vendor'
+    # src_bckp = module / 'foundation.bak'
+
+    for p in (src, vendor_src, assets):
+        assert p.is_dir(), str(p)
 
     foundation_js_files = ('foundation.min.js', 'foundation.js')
 
@@ -56,28 +60,33 @@ def update():
     os.chdir('foundation-update')
     node_root = Path('/tmp/foundation-update/node_modules/foundation-sites')
 
-    click.secho('Create a backup', fg='green')
-    shutil.move(src, src_bckp)
+    # click.secho('Create a backup', fg='green')
+    # shutil.copytree(src, src_bckp)
 
     click.secho('Copy _vendor files')
-    shutil.copytree(node_root / '_vendor', src / '_vendor')
+    shutil.copytree(
+        node_root / '_vendor',
+        src / '_vendor',
+        dirs_exist_ok=True
+    )
 
-    click.secho('Copy scss files')
-    shutil.copytree(node_root / 'scss', src / 'scss')
-
-    click.secho('Copy foundation js files')
-    for name in foundation_js_files:
-        shutil.move(assets / name, assets / f'{name}.bak')
-        shutil.copyfile(
-            node_root / 'dist' / 'js' / name,
-            assets / name
-        )
-
-    click.secho('Copy motion-ui files')
-    mui_src = node_root.parent / 'motion-ui' / 'src'
-    mui_dest = vendor_src / 'motion-ui'
-    shutil.move(mui_dest, vendor_src / 'motion-ui.bak')
-    shutil.copytree(mui_src, mui_dest)
-
-    click.secho('Finished.', fg='green')
-    click.secho('Remove .bak folder/file manually.', fg='green')
+    # click.secho('Copy scss files')
+    # shutil.copytree(node_root / 'scss', src / 'scss', dirs_exist_ok=True)
+    #
+    # click.secho('Copy foundation js files')
+    # for name in foundation_js_files:
+    #     # shutil.move(assets / name, assets / f'{name}.bak')
+    #     shutil.copyfile(
+    #         node_root / 'dist' / 'js' / name,
+    #         assets / name
+    #     )
+    #
+    # click.secho('Copy motion-ui files')
+    # mui_src = node_root.parent / 'motion-ui' / 'src'
+    # assert mui_src.is_dir()
+    # mui_dest = vendor_src / 'motion-ui'
+    # # shutil.move(mui_dest, vendor_src / 'motion-ui.bak')
+    # shutil.copytree(mui_src, mui_dest)
+    #
+    # click.secho('Finished.', fg='green')
+    # click.secho('Remove .bak folder/file manually.', fg='green')
