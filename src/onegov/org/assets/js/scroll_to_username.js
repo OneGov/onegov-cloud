0d00666176349d1a66ba1dacf2eb368a3236866f
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-07-27 17:17:09 +0200

    Fixes scroll_to_username and check if elements are in view

--- src/onegov/org/assets/js/scroll_to_username.js
+++ src/onegov/org/assets/js/scroll_to_username.js
@@ -1,9 +1,29 @@
 // Scroll to the input username field
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  );
+}
+
 $(document).ready(function () {
     var target = $('input#username')[0] || $('input#email')[0]
-    if (target) {
+    if (target && !elementInViewport(target)) {
         $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: $(target).offset().top
         }, 'slow');
     }
 });
