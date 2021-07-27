bec1279ba974bc451b56f71bca98f612737e30f3
Lukas Burkhard <lukas.burkhard@seantis.ch>
2021-07-27 14:06:00 +0200

    Fix showing change net amount if payment was paid [skip ci]

--- src/onegov/org/views/ticket.py
+++ src/onegov/org/views/ticket.py
@@ -91,7 +91,7 @@ def view_ticket(self, request, layout=None):
 
     if payment and payment.source == 'manual':
         payment_button = manual_payment_button(payment, layout)
-        if request.is_manager:
+        if request.is_manager and not payment.paid:
             edit_amount_url = layout.csrf_protected_url(
                 request.link(payment, name='change-net-amount')
             )
