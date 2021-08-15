a30edf5b216dc0552eb291faf280379fd771519b
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2021-08-15 09:34:00 +0200

    Fix deprecation warning

--- tests/onegov/org/test_views_signing.py
+++ tests/onegov/org/test_views_signing.py
@@ -93,4 +93,4 @@ def test_sign_document(client):
     # deleting the signed file at this point should yield another message
     pdf = FileCollection(client.app.session()).query().one()
     client.get(f'/storage/{pdf.id}/details').click("Löschen")
-    assert b'Signierte Datei gel\u00f6scht' in client.get('/timeline').body
+    assert 'Signierte Datei gel\\u00f6scht' in client.get('/timeline').text
