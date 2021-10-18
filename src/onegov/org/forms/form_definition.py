d874db37984742735ea6ee20f61b40dd9b6c4dc4
Chantal Trutmann <chatru.work@gmail.com>
2021-10-18 11:02:39 +0200

    Add field in forms for pick-up

--- src/onegov/org/forms/form_definition.py
+++ src/onegov/org/forms/form_definition.py
@@ -29,6 +29,13 @@ class FormDefinitionBaseForm(Form):
         validators=[validators.InputRequired(), ValidFormDefinition()],
         render_kw={'rows': 32, 'data-editor': 'form'})
 
+    pick_up = TextAreaField(
+        label=_("Pick-Up"),
+        description=_("Describes how this resource can be picked up. "
+                      "This text is used on the ticket status page to "
+                      "inform the user")
+    )
+
 
 class FormDefinitionForm(merge_forms(
     FormDefinitionBaseForm,
