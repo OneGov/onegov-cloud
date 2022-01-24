b82d95d14e189d9f1ac799da091aa05a4c7d863f
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-01-24 11:06:04 +0100

    Allow change ids of items

--- src/onegov/election_day/forms/common.py
+++ src/onegov/election_day/forms/common.py
@@ -1,5 +1,37 @@
+from onegov.election_day import _
 from onegov.form import Form
+from wtforms import StringField
+from wtforms import ValidationError
+from wtforms.validators import InputRequired
 
 
 class EmptyForm(Form):
     pass
+
+
+class ChangeIdForm(Form):
+
+    callout = _(
+        "The ID is used in the URL and might be used somewhere. Changing the"
+        "ID might break links on external sites!"
+    )
+
+    id = StringField(
+        label=_("ID"),
+        validators=[
+            InputRequired()
+        ],
+    )
+
+    def validate_id(self, field):
+        if self.model.id != field.data:
+            query = self.request.session.query(self.model.__class__.id)
+            query = query.filter_by(id=field.data)
+            if query.first():
+                raise ValidationError(_('ID already exists'))
+
+    def update_model(self, model):
+        model.id = self.id.data
+
+    def apply_model(self, model):
+        self.id.data = model.id
