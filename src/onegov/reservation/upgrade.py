3c2cd185f4dfe5127f4477fab71320e590af129c
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-05-05 16:13:16 +0200

    Update reservation, requires new libres

--- src/onegov/reservation/upgrade.py
+++ src/onegov/reservation/upgrade.py
@@ -94,3 +94,12 @@ def add_default_view_to_existing_resource_types(context):
                 resource.default_view = 'month'
             else:
                 resource.default_view = 'agendaWeek'
+
+
+@upgrade_task('Make resource polymorphic type non-nullable')
+def make_resource_polymorphic_type_non_nullable(context):
+    context.operations.execute("""
+        UPDATE resources SET type = 'generic' WHERE type IS NULL;
+    """)
+
+    context.operations.alter_column('resources', 'type', nullable=False)
