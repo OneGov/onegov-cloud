489484c4d7fd58f90348ba15314c758456f70b16
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-07-11 08:42:19 +0200

    Winterthur: Fixes title not using translations.
    TYPE: Bugfix
    LINK: OGC-447

--- src/onegov/winterthur/views/shift_schedule.py
+++ src/onegov/winterthur/views/shift_schedule.py
@@ -3,7 +3,7 @@ import base64
 from onegov.core.security import Public
 from onegov.org.layout import DefaultLayout
 from onegov.org.models import Organisation
-from onegov.winterthur import WinterthurApp
+from onegov.winterthur import WinterthurApp, _
 
 
 @WinterthurApp.html(
@@ -20,7 +20,7 @@ def view_shift_schedule(self, request):
         image = f'data:image/png;base64,{image}'
 
     return {
-        'title': "Schichtplan",
+        'title': _('Shift schedule'),
         'layout': DefaultLayout(self, request),
         'image': image,
         'model': self
