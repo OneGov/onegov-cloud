9c70a649ef09aa100d056d2bd52bef8f95cbd1f4
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-08-11 09:48:59 +0200

    Fix news years filter

--- tests/onegov/town6/test_views_news.py
+++ tests/onegov/town6/test_views_news.py
@@ -5,81 +5,155 @@ from freezegun import freeze_time
 def test_news(client):
     client.login_admin().follow()
 
-    with freeze_time("2022-02-03 8:00"):
-        page = client.get('/news')
-        page = page.click('Nachricht')
-        page.form['title'] = "Corona-Virus: Status-Update from 28.01.2022"
-        page.form['lead'] = "First lead"
-        page.form['text'] = "First hashtag #onegov"
-        page.form['publication_start'] = datetime(2022, 1, 28, 8).isoformat()
-        page = page.form.submit().follow()
-        assert "28. Januar 2022" in page
-        assert "#onegov" in page
+    with freeze_time("2021-12-30 8:00"):
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 30.12.2021, no publication date'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #one'
+        assert '30. Dezember 2021' in page.form.submit().follow()
 
-    with freeze_time("2022-01-05 8:00"):
-        page = client.get('/news')
-        page = page.click('Nachricht')
-        page.form['title'] = "Corona-Virus: Status-Update from 31.12.2021"
-        page.form['lead'] = "Second lead"
-        page.form['text'] = "Second hashtag #pilatus"
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 30.12.2021, published 31.12.2021'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #two'
         page.form['publication_start'] = datetime(2021, 12, 31, 8).isoformat()
-        page = page.form.submit().follow()
-        assert "31. Dezember 2021" in page
-        assert "#pilatus" in page
+        assert '31. Dezember 2021' in page.form.submit().follow()
 
-    #  Test if both are in the overview
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 30.12.2021, published 1.1.2022'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #three'
+        page.form['publication_start'] = datetime(2022, 1, 1, 8).isoformat()
+        assert '1. Januar 2022' in page.form.submit().follow()
+
+    with freeze_time('2022-1-2 8:00'):
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 2.1.2022, no publication date'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #one'
+        assert '2. Januar 2022' in page.form.submit().follow()
+
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 2.1.2022, published 31.12.2021'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #two'
+        page.form['publication_start'] = datetime(2021, 12, 31, 8).isoformat()
+        assert '31. Dezember 2021' in page.form.submit().follow()
+
+        page = client.get('/news').click('Nachricht')
+        page.form['title'] = 'News created 2.1.2022, published 1.1.2022'
+        page.form['lead'] = 'Lead'
+        page.form['text'] = 'hashtag #three'
+        page.form['publication_start'] = datetime(2022, 1, 1, 8).isoformat()
+        assert '1. Januar 2022' in page.form.submit().follow()
+
+    # No filter
     page = client.get('/news')
-    assert "Corona-Virus: Status-Update from 28.01.2022" in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" in page
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' in page
+    assert 'News created 30.12.2021, published 1.1.2022' in page
+    assert 'News created 2.1.2022, no publication date' in page
+    assert 'News created 2.1.2022, published 31.12.2021' in page
+    assert 'News created 2.1.2022, published 1.1.2022' in page
 
-    #  Test if both are not in the overview with 2020 Year Filter
+    # 2020
     page = client.get('/news?filter_years=2020')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
-    #  Test if 2021 news are in the overview with Year Filter
+    # 2021
     page = client.get('/news?filter_years=2021')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" in page
-    # Test result = not in page
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
-    #  Test if 2022 news are in the overview with Year Filter
+    # 2022
     page = client.get('/news?filter_years=2022')
-    assert "Corona-Virus: Status-Update from 28.01.2022" in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
-    # Test result = in page
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' in page
+    assert 'News created 2.1.2022, no publication date' in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' in page
 
-    #  Test if news are in the overview with #onegov Hash Filter
-    page = client.get('/news?filter_tags=onegov')
-    assert "Corona-Virus: Status-Update from 28.01.2022" in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
+    # 2023
+    page = client.get('/news?filter_years=2023')
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
-    #  Test if news are in the overview with #pilatus Hash Filter
-    page = client.get('/news?filter_tags=pilatus')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" in page
+    # 2021 & 2022
+    page = client.get('/news?filter_years=2021&filter_years=2022')
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' in page
+    assert 'News created 30.12.2021, published 1.1.2022' in page
+    assert 'News created 2.1.2022, no publication date' in page
+    assert 'News created 2.1.2022, published 31.12.2021' in page
+    assert 'News created 2.1.2022, published 1.1.2022' in page
 
-    #  Test if news are in the overview with #onegov Hash Filter and Year 2021
-    page = client.get('/news?filter_tags=onegov&filter_years=2021')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
+    # #one
+    page = client.get('/news?filter_tags=one')
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
-    #  Test if news are in the overview with #onegov Hash Filter and Year 2022
-    page = client.get('/news?filter_tags=onegov&filter_years=2022')
-    assert "Corona-Virus: Status-Update from 28.01.2022" in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
+    # #two
+    page = client.get('/news?filter_tags=two')
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
-    #  Test if news are in the overview with #pilatus Hash Filter and Year 2021
-    page = client.get('/news?filter_tags=pilatus&filter_years=2021')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" in page
-    #  Test result = not in page
+    # #three
+    page = client.get('/news?filter_tags=three')
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' in page
 
-    #  Test if news are in the overview with #pilatus Hash Filter and Year 2022
-    page = client.get('/news?filter_tags=pilatus&filter_years=2022')
-    assert "Corona-Virus: Status-Update from 28.01.2022" not in page
-    assert "Corona-Virus: Status-Update from 31.12.2021" not in page
-    #  Test result = in page
+    # #four
+    page = client.get('/news?filter_tags=four')
+    assert 'News created 30.12.2021, no publication date' not in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
+
+    # #one & #two
+    page = client.get('/news?filter_tags=one&filter_tags=two')
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' in page
+    assert 'News created 2.1.2022, published 31.12.2021' in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
+
+    # 2021 & #one
+    page = client.get('/news?filter_years=2021&filter_tags=one')
+    assert 'News created 30.12.2021, no publication date' in page
+    assert 'News created 30.12.2021, published 31.12.2021' not in page
+    assert 'News created 30.12.2021, published 1.1.2022' not in page
+    assert 'News created 2.1.2022, no publication date' not in page
+    assert 'News created 2.1.2022, published 31.12.2021' not in page
+    assert 'News created 2.1.2022, published 1.1.2022' not in page
 
 
 def test_hide_news(client):
