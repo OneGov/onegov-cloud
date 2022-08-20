3e28a6680fe89e83d532b7abe0043595e88832d5
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-08-20 07:50:43 +0200

    First draft of rate limit [skip ci]

--- src/onegov/api/integration.py
+++ src/onegov/api/integration.py
@@ -9,6 +9,18 @@ class ApiApp(App):
 
     # todo: allow instance specific settings, probably using the filestorage?
 
+    @property
+    def rate_limit(self):
+        """ The number of requests per timedelta in seconds. """
+        return 10000, 15 * 60 * 60
+
+    @property
+    def rate_limit_cache(self):
+        """ A cache for rate limits. """
+
+        limit, expiration = self.rate_limit
+        return self.get_cache('rate_limits', expiration)
+
 
 @ApiApp.setting(section='api', name='endpoints')
 def get_api_endpoints():
--- src/onegov/api/views.py
+++ src/onegov/api/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from onegov.api import ApiApp
 from onegov.api.models import ApiEndpointCollection
 from onegov.api.models import ApiEndpoint
 from onegov.core.security import Public
+from webob.exc import HTTPTooManyRequests
 
 
 @ApiApp.json(
@@ -20,4 +22,24 @@ def view_api_endpoints(self, request):
     permission=Public
 )
 def view_api_endpoint(self, request):
+
+    # todo: add some headers
+    #  https://stackoverflow.com/questions/16022624/examples-of-http-api-
+    # rate-limiting-http-response-headers
+    limit, expiration = request.app.rate_limit
+    requests, timestamp = request.app.rate_limit_cache.get_or_create(
+        request.client_addr,
+        creator=lambda: (0, datetime.now()),
+    )
+    if (datetime.now() - timestamp).seconds < expiration:
+        requests += 1
+    else:
+        timestamp = datetime.now()
+        requests = 1
+    request.app.rate_limit_cache.set(
+        request.client_addr, (requests, timestamp)
+    )
+    if requests > limit:
+        raise HTTPTooManyRequests()
+
     return self.query(request)
