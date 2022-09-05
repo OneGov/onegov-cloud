39515de3bcd6eba668e9a0e7544cd631b945b973
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2022-09-05 14:55:03 +0200

    User: Revert back to using a string field instead of an email field for logins.
    TYPE: Bugfix
    LINK: OGC-591

--- src/onegov/user/forms/login.py
+++ src/onegov/user/forms/login.py
@@ -1,7 +1,6 @@
 from onegov.form import Form
 from onegov.user import _
 from wtforms.fields import PasswordField
-from wtforms.fields import EmailField
 from wtforms.fields import StringField
 from wtforms.validators import InputRequired
 
@@ -9,7 +8,7 @@ from wtforms.validators import InputRequired
 class LoginForm(Form):
     """ A generic login form for onegov.user """
 
-    username = EmailField(
+    username = StringField(
         label=_("E-Mail Address"),
         validators=[InputRequired()],
         render_kw={
