841125a788ad0b6f6ed8aaa83ce506ba99c781dc
Chantal Trutmann <chatru.work@gmail.com>
2022-09-21 13:34:53 +0200

    Remove address fields

--- src/onegov/people/upgrade.py
+++ src/onegov/people/upgrade.py
@@ -12,7 +12,6 @@ from onegov.people import AgencyMembership
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Text
-from sqlalchemy import String
 
 
 @upgrade_task('Rename academic_title to salutation')
@@ -165,20 +164,14 @@ def make_people_models_polymorphic_type_non_nullable(context):
             context.operations.alter_column(table, 'type', nullable=False)
 
 
-@upgrade_task('Add address columns to agency')
-def add_address_columns_to_agency(context):
-    if not context.has_column('agencies', 'street'):
-        context.operations.add_column('agencies', Column(
-            'street', Text, nullable=True
-        ))
-    if not context.has_column('agencies', 'zip_code'):
-        context.operations.add_column('agencies', Column(
-            'zip_code', String(length=10), nullable=True
-        ))
-    if not context.has_column('agencies', 'city'):
-        context.operations.add_column('agencies', Column(
-            'city', Text, nullable=True
-        ))
+@upgrade_task('Remove address columns to agency')
+def remove_address_columns_to_agency(context):
+    if context.has_column('agencies', 'street'):
+        context.operations.drop_column('agencies', 'street')
+    if context.has_column('agencies', 'zip_code'):
+        context.operations.drop_column('agencies', 'zip_code')
+    if context.has_column('agencies', 'city'):
+        context.operations.drop_column('agencies', 'city')
 
 
 @upgrade_task('Fix agency address column')
