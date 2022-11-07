a3cd5595fe31572e98c6ab99aff6350eecaaa28f
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2022-11-07 13:21:03 +0100

    Set maximum filename length of exported csv to 60

--- src/onegov/election_day/views/vote/data.py
+++ src/onegov/election_day/views/vote/data.py
@@ -48,7 +48,9 @@ def view_vote_data_as_csv(self, request):
     def add_last_modified(response):
         add_last_modified_header(response, self.last_modified)
 
+    max_filename_len = 60
+    filename = self.title[:max_filename_len]
     return {
         'data': self.export(sorted(request.app.locales)),
-        'name': normalize_for_url(self.title)
+        'name': normalize_for_url(filename)
     }
