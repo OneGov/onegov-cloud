8a3b755b12d8ca0a99c665ab9471f66d78c2d747
Cyrill Küttel <36520284+cyrillkuettel@users.noreply.github.com>
2023-01-03 09:27:13 +0100

    Town6: Fix html rendering in newsletters/new view
    TYPE: Bugfix
    LINK: OGC-734

--- src/onegov/org/forms/newsletter.py
+++ src/onegov/org/forms/newsletter.py
@@ -13,10 +13,10 @@ from wtforms.fields import StringField
 from wtforms.fields import TextAreaField
 from wtforms.validators import InputRequired
 from wtforms.validators import ValidationError
+from markupsafe import Markup
 
 
 class NewsletterForm(Form):
-
     title = StringField(
         label=_("Title"),
         description=_("Used in the overview and the e-mail subject"),
@@ -46,17 +46,16 @@ class NewsletterForm(Form):
 
         choices = tuple((
             str(item.id),
-            '<div class="title">{}</div><div class="date">{}</div>'.format(
-                item.title,
-                layout.format_date(item.created, 'relative')
-            )
+            Markup("""
+            <div class="title">{}</div><div class="date">{}</div>
+            """.format(item.title, layout.format_date(item.created,
+                                                      'relative')))
         ) for item in news)
 
         if not choices:
             return cls
 
         class NewsletterWithNewsForm(cls):
-
             news = MultiCheckboxField(
                 label=_("Latest news"),
                 choices=choices,
@@ -81,22 +80,18 @@ class NewsletterForm(Form):
 
         layout = Layout(None, request)
 
-        choices = tuple(
-            (
-                str(item.id),
-                '<div class="title">{}</div><div class="date">{}</div>'.format(
-                    item.title, layout.format_date(
-                        item.localized_start, 'datetime'
-                    )
-                )
-            ) for item in occurrences
-        )
+        choices = tuple((
+            str(item.id),
+            Markup("""
+            <div class="title">{}</div><div class="date">{}</div>
+            """.format(item.title, layout.format_date(item.localized_start,
+                                                      'datetime')))
+        )for item in occurrences)
 
         if not choices:
             return cls
 
         class NewsletterWithOccurrencesForm(cls):
-
             occurrences = MultiCheckboxField(
                 label=_("Events"),
                 choices=choices,
@@ -121,21 +116,18 @@ class NewsletterForm(Form):
 
         layout = Layout(None, request)
 
-        choices = tuple(
-            (
-                str(item.id),
-                '<div class="title">{}</div><div class="date">{}</div>'.format(
-                    name_without_extension(item.name),
-                    layout.format_date(item.created, 'date')
-                )
-            ) for item in publications
-        )
+        choices = tuple((
+            str(item.id),
+            Markup("""
+            <div class="title">{}</div><div class="date">{}</div>
+            """.format(name_without_extension(item.name),
+                       layout.format_date(item.created, 'date')))
+        ) for item in publications)
 
         if not choices:
             return cls
 
         class NewsletterWithPublicationsForm(cls):
-
             publications = MultiCheckboxField(
                 label=_("Publications"),
                 choices=choices,
@@ -157,7 +149,6 @@ class NewsletterForm(Form):
 
 
 class NewsletterSendForm(Form):
-
     send = RadioField(
         _("Send"),
         choices=(
@@ -209,7 +200,6 @@ class NewsletterTestForm(Form):
         choices = tuple((r.id.hex, r.address) for r in recipients)
 
         class NewsletterSendFormWithRecipients(cls):
-
             selected_recipient = ChosenSelectField(
                 label=_("Recipient"),
                 choices=choices,
