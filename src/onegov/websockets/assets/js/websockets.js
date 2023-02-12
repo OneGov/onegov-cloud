4d1f7ea474ce4925266d0aa161e3204fe6f171ca
Marc Sommerhalder <msom@users.noreply.github.com>
2023-02-12 12:49:07 +0100

    Websockets: Add websocket server and client.
    TYPE: Feature
    LINK: OGC-935

---
+++ src/onegov/websockets/assets/js/websockets.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", function() {
+    if (typeof(WebsocketConfig) !== "undefined") {
+      try {
+        const websocket = new WebSocket(WebsocketConfig.endpoint);
+        websocket.addEventListener("open", function() {
+          let payload = {
+            type: "register",
+            schema: WebsocketConfig.schema
+          }
+          websocket.send(JSON.stringify(payload));
+        });
+        websocket.addEventListener('message', function(message) {
+            const data = JSON.parse(message.data)
+            if (data.type === 'notification') {
+                if (data.message.event === 'refresh') {
+                    const path = data.message.path;
+                    if (WebsocketConfig.onrefresh && path && window.location.href.search(path) !== -1) {
+                        WebsocketConfig.onrefresh(data.message);
+                    }
+                }
+            }
+        });
+      } catch (error) {}
+  }
+});
---
+++ src/onegov/websockets/server.py
@@ -0,0 +1,175 @@
+from asyncio import Future
+from json import dumps
+from json import loads
+from onegov.websockets import log
+from websockets import broadcast
+from websockets import serve
+
+
+CONNECTIONS = {}
+TOKEN = ''
+
+
+def get_payload(message, expected):
+    """ Deserialize JSON payload and check type. """
+
+    try:
+        payload = loads(message)
+        assert payload['type'] in expected
+        return payload
+    except Exception:
+        pass
+
+
+async def error(websocket, message, close=True):
+    """ Sends an error. """
+
+    await websocket.send(
+        dumps({
+            'type': 'error',
+            'message': message
+        })
+    )
+    if close:
+        await websocket.close()
+
+
+async def acknowledge(websocket):
+    """ Sends an acknowledge. """
+
+    await websocket.send(
+        dumps({
+            'type': 'acknowledged'
+        })
+    )
+
+
+async def handle_listen(websocket, payload):
+    """ Handles listening clients. """
+
+    assert payload.get('type') == 'register'
+
+    schema = payload.get('schema')
+    if not schema or not isinstance(schema, str):
+        await error(websocket, f'invalid schema: {schema}')
+        return
+
+    await acknowledge(websocket)
+
+    log.debug(f'{websocket.id} listens @ {schema}')
+    connections = CONNECTIONS.setdefault(schema, set())
+    connections.add(websocket)
+    try:
+        await websocket.wait_closed()
+    finally:
+        connections = CONNECTIONS.setdefault(schema, set())
+        if websocket in connections:
+            connections.remove(websocket)
+
+
+async def handle_authentication(websocket, payload):
+    """ Handles authentication. """
+
+    assert payload.get('type') == 'authenticate'
+
+    token = payload.get('token')
+    if not token or not isinstance(token, str):
+        await error(websocket, 'invalid token')
+        return
+    if token != TOKEN:
+        await error(websocket, 'authentication failed')
+        return
+
+    await acknowledge(websocket)
+
+    log.debug(f'{websocket.id} authenticated')
+
+
+async def handle_status(websocket, payload):
+    """ Handles status requests. """
+
+    assert payload.get('type') == 'status'
+
+    await acknowledge(websocket)
+
+    await websocket.send(
+        dumps({
+            'type': 'status',
+            'message': {
+                'connections': {
+                    key: len(values)
+                    for key, values in CONNECTIONS.items()
+                }
+            }
+        })
+    )
+
+    log.debug(f'{websocket.id} status sent')
+
+
+async def handle_broadcast(websocket, payload):
+    """ Handles broadcasts. """
+
+    assert payload.get('type') == 'broadcast'
+
+    message = payload.get('message')
+    schema = payload.get('schema')
+    if not schema or not isinstance(schema, str):
+        await error(websocket, f'invalid schema: {schema}')
+        return
+    if not message:
+        await error(websocket, 'missing message')
+        return
+
+    await acknowledge(websocket)
+
+    connections = CONNECTIONS.get(schema, set())
+    if connections:
+        broadcast(
+            connections,
+            dumps({
+                'type': 'notification',
+                'message': message
+            })
+        )
+
+    log.debug(
+        f'{websocket.id} sent {message}'
+        f' to {len(connections)} receiver(s) @ {schema}'
+    )
+
+
+async def handle_manage(websocket, payload):
+    """ Handles managing clients. """
+
+    await handle_authentication(websocket, payload)
+
+    async for message in websocket:
+        payload = get_payload(message, ('broadcast', 'status'))
+        if payload and payload['type'] == 'broadcast':
+            await handle_broadcast(websocket, payload)
+        elif payload and payload['type'] == 'status':
+            await handle_status(websocket, payload)
+        else:
+            await error(websocket, f'invalid command: {message}')
+
+
+async def handle_start(websocket):
+    log.debug(f'{websocket.id} connected')
+    message = await websocket.recv()
+    payload = get_payload(message, ('authenticate', 'register'))
+    if payload and payload['type'] == 'authenticate':
+        await handle_manage(websocket, payload)
+    elif payload and payload['type'] == 'register':
+        await handle_listen(websocket, payload)
+    else:
+        await error(websocket, f'invalid command: {message}')
+    log.debug(f'{websocket.id} disconnected')
+
+
+async def main(host, port, token):
+    global TOKEN
+    TOKEN = token
+    log.debug(f'Serving on ws://{host}:{port}')
+    async with serve(handle_start, host, port):
+        await Future()
