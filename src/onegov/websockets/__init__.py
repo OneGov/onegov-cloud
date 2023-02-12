368732ce59ae14d245d7abc6827ad6caad0a0621
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2023-02-12 11:47:39 +0100

    Add module to docs [skip ci]

--- src/onegov/websockets/__init__.py
+++ src/onegov/websockets/__init__.py
@@ -1,3 +1,12 @@
+"""
+
+Websocket server and client to send and receive broadcast messages.
+
+The websocket server can be started using the CLI command ``serve``.
+
+The websocket client can be used by using the `WebsocketsApp` integration.
+
+"""
 import logging
 log = logging.getLogger('onegov.websockets')  # noqa
 log.addHandler(logging.NullHandler())  # noqa
