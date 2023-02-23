077669cb6a7e1753ccceaea386b79711628fd38c
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-02-23 15:09:14 +0100

    Adding the form to move pages

--- src/onegov/org/forms/page.py
+++ src/onegov/org/forms/page.py
@@ -1,7 +1,11 @@
+from onegov.core.security import Private
 from onegov.form import Form
+from onegov.form.fields import ChosenSelectField
 from onegov.org import _
 from onegov.org.forms.fields import HtmlField
 from onegov.org.forms.generic import ChangeAdjacencyListUrlForm
+from onegov.page.collection import PageCollection
+from unidecode import unidecode
 from wtforms.fields import BooleanField
 from wtforms.fields import StringField
 from wtforms.fields import TextAreaField
@@ -55,3 +59,72 @@ class PageUrlForm(ChangeAdjacencyListUrlForm):
 
     def get_model(self):
         return self.model.page
+
+
+class MovePageForm(Form):
+    """ Form to move a page including its sub pages. """
+
+    parent_id = ChosenSelectField(
+        label=_("Destination"),
+        choices=[],
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    def on_request(self):
+        self.request.include('common')
+        self.request.include('chosen')
+
+        pages = PageCollection(self.request.session)
+        self.iterate_page_tree(pages.roots, indent='',
+                               page_list=self.parent_id.choices)
+
+        # adding root element
+        if self.request.has_permission(pages, Private):
+            self.parent_id.choices.insert(
+                0, ('root', self.request.translate(_("- Root -")))
+            )
+
+    def iterate_page_tree(self, pages, indent='', page_list=None):
+        """
+        Iterates over the page tree and lists the elements with ident
+        to show the page hierarchy in the choices list
+        """
+        for page in pages:
+            if self.request.has_permission(page, Private):
+                item = (str(page.id), f'{indent} {unidecode(page.title)}')
+                page_list.append(item)
+
+            self.iterate_page_tree(page.children, indent=indent + ' -',
+                                   page_list=page_list)
+
+    def update_model(self, model):
+        session = self.request.session
+        agencies = PageCollection(session)
+
+        parent_id = None
+        parent = None
+        if self.parent_id.data and self.parent_id.data.isdigit():
+            parent_id = int(self.parent_id.data)
+            parent = agencies.by_id(parent_id)
+        model.name = agencies.get_unique_child_name(model.title, parent)
+        model.parent_id = parent_id
+
+    def apply_model(self, model):
+
+        def remove(item):
+            item = (str(item.id), item.title)
+            if item in self.parent_id.choices:
+                self.parent_id.choices.remove(item)
+
+        def remove_with_children(item):
+            remove(item)
+            for child in item.children:
+                remove_with_children(child)
+
+        if model.parent:
+            remove(model.parent)
+        else:
+            self.parent_id.choices.pop(0)
+        remove_with_children(model)
