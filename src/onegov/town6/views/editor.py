e1afa5f8ee2f7d953f3699f1a9b0a7e1e6a1ca26
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-02-23 15:13:31 +0100

    Extend editor to forward moving a page in town6 using org functionality

--- src/onegov/town6/views/editor.py
+++ src/onegov/town6/views/editor.py
@@ -1,14 +1,19 @@
 from onegov.core.security import Private
-from onegov.org.views.editor import get_form_class
+from onegov.org.views.editor import get_form_class, handle_move_page
 from onegov.org.views.editor import handle_page_form, view_topics_sort
 from onegov.town6 import TownApp
 from onegov.org.models import Editor
-from onegov.town6.layout import EditorLayout
+from onegov.town6.layout import EditorLayout, PageLayout
 
 
 @TownApp.form(model=Editor, template='form.pt', permission=Private,
               form=get_form_class)
 def town_handle_page_form(self, request, form):
+    if self.action == 'move':
+        # make use of implementation in org/views/editor
+        return handle_move_page(self, request, form,
+                                layout=PageLayout(self.page, request))
+
     return handle_page_form(
         self, request, form, EditorLayout(self, request, site_title=None)
     )
