90d9083586cb29e8bc0b272fbbbbe1aec8cb9b00
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-02-28 12:12:21 +0100

    Hide Gever Upload in navbar if it is not set in settings

--- tests/onegov/town6/test_views_forms.py
+++ tests/onegov/town6/test_views_forms.py
@@ -232,3 +232,36 @@ def test_form_group_sort(client):
 
     assert groups == page.pyquery(
         '.page-content-main h2').text().strip().split(' ')
+
+
+def test_navbar_links_visibility(client):
+    collection = FormCollection(client.app.session())
+    collection.definitions.add('Profile', definition=textwrap.dedent("""
+        First name * = ___
+        Last name * = ___
+        E-Mail * = @@@
+    """), type='custom')
+
+    transaction.commit()
+
+    client.login_admin()
+
+    page = client.get("/forms").click("Profile")
+    page.form["first_name"] = "Foo"
+    page.form["last_name"] = "Bar"
+    page.form["e_mail"] = "admin@example.org"
+    page = page.form.submit().follow().form.submit().follow()
+    ticket_number = page.pyquery(".ticket-number").text()
+    page = client.get("/tickets/ALL/open").click(ticket_number)
+    # the Gever upload button should not be shown ...
+    assert "Hochladen auf Gever" not in page
+
+    settings = client.get('/settings').click('Gever API')
+    settings.form['gever_username'] = 'foo'
+    settings.form['gever_password'] = 'bar'
+    settings.form['gever_endpoint'] = 'https://example.org/'
+    settings.form.submit()
+
+    page = client.get("/tickets/ALL/open").click(ticket_number)
+    # ... until it has been activated in settings
+    assert "Hochladen auf Gever" in page
