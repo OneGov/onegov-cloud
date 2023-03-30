4ed4c26411c9ded4bdc342415d06162cffbfe4fc
Marc Sommerhalder <msom@users.noreply.github.com>
2023-03-30 14:55:00 +0200

    enable eslint pre-commit hook and enable es6, specify react version, use explicit rule severities [skip ci]

--- src/onegov/websockets/assets/js/websockets.js
+++ src/onegov/websockets/assets/js/websockets.js
@@ -1,19 +1,17 @@
 var openWebsocket = function(endpoint, schema, channel, onnotifcation) {
-  try {
     const websocket = new WebSocket(endpoint);
     websocket.addEventListener("open", function() {
-      let payload = {
-        type: "register",
-        schema: schema,
-        channel: channel
-      }
-      websocket.send(JSON.stringify(payload));
+        const payload = {
+            type: "register",
+            schema: schema,
+            channel: channel
+        };
+        websocket.send(JSON.stringify(payload));
     });
     websocket.addEventListener('message', function(message) {
-      const data = JSON.parse(message.data)
-      if (data.type === 'notification') {
-          onnotifcation(data.message, websocket);
-      }
+        const data = JSON.parse(message.data);
+        if (data.type === 'notification') {
+            onnotifcation(data.message, websocket);
+        }
     });
-  } catch (error) {}
 };
