5a1b177e20182a28a86e2e32772219d683e281c5
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-04-25 16:30:49 +0200

    open the default mail client with all translators in bcc field

--- src/onegov/translator_directory/assets/js/translator_directory.js
+++ src/onegov/translator_directory/assets/js/translator_directory.js
@@ -2,3 +2,14 @@
 function updateUploadUrl(protected_url) {
     $('form.upload').attr("action", protected_url);
 }
+
+
+const bulk_email_button = document.querySelectorAll('a.button.envelope.tiny')[0];
+bulk_email_button.addEventListener('click', function (event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    // could even show the number of recipients in the message
+    showAlertMessage(locale("Opened mail client with all emails in BCC"), 'success');
+    window.location.href = href;
+});
+
