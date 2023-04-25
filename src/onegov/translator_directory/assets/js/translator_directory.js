6fb0c9d20a957ff00683a8163e5036fac4cc6c8e
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-04-25 17:23:12 +0200

    Simplify [skip ci]

--- src/onegov/translator_directory/assets/js/translator_directory.js
+++ src/onegov/translator_directory/assets/js/translator_directory.js
@@ -5,11 +5,6 @@ function updateUploadUrl(protected_url) {
 
 const bulk_email_button = document.querySelectorAll('a.button.envelope.tiny')[0];
 bulk_email_button.addEventListener('click', function(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    // could even show the number of recipients in the message
-    const message = locale("The circular to translators has been opened in your e-mail software.");
     showAlertMessage(message, 'success');
-    window.location.href = href;
 });
 
