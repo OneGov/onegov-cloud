1cb23e7c8c0ec5123420aa26bf64cc93e46f28b1
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-05-04 08:23:13 -0400

    Adding geo coordinates for agencies

--- src/onegov/agency/api.py
+++ src/onegov/agency/api.py
@@ -3,6 +3,7 @@ from onegov.agency.collections import ExtendedPersonCollection
 from onegov.agency.collections import PaginatedAgencyCollection
 from onegov.agency.collections import PaginatedMembershipCollection
 from onegov.api import ApiEndpoint
+from onegov.gis import Coordinates
 
 
 class ApisMixin:
@@ -18,6 +19,11 @@ class ApisMixin:
     @cached_property
     def membership_api(self):
         return MembershipApiEndpoint(self.app)
+
+
+def get_geo_location(item):
+    geo = item.content.get('coordinates', Coordinates())
+    return dict(lon=geo.lon, lat=geo.lat, zoom=geo.zoom)
 
 
 class PersonApiEndpoint(ApiEndpoint, ApisMixin):
@@ -106,6 +112,7 @@ class AgencyApiEndpoint(ApiEndpoint, ApisMixin):
             'phone': item.phone,
             'phone_direct': item.phone_direct,
             'opening_hours': item.opening_hours,
+            'geo_location': get_geo_location(item),
         }
 
     def item_links(self, item):
