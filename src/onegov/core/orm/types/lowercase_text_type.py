f040daf2c1db7846ab263cb1273dad34a74344ac
David Salvisberg <david.salvisberg@seantis.ch>
2023-05-17 13:58:46 +0200

    mypy: Adds stubs for onegov.core.orm

--- src/onegov/core/orm/types/lowercase_text_type.py
+++ src/onegov/core/orm/types/lowercase_text_type.py
@@ -2,14 +2,24 @@ from sqlalchemy.types import TypeDecorator, TEXT
 from sqlalchemy_utils.operators import CaseInsensitiveComparator
 
 
-class LowercaseText(TypeDecorator):
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from sqlalchemy.engine.interfaces import Dialect
+    _Base = TypeDecorator[str]
+else:
+    _Base = TypeDecorator
+
+
+class LowercaseText(_Base):
     """ Text column that forces all text to be lowercase. """
 
     impl = TEXT
     omparator_factory = CaseInsensitiveComparator
 
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            return value.lower()
+    def process_bind_param(
+        self,
+        value: str | None,
+        dialect: 'Dialect'
+    ) -> str | None:
 
-        return value
+        return None if value is None else value.lower()
--- src/onegov/core/orm/types/utcdatetime_type.py
+++ src/onegov/core/orm/types/utcdatetime_type.py
@@ -1,8 +1,17 @@
 from sedate import standardize_date, to_timezone
-from sqlalchemy import types
+from sqlalchemy.types import DateTime, TypeDecorator
 
 
-class UTCDateTime(types.TypeDecorator):
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from datetime import datetime
+    from sqlalchemy.engine.interfaces import Dialect
+    _Base = TypeDecorator[datetime]
+else:
+    _Base = TypeDecorator
+
+
+class UTCDateTime(_Base):
     """ Stores dates as UTC.
 
     Internally, they are stored as timezone naive, because Postgres takes
@@ -11,15 +20,27 @@ class UTCDateTime(types.TypeDecorator):
 
     """
 
-    impl = types.DateTime
+    impl = DateTime
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(timezone=False)
 
-    def process_bind_param(self, value, engine):
-        if value is not None:
-            return to_timezone(value, 'UTC').replace(tzinfo=None)
+    def process_bind_param(  # type:ignore[override]
+        self,
+        value: 'datetime | None',
+        dialect: 'Dialect'
+    ) -> 'datetime | None':
 
-    def process_result_value(self, value, engine):
-        if value is not None:
-            return standardize_date(value, timezone='UTC')
+        if value is None:
+            return None
+        return to_timezone(value, 'UTC').replace(tzinfo=None)
+
+    def process_result_value(
+        self,
+        value: 'datetime | None',
+        dialect: 'Dialect'
+    ) -> 'datetime | None':
+
+        if value is None:
+            return None
+        return standardize_date(value, timezone='UTC')
