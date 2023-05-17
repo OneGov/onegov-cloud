516f216e2766f663610599598aff92b51c1142ac
David Salvisberg <david.salvisberg@seantis.ch>
2023-05-17 11:52:57 +0200

    mypy: Adds stubs for onegov.core.orm

--- src/onegov/core/orm/utils.py
+++ src/onegov/core/orm/utils.py
@@ -1,14 +1,20 @@
 from sqlalchemy_utils import QueryChain as QueryChainBase
 
 
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+# FIXME: We should create stubs for QueryChainBase
 class QueryChain(QueryChainBase):
     """ Extends SQLAlchemy Utils' QueryChain with some extra methods. """
 
-    def slice(self, start, end):
+    def slice(self, start: int | None, end: int | None) -> 'Self':
         return self[start:end]
 
-    def first(self):
+    def first(self) -> Any | None:
         return next((o for o in self), None)
 
-    def all(self):
+    def all(self) -> tuple[Any, ...]:
         return tuple(self)
