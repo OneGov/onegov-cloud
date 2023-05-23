26c11f3be94b712a40fb1b6b3bb4cbcaf79db18a
David Salvisberg <david.salvisberg@seantis.ch>
2023-05-23 15:27:00 +0200

    Tests out just the faster pipeline.
    Defer dynamic pipeline until ci-agent container is rebuilt with support
    for uploading a specific pipeline file other than pipeline.yml

--- .buildkite/container-pipeline.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-./.buildkite/cat .buildkite/container-pipeline.yml | buildkite-agent pipeline upload
--- .buildkite/generic-pipeline.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-./.buildkite/cat .buildkite/generic-pipeline.yml | buildkite-agent pipeline upload
