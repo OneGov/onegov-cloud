c6ac19e2530c81fe365575eaa0342c98312126c5
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-06-12 14:05:32 +0200

    Adds cronjob to import DWS events for Winterthur daily

--- src/onegov/winterthur/cronjobs.py
+++ src/onegov/winterthur/cronjobs.py
@@ -1,3 +1,8 @@
+import os
+import requests
+
+from onegov.core.utils import module_path
+from onegov.event import EventCollection
 from onegov.winterthur.app import WinterthurApp
 from onegov.winterthur.collections import AddressCollection
 
@@ -5,3 +10,32 @@ from onegov.winterthur.collections import AddressCollection
 @WinterthurApp.cronjob(hour=15, minute=50, timezone='Europe/Zurich')
 def update_streets_directory(request):
     AddressCollection(request.session).update()
+
+
+@WinterthurApp.cronjob(hour='02', minute='00', timezone='Europe/Zurich')
+def import_dws_vk(request):
+    """
+    Download ics file from DWS and import the events daily.
+    https://dwswinterthur.ch/index.php/tipps-fuer-vereine/dws-sprtagenda
+
+    NOTE: typo in website url is correct (June 2023)
+    """
+    ical_url = 'https://www.google.com/calendar/ical/dwskalender%40gmail.com' \
+               '/public/basic.ics'
+    response = requests.get(ical_url, timeout=30)
+    if not response.status_code == 200:
+        raise Exception(f'Failed to retrieve DWS events from {ical_url}')
+
+    icon_name = 'Veranstaltung_breit.jpg'
+    icon_path = module_path('onegov.winterthur', 'static') + os.sep + icon_name
+    file = open(icon_path, 'rb')
+
+    # import events from response
+    collection = EventCollection(request.session)
+    added, updated, purged = \
+        collection.from_ical(response.content,
+                             future_events_only=True,
+                             event_image=file)
+    print(f"Events successfully imported "
+          f"({len(added)} added, {len(updated)} updated, "
+          f"{len(purged)} deleted)")
