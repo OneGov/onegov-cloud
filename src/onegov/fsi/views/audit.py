1a131d3905ece0cb91fb9dff31e03db05152d833
Chantal Trutmann <79152553+BreathingFlesh@users.noreply.github.com>
2023-06-12 15:44:42 +0200

    FSI: Add option for reminder mails
    TYPE: Feature
    LINK: OGC-1037

--- src/onegov/fsi/views/audit.py
+++ src/onegov/fsi/views/audit.py
@@ -7,6 +7,7 @@ from onegov.fsi.collections.audit import AuditCollection
 from onegov.fsi.forms.audit import AuditForm
 from onegov.fsi.layouts.audit import AuditLayout
 from onegov.fsi.pdf import FsiPdf
+from onegov.fsi.models import CourseAttendee
 from sedate import utcnow
 from webob import Response
 
@@ -65,6 +66,21 @@ def invite_attendees_for_event(self, request, form):
         ) for letter in self.used_letters
     )
 
+    next_subscriptions = self.next_subscriptions(request)
+
+    recipients = self.cached_subset if self.course_id else []
+    recipients = [r.id for r in recipients if not (
+        next_subscriptions.get(r.id, False) or r.event_completed)]
+
+    all_attendees = self.session.query(CourseAttendee).filter(
+        CourseAttendee.id.in_(recipients)
+    ).all()
+
+    email_recipients = ('; '.join([a.email for a in all_attendees])
+                        if len(all_attendees) < 100 else False)
+
+    subject = request.translate(_('Reminder due course registration'))
+
     return {
         'layout': layout,
         'model': self,
@@ -73,7 +89,9 @@ def invite_attendees_for_event(self, request, form):
         'button_text': _('Update'),
         'now': now,
         'letters': letters,
-        'next_subscriptions': self.next_subscriptions(request)
+        'email_recipients': email_recipients,
+        'subject': subject,
+        'next_subscriptions': next_subscriptions
     }
 
 
