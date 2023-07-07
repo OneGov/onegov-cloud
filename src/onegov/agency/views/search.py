7c4fe206fa49b3dcd31bfc4e93635ba8db3ef0e6
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-07-07 14:26:40 +0200

    Adds postgres search view to agency

--- src/onegov/agency/views/search.py
+++ src/onegov/agency/views/search.py
@@ -1,13 +1,23 @@
 from onegov.agency import AgencyApp
 from onegov.agency.layout import AgencySearchLayout
 from onegov.core.security import Public
-from onegov.org.models import Search
+from onegov.org.models import Search, SearchPostgres
 from onegov.org.views.search import search as search_view
+from onegov.org.views.search import search_postgres as search_postgres_view
 
 
 @AgencyApp.html(model=Search, template='search.pt', permission=Public)
-def search(self, request):
+def agency_search(self, request):
     data = search_view(self, request)
+    if isinstance(data, dict):
+        data['layout'] = AgencySearchLayout(self, request)
+    return data
+
+
+@AgencyApp.html(model=SearchPostgres, template='search_postgres.pt',
+                permission=Public)
+def agency_search_postgres(self, request):
+    data = search_postgres_view(self, request)
     if isinstance(data, dict):
         data['layout'] = AgencySearchLayout(self, request)
     return data
