82635490e25cc86c6f4c73970a0591222fd06ace
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-07-24 07:11:24 +0200

    Revert "Add upgrade step for column renaming tickets.group"
    This reverts commit 44de655340999926bc18a60a8503d343399b0354.

--- src/onegov/search/upgrade.py
+++ src/onegov/search/upgrade.py
@@ -6,11 +6,3 @@ upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
 from onegov.core.upgrade import upgrade_task
 
 
-@upgrade_task('Rename tickets group column')
-def rename_tickets_group_column(context):
-    if context.has_column('tickets', 'group'):
-        context.operations.alter_column(
-            table_name='tickets',
-            column_name='group',
-            new_column_name='group_title'
-        )
