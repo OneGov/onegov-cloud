25c35443510c756125157360f266c8fc25100d86
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-07-25 16:32:32 +0200

    Revert "Fix tsvector generation for tickets as 'group' is a reserved word in prostgres. Renaming column"
    This reverts commit 5709ff599e17ccf13d11a434c45d5d714d794c54.

--- src/onegov/search/upgrade.py
+++ src/onegov/search/upgrade.py
@@ -2,16 +2,3 @@
 upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
 
 """
-from onegov.core.upgrade import upgrade_task
-
-
-@upgrade_task('rename tickets column group to group_name')
-def rename_tickets_column_group_to_group_name(context):
-    # Renaming the column group to group_name as 'group' is a reserved word
-    # in postgres and reindexing fails while generating the tsvector string
-
-    if context.has_column('tickets', 'group'):
-        context.session.execute("""
-            ALTER TABLE tickets
-            RENAME COLUMN group to group_name;
-        """)
