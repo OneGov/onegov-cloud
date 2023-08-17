8cdb63fda846ce0a4c6c790c1ee66046d15acda7
Chantal Trutmann <chatru.work@gmail.com>
2023-08-17 12:27:50 +0200

    Scroll sidebar with content

--- src/onegov/town6/app.py
+++ src/onegov/town6/app.py
@@ -196,6 +196,8 @@ def get_common_asset():
     yield 'aos.css'
     yield 'homepage_video.js'
     yield 'notifications.js'
+    yield 'jquery.sticky-sidebar.js'
+    yield 'sidebar_fixed.js'
 
 
 @TownApp.webasset('editor')
---
+++ src/onegov/town6/assets/js/sidebar_fixed.js
@@ -0,0 +1,6 @@
+if ($('.sidebar-content-wrapper') && $('.sidebar-wrapper')) {
+    $('.sidebar-wrapper').stickySidebar({
+        topSpacing: 115,
+        innerWrapperSelector: '.sidebar-content-wrapper'
+        });
+}
