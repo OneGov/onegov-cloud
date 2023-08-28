b8e9a67a85d5b74a3d817bd7a9e1f6a74568db6f
Chantal Trutmann <chatru.work@gmail.com>
2023-08-28 15:27:58 +0200

    Fix copying of sidebar on large screens

--- src/onegov/town6/assets/js/sidebar_mobile.js
+++ src/onegov/town6/assets/js/sidebar_mobile.js
@@ -1,4 +1,3 @@
-if ($('.sidebar-content-wrapper').length){
-    $('.sidebar-toggler').css('display','block');
-    $(".sidebar-content-wrapper").clone().appendTo("#offCanvasSidebar");
+if ($('.sidebar-content-wrapper').length && $('.sidebar-toggler').css('display') == 'block') {
+    $(".sidebar-content-wrapper").detach().appendTo("#offCanvasSidebar");
 }
