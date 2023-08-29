950ed48007df3b93d9cdc0f5a4b7277089610176
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-08-29 23:39:32 +0200

    fix linting

--- src/onegov/org/models/ticket.py
+++ src/onegov/org/models/ticket.py
@@ -166,7 +166,9 @@ class FormSubmissionHandler(Handler, TicketDeletionMixin):
 
     @property
     def group(self):
-        return self.submission.form.title if self.submission is not None else ''
+        return (
+            self.submission.form.title if self.submission is not None else ''
+        )
 
     @property
     def payment(self):
