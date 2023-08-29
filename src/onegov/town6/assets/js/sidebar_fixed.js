5f9207688bd16e28fd4dd1c4a22b0cb68cb0b047
Chantal Trutmann <chatru.work@gmail.com>
2023-08-29 18:37:11 +0200

    Landsgemeinde: Remove merge comments

--- src/onegov/town6/assets/js/sidebar_fixed.js
+++ src/onegov/town6/assets/js/sidebar_fixed.js
@@ -1,10 +1,7 @@
 if ($('.sidebar-content-wrapper').length){
     $('.sidebar-wrapper').stickySidebar({
         topSpacing: 115,
-<<<<<<< HEAD
         bottomSpacing: 150,
-=======
->>>>>>> master
         innerWrapperSelector: '.sidebar-content-wrapper'
         });
 }
