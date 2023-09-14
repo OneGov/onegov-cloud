133be4e803e41e3a7561ae0a02f441ec4ca7f499
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-09-14 14:05:55 +0200

    Revert "test build not failing with this layout"
    This reverts commit 89d03089a91db88af95feb1c8ed1ad1ce25f17f1.

--- src/onegov/town6/views/occurrence.py
+++ src/onegov/town6/views/occurrence.py
@@ -4,6 +4,7 @@ from onegov.core.security import Public, Private, Secret
 
 from onegov.event import Occurrence, OccurrenceCollection
 from onegov.org.forms.event import EventConfigurationForm
+from onegov.town6.layout import OccurrenceLayout
 from onegov.org.views.occurrence import view_occurrences, view_occurrence, \
     export_occurrences, import_occurrences, handle_edit_event_filters
 from onegov.town6 import TownApp
@@ -19,7 +20,7 @@ def town_view_occurrences(self, request):
 
 @TownApp.html(model=Occurrence, template='occurrence.pt', permission=Public)
 def town_view_occurrence(self, request):
-    layout = OccurrencesLayout(self, request)
+    layout = OccurrenceLayout(self, request)
     request.include('monthly-view')
     return view_occurrence(self, request, layout)
 
