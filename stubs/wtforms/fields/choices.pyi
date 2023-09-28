b2f01ce5164241dbb1e76184032f9bdd1a092306
David Salvisberg <david.salvisberg@seantis.ch>
2023-09-28 13:28:16 +0200

    mypy: Updates WTForms stubs with some fixes

--- stubs/wtforms/fields/choices.pyi
+++ stubs/wtforms/fields/choices.pyi
@@ -13,13 +13,15 @@ _Choice: TypeAlias = tuple[Any, str]
 _GroupedChoices: TypeAlias = dict[str, Iterable[_Choice]]
 _FullChoice: TypeAlias = tuple[Any, str, bool]  # value, label, selected
 _FullGroupedChoices: TypeAlias = tuple[str, Iterable[_FullChoice]]
+_Option: TypeAlias = SelectFieldBase._Option
 
 class SelectFieldBase(Field):
+    option_widget: _Widget[_Option]
     def __init__(
         self,
         label: str | None = None,
         validators: tuple[_Validator[_FormT, Self], ...] | list[Any] | None = None,
-        option_widget: _Widget[SelectFieldBase._Option] | None = None,
+        option_widget: _Widget[_Option] | None = None,
         *,
         filters: Sequence[_Filter] = (),
         description: str = "",
@@ -40,12 +42,10 @@ class SelectFieldBase(Field):
     def iter_choices(self) -> Iterator[_FullChoice]: ...
     def has_groups(self) -> bool: ...
     def iter_groups(self) -> Iterator[_FullGroupedChoices]: ...
-    def __iter__(self) -> Iterator[SelectFieldBase._Option]: ...
+    def __iter__(self) -> Iterator[_Option]: ...
 
     class _Option(Field):
         checked: bool
-
-    option_widget: _Widget[_Option]
 
 class SelectField(SelectFieldBase):
     coerce: Callable[[Any], Any]
