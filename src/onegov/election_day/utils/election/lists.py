851159db58bbf01e2c72a538ac654397caec7b7a
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2023-10-07 16:00:59 +0200

    Election Day: Remove untested and failing view.
    TYPE: Bugfix

--- src/onegov/election_day/utils/election/lists.py
+++ src/onegov/election_day/utils/election/lists.py
@@ -1,99 +1,12 @@
-from collections import defaultdict
 from collections import OrderedDict
-from itertools import groupby
 from onegov.ballot import ElectionResult
 from onegov.ballot import List
 from onegov.ballot import ListResult
-from onegov.core.orm import as_selectable_from_path
-from onegov.core.utils import module_path
 from onegov.election_day import _
-from onegov.election_day.utils.common import LastUpdatedOrderedDict
 from sqlalchemy import desc
 from sqlalchemy import func
-from sqlalchemy import select
 from sqlalchemy.orm import object_session
 from sqlalchemy.sql.expression import case
-
-
-def get_aggregated_list_results(election, session, use_checks=False):
-    if election.type == 'majorz':
-        return {}
-
-    agg_lr_query = as_selectable_from_path(
-        module_path(
-            'onegov.election_day', 'queries/aggregated_list_results.sql'))
-
-    agg_lr = agg_lr_query.c
-    query = select(agg_lr).where(agg_lr.election_id == election.id)
-    result = session.execute(query)
-
-    data = LastUpdatedOrderedDict({})
-
-    # checks
-    lst_ids = set()
-    lst_list_ids = set()
-    lst_names = set()
-    validations = defaultdict(list)
-    summary = OrderedDict()
-
-    for lid, g in groupby(result, lambda l: l.id):
-        for lst in g:
-            # checks
-            lst_ids.add(lst.id)
-            lst_list_ids.add(lst.list_id)
-            lst_names.add(lst.name)
-
-            summary.setdefault('election_list_votes',
-                               int(lst.election_list_votes))
-            summary.setdefault('election_candidate_votes',
-                               int(lst.election_candidate_votes))
-
-            key = lst.name
-            data.setdefault(
-                key,
-                {
-                    'name': lst.name,
-                    'list_id': lst.list_id,
-                    'votes': lst.list_votes,
-                    'total_candidate_votes': int(lst.candidate_votes_by_list),
-                    'perc_to_total_votes': float(lst.perc_to_total_votes),
-                    'number_of_mandates': lst.number_of_mandates
-                    if election.completed else 0,
-                    'candidates': [],
-                }
-            )
-            data[key]['candidates'].append({
-                'family_name': lst.family_name,
-                'first_name': lst.first_name,
-                'total_votes': lst.candidate_votes,
-                'perc_to_list_votes': float(lst.perc_to_list_votes)
-            })
-
-    # all of these must be unique for an election
-    assert len(lst_ids) == len(lst_list_ids)
-    assert len(lst_list_ids) == len(lst_names)
-
-    total_list_votes = 0
-
-    for name, item in data.items():
-        if item['votes'] < item['total_candidate_votes']:
-            validations['errors'].append(
-                f'List {name} has more candidates votes than list votes.'
-            )
-        total_list_votes += item['votes']
-
-    assert summary['election_list_votes'] == total_list_votes
-
-    if summary['election_list_votes'] < summary['election_candidate_votes']:
-        validations['errors'].append(
-            "The election has less list votes than candidate votes"
-        )
-
-    return {
-        'summary': summary,
-        'validations': validations,
-        'results': data
-    }
 
 
 def get_list_results(election, limit=None, names=None, sort_by_names=None,
