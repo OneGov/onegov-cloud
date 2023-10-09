04df37c807941b47866171d90497a8cf103ba5c5
Chantal Trutmann <chatru.work@gmail.com>
2023-10-09 21:29:39 +0200

    Fix typo

--- src/onegov/town6/assets/js/sidebar_mobile.js
+++ src/onegov/town6/assets/js/sidebar_mobile.js
@@ -2,7 +2,7 @@ if (!$('.sidebar-content-wrapper').length) {
     $('.sidebar-toggler').css('display', 'none');
 }
 
-if ($('.sidebar-toggler').css('d    isplay') === 'block') {
+if ($('.sidebar-toggler').css('display') === 'block') {
     $(".sidebar-content-wrapper").detach().appendTo("#offCanvasSidebar");
     $('.off-canvas a').click(function() { $('.off-canvas').foundation('close'); });
 }
