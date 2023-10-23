45b0b450d4ab7b8c99d0afe4705599728fcb7dd9
Chantal Trutmann <chatru.work@gmail.com>
2023-10-23 08:50:22 +0200

    rename view

--- src/onegov/town6/views/chat.py
+++ src/onegov/town6/views/chat.py
@@ -7,5 +7,5 @@ from onegov.town6.layout import DefaultLayout
 
 
 @TownApp.html(model=ChatCollection, template='chats.pt', permission=Public)
-def town_view_people(self, request):
+def town_view_chat(self, request):
     return view_chat(self, request, DefaultLayout(self, request))
