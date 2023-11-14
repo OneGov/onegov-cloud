16c1ef15fd81d3075da87b43e4b6a06cf00de2c9
Chantal Trutmann <chatru.work@gmail.com>
2023-11-14 11:10:27 +0100

    Add chat initiation link to button

--- src/onegov/town6/views/homepage.py
+++ src/onegov/town6/views/homepage.py
@@ -2,13 +2,19 @@ from onegov.core.security import Public, Private
 from onegov.org import _
 from onegov.org.views.homepage import view_org
 from onegov.org.models import Organisation
+from onegov.chat.collections import ChatCollection
 from onegov.town6 import TownApp
 from onegov.town6.layout import HomepageLayout
 
 
 @TownApp.html(model=Organisation, template='homepage.pt', permission=Public)
 def town_view_org(self, request):
-    return view_org(self, request, HomepageLayout(self, request))
+    view = view_org(self, request, HomepageLayout(self, request))
+    chats = ChatCollection(request.session)
+    chat_link = request.link(chats, 'initiate')
+
+    view['chat_link'] = chat_link
+    return view
 
 
 @TownApp.html(
