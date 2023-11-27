0fddbf791a4437264c071268976a334ab6b6369a
Chantal Trutmann <chatru.work@gmail.com>
2023-11-27 09:57:40 +0100

    Fix sidebar

--- src/onegov/town6/assets/js/sidebar_fixed.js
+++ src/onegov/town6/assets/js/sidebar_fixed.js
@@ -2,13 +2,17 @@ if ($('.sidebar-content-wrapper').length){
 $(window).on("load", function() {
 
     var header_height = $('.nav-bar-sticky').height();
-    var footer_height = $('#footer').height();
 
     var content_lc = $('#content > .grid-x > div:first-child > *:last-child');
     var content_bottom = content_lc.offset().top + content_lc.height();
 
-    var sidebar_lc = $('.sidebar > *:last-child');
+    var sidebar_lc = $('.sidebar-content-wrapper > *:last-child');
     var sidebar_bottom = sidebar_lc.offset().top + sidebar_lc.height();
+
+    console.log('content', content_lc)
+    console.log('content height', content_bottom)
+    console.log('sidebar', sidebar_lc)
+    console.log('sidebar height', sidebar_bottom)
 
     if (content_bottom > sidebar_bottom) {
         $('.sidebar-wrapper').stickySidebar({
