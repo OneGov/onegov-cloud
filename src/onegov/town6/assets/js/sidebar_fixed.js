048510e5b0216195009a5a22106dcbba5d82812d
Chantal Trutmann <chatru.work@gmail.com>
2024-01-08 15:36:32 +0100

    Adjust rest of templates for sidebar

--- src/onegov/town6/assets/js/sidebar_fixed.js
+++ src/onegov/town6/assets/js/sidebar_fixed.js
@@ -1,7 +1,15 @@
 var header_height = $('.nav-bar-sticky').height();
 
+var content_lc = $('#content > .grid-x > div:first-child > *:last-child');
+var content_bottom = content_lc.offset().top + content_lc.height();
+
+var sidebar_lc = $('.sidebar-wrapper > *:last-child');
+var sidebar_bottom = sidebar_lc.offset().top + sidebar_lc.height();
+
 jQuery(document).ready(function() {
+  if (content_bottom > sidebar_bottom) {
     jQuery('.content, .sidebar').theiaStickySidebar({
       additionalMarginTop: header_height
     });
-  });
+  }
+});
