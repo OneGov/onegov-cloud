e1cac2badc23ff4fb593387b450c5a9ea60aa550
Chantal Trutmann <chatru.work@gmail.com>
2024-01-30 14:16:46 +0100

    Fix date in tests

--- tests/onegov/town6/test_notifications.py
+++ tests/onegov/town6/test_notifications.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import transaction
 
 from datetime import datetime
+from locale import setlocale, LC_TIME
 from onegov.org.models import ResourceRecipientCollection
 from onegov.reservation import ResourceCollection
 from onegov.ticket import TicketCollection
@@ -66,6 +67,10 @@ def test_new_reservation_notification(client):
     ticket = ticket.click("Annehmen").follow()
     ticket.click("Alle Reservationen annehmen")
 
+    setlocale(LC_TIME, 'de_DE.UTF-8')
+    today = datetime.now()
+    today_string = today.strftime('%A, %d. %B %Y')
+
     # two mails for Jessie, one for John and none for Paul
     assert len(os.listdir(client.app.maildir)) == 3
 
@@ -76,13 +81,13 @@ def test_new_reservation_notification(client):
                     or "Ihre Reservationen wurden angenommen" in
                     mail['Subject'])
             assert "Gymnasium" in mail['TextBody']
-            assert "Montag, 29. Januar 2024" in mail['TextBody']
+            assert today_string in mail['TextBody']
             assert "Anzahl:" in mail['TextBody']
             assert "Foobar" in mail['TextBody']
         if mail['To'] == "john@example.org":
             assert "Neue Reservation(en)" in mail['Subject']
             assert "Gymnasium" in mail['TextBody']
-            assert "Montag, 29. Januar 2024" in mail['TextBody']
+            assert today_string in mail['TextBody']
             assert "Anzahl:" in mail['TextBody']
             assert "Foobar" in mail['TextBody']
         if mail['To'] == "paul@example.org":
