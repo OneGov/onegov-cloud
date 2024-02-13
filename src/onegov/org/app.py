a005371cfb0d8b5ab7e4692fdd64b0748e4934cb
Chantal Trutmann <chatru.work@gmail.com>
2024-02-13 16:05:54 +0100

    Fix order and remove include for 'many'

--- src/onegov/org/app.py
+++ src/onegov/org/app.py
@@ -745,17 +745,17 @@ def get_common_asset() -> 'Iterator[str]':
     yield 'form_dependencies.js'
     yield 'confirm.jsx'
     yield 'typeahead.jsx'
+    yield 'jquery.datetimepicker.js'
+    yield 'datetimepicker.js'
     yield 'many.jsx'
     yield 'pay'
     yield 'moment.js'
     yield 'moment.de-ch.js'
     yield 'moment.fr-ch.js'
-    yield 'jquery.datetimepicker.js'
     yield 'jquery.mousewheel.js'
     yield 'jquery.popupoverlay.js'
     yield 'jquery.load.js'
     yield 'videoframe.js'
-    yield 'datetimepicker.js'
     yield 'url.js'
     yield 'date-range-selector.js'
     yield 'lazyalttext.js'
@@ -769,7 +769,6 @@ def get_common_asset() -> 'Iterator[str]':
     yield 'items_selectable.js'
     yield 'notifications.js'
     yield 'foundation.accordion.js'
-
 
 
 @OrgApp.webasset('fontpreview')
