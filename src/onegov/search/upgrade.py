aa8005b7960af63cd6ae1f6f144305343a3e5ad4
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-02-16 13:55:58 +0100

    Adds upgrade step for fts index columns

--- src/onegov/search/upgrade.py
+++ src/onegov/search/upgrade.py
@@ -2,3 +2,22 @@
 upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
 
 """
+from sqlalchemy import Text, Column
+from sqlalchemy.dialects.postgresql import TSVECTOR
+
+from onegov.core.orm import Base
+from onegov.core.upgrade import upgrade_task
+from onegov.search.utils import searchable_sqlalchemy_models
+
+
+@upgrade_task('Adds fts index and fts index data columns to all models')
+def adds_index_data_columns(context):
+    for model in searchable_sqlalchemy_models(Base):
+        if not context.has_column(model.__tablename__, 'fts_idx'):
+            context.operations.add_column(
+                model.__tablename__, Column('fts_idx', TSVECTOR, default='')
+            )
+        if not context.has_column(model.__tablename__, 'fts_idx_data'):
+            context.operations.add_column(
+                model.__tablename__, Column('fts_idx_data', Text, default='')
+            )
