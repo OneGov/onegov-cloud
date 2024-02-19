00a13c0f3ac9f810f5798ac15e635cee734844eb
Marc Sommerhalder <msom@users.noreply.github.com>
2024-02-19 08:38:09 +0100

    Election Day: Remove experimental eCH-0252 export.
    TYPE: Feature
    LINK: OGC-1151

--- src/onegov/election_day/formats/exports/vote/__init__.py
+++ src/onegov/election_day/formats/exports/vote/__init__.py
@@ -1,10 +1,7 @@
-from onegov.election_day.formats.exports.vote.ech_0252 import (
-    export_vote_ech_0252)
 from onegov.election_day.formats.exports.vote.internal import (
     export_vote_internal)
 
 
 __all__ = [
-    'export_vote_ech_0252',
     'export_vote_internal',
 ]
