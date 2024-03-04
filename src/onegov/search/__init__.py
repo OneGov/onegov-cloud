bd39ec800dac6dabe5419a4d5a92dc77bcad6383
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-03-04 07:33:15 +0100

    Adds NullHandler to index logger

--- src/onegov/search/__init__.py
+++ src/onegov/search/__init__.py
@@ -3,6 +3,7 @@ log = logging.getLogger('onegov.search')
 log.addHandler(logging.NullHandler())
 
 index_log = logging.getLogger('onegov.search.index')
+index_log.addHandler(logging.NullHandler())
 
 from onegov.search.mixins import Searchable, ORMSearchable, SearchableContent
 from onegov.search.dsl import Search
