c1235b7d3f7cd952a4c13118e59fa18c78dc4cac
David Salvisberg <david.salvisberg@seantis.ch>
2024-03-13 13:11:24 +0100

    mypy: Adds type hints for `onegov.fsi`

--- src/onegov/fsi/initial_content.py
+++ src/onegov/fsi/initial_content.py
@@ -3,7 +3,16 @@ from onegov.org.initial_content import load_content, add_pages
 from onegov.org.models import Organisation
 
 
-def create_new_organisation(app, name, locale='de_CH'):
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from onegov.fsi.app import FsiApp
+
+
+def create_new_organisation(
+    app: 'FsiApp',
+    name: str,
+    locale: str = 'de_CH'
+) -> Organisation:
     assert locale == 'de_CH'
 
     path = module_path('onegov.fsi', 'content/de.yaml')
