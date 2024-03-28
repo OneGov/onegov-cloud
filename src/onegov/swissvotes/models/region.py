b0c38e013e1b6b4cf84b0882430260773b3a979e
Marc Sommerhalder <msom@users.noreply.github.com>
2024-03-28 11:54:59 +0100

    Update src/onegov/swissvotes/models/region.py
    Co-authored-by: David Salvisberg <david.salvisberg@seantis.ch>

--- src/onegov/swissvotes/models/region.py
+++ src/onegov/swissvotes/models/region.py
@@ -99,9 +99,7 @@ class Region:
         }.get(self.name, self.name)
 
     def html(self, request: 'SwissvotesRequest') -> str:
-        return Markup(  # noqa: MS001
-            '<span title="{}">{}</span>'.format(
-                request.translate(self.label),
-                request.translate(self.abbreviation)
-            )
+        return Markup( '<span title="{}">{}</span>').format(
+            request.translate(self.label),
+            request.translate(self.abbreviation)
         )
