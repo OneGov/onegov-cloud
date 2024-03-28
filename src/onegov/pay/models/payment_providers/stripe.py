ab2c1d205aecbd2e3a16ec98a47beec2e705353f
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-03-28 09:14:16 +0100

    Adds check for business name in account object

--- src/onegov/pay/models/payment_providers/stripe.py
+++ src/onegov/pay/models/payment_providers/stripe.py
@@ -269,7 +269,7 @@ class StripeConnect(PaymentProvider[StripePayment]):
     def public_identity(self) -> str:
         account = self.account
         assert account is not None
-        if account.business_name:
+        if 'business_name' in account:
             return f'{account.business_name} / {account.email}'
         return account.email
 
