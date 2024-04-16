fa859428f38cde9796a3c90e7a159547993082fd
Marc Sommerhalder <msom@users.noreply.github.com>
2024-04-16 10:26:15 +0200

    Enable chameleon cache for development

--- src/onegov/server/cli.py
+++ src/onegov/server/cli.py
@@ -506,7 +506,8 @@ class WsgiServer(FileSystemEventHandler):
 
     def spawn(self) -> WsgiProcess:
         return WsgiProcess(self.app_factory, self._host, self._port, {
-            'ONEGOV_DEVELOPMENT': '1'
+            'ONEGOV_DEVELOPMENT': '1',
+            'CHAMELEON_CACHE': '.chameleon_cache'
         }, **self.kwargs)
 
     def join(self, timeout: float | None = None) -> None:
