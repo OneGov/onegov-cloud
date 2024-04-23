1d7df063e16e1b938da0b4e61bc51fe2ef989015
David Salvisberg <david.salvisberg@seantis.ch>
2024-04-23 13:19:48 +0200

    Core: Avoids writing `.tmp` files to the SMS queue altogether
    TYPE: Bugfix

--- src/onegov/core/framework.py
+++ src/onegov/core/framework.py
@@ -1161,6 +1161,10 @@ class Framework(
         if not os.path.exists(path):
             os.makedirs(path)
 
+        tmp_path = os.path.join(self.sms_directory, 'tmp')
+        if not os.path.exists(tmp_path):
+            os.makedirs(tmp_path)
+
         if isinstance(receivers, str):
             receivers = [receivers]
 
@@ -1186,7 +1190,12 @@ class Framework(
                 path, f'{index}.{len(receiver_batch)}.{timestamp}'
             )
 
-            FileDataManager.write_file(payload, dest_path)
+            tmp_dest_path = os.path.join(
+                tmp_path,
+                f'{self.schema}-{index}.{len(receiver_batch)}.{timestamp}'
+            )
+
+            FileDataManager.write_file(payload, dest_path, tmp_dest_path)
 
     def send_zulip(self, subject: str, content: str) -> PostThread | None:
         """ Sends a zulip chat message asynchronously.
