3e63d26b5a6acba4864a69050dc04894861bcc4f
Chantal Trutmann <chatru.work@gmail.com>
2024-05-13 08:34:49 +0200

    Mute the video, so it starts playing.

--- src/onegov/landsgemeinde/views/agenda.py
+++ src/onegov/landsgemeinde/views/agenda.py
@@ -75,6 +75,7 @@ def view_agenda_item(
             video_url, 'start', str(request.params['start']))
         video_url = append_query_param(video_url, 'autoplay', '1')
         video_url = append_query_param(video_url, 'allow', '"autoplay"')
+        video_url = append_query_param(video_url, 'mute', '1')
 
     return {
         'layout': layout,
