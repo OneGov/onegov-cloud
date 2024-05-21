9f26be1ea5db6bbf406888877151eabce02a44d0
Chantal Trutmann <chatru.work@gmail.com>
2024-05-21 09:12:30 +0200

    Detect Window-Resize

--- src/onegov/town6/assets/js/sidebar_mobile.js
+++ src/onegov/town6/assets/js/sidebar_mobile.js
@@ -1,8 +1,12 @@
-if (!$('.sidebar-wrapper').length) {
-    $('.sidebar-toggler').css('display', 'none');
-}
 
-if ($('.sidebar-toggler').css('display') === 'block') {
-    $(".sidebar-wrapper").detach().appendTo("#offCanvasSidebar");
-    $('.off-canvas a').click(function() { $('.off-canvas').foundation('close'); });
-}
+// detect if window is resized
+$(window).resize(function() {
+    if (!$('.sidebar-wrapper').length) {
+        $('.sidebar-toggler').css('display', 'none');
+        $(".sidebar-wrapper").detach().appendTo("#right-sidebar");
+    }
+    if ($('.sidebar-toggler').css('display') === 'block') {
+        $(".sidebar-wrapper").detach().appendTo("#offCanvasSidebar");
+        $('.off-canvas a').click(function() { $('.off-canvas').foundation('close'); });
+    }
+});
