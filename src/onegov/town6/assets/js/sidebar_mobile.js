81a15c92bb0fc7c1bf69c59b8f78101323105fd9
Chantal Trutmann <chatru.work@gmail.com>
2024-05-22 09:12:56 +0200

    Show content on window resize without having to refresh

--- src/onegov/town6/assets/js/sidebar_mobile.js
+++ src/onegov/town6/assets/js/sidebar_mobile.js
@@ -1,12 +1,3 @@
+$('.off-canvas a').click(function() { $('.off-canvas').foundation('close'); });
 
-// detect if window is resized
-$(window).resize(function() {
-    if (!$('.sidebar-wrapper').length) {
-        $('.sidebar-toggler').css('display', 'none');
-        $(".sidebar-wrapper").detach().appendTo("#right-sidebar");
-    }
-    if ($('.sidebar-toggler').css('display') === 'block') {
-        $(".sidebar-wrapper").detach().appendTo("#offCanvasSidebar");
-        $('.off-canvas a').click(function() { $('.off-canvas').foundation('close'); });
-    }
-});
+$(".sidebar-wrapper").clone().appendTo("#offCanvasSidebar");
