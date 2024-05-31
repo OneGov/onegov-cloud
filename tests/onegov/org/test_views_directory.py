390c58fe53f422b81b8cb44337463e920130a6b1
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-05-31 15:59:14 +0200

    Rework tests for town6, org

--- tests/onegov/org/test_views_directory.py
+++ tests/onegov/org/test_views_directory.py
@@ -937,32 +937,34 @@ def test_change_directory_url(client):
     assert 'Das Formular enthält Fehler' in page
 
 
-def create_faq_directory(client, title='Meetings'):
-    page = (client.get('/directories').
-            click('Frequently Asked Questions Directory'))
-    page.form['title'] = title
-    return page.form.submit().follow()
+def test_create_directory_accordion_layout(client):
 
+    def create_directory(client, title):
+        page = (client.get('/directories').
+                click('Verzeichnis'))
+        page.form['title'] = title
+        page.form['structure'] = "Question *= ___\nAnswer *= ___"
+        page.form['title_format'] = '[Question]'
+        page.form['layout'] = 'accordion'
+        return page.form.submit().follow()
 
-def test_create_faq_directory(client):
     client.login_admin()
     title = "Questions and Answers about smurfs"
 
-    faq_dir = create_faq_directory(client, title)
+    faq_dir = create_directory(client, title)
     assert title in faq_dir
 
     question = "Are smurfs real?"
     answer = "Yes, they are."
-    q1 = faq_dir.click('Eintrag', index=0)
+    q1 = faq_dir.click('Eintrag')
     q1.form['question'] = question
     q1.form['answer'] = answer
     q1 = q1.form.submit().follow()
     assert question in q1
     assert answer not in q1
-    assert 'Eintrag vorschlagen' not in q1
 
     question = "Who is the boss of the smurfs?"
-    q2 = faq_dir.click('Eintrag', index=0)
+    q2 = faq_dir.click('Eintrag')
     q2.form['question'] = question
     q2.form['answer'] = 'Papa Schlumpf'
     q2 = q2.form.submit().follow()
