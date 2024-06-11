df7a91728a22736fda716abd7a799e6b7979bda8
David Salvisberg <david.salvisberg@seantis.ch>
2024-06-11 13:49:31 +0200

    Uses a different http port for every xdist worker

--- tests/onegov/org/conftest.py
+++ tests/onegov/org/conftest.py
@@ -120,7 +120,18 @@ def browser(browser, org_app_url):
 @pytest.fixture(scope='function')
 def org_app_url(request, org_app):
     org_app.print_exceptions = True
-    server = WSGIServer(application=org_app)
+    try:
+        from xdist import get_xdist_worker_id
+        worker_id = get_xdist_worker_id(request)
+        worker_id = worker_id.removeprefix('gw')
+        if worker_id.isnumeric():
+            port = 8080 + int(worker_id)
+        else:
+            port = 0
+    except ImportError:
+        port = 0
+
+    server = WSGIServer(application=org_app, port=port)
     server.start()
     yield server.url
     server.stop()
