30b18b23f90225750d89ea458c781596812c5729
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-07-03 09:44:33 +0200

    Extend collection

--- src/onegov/people/collections/people.py
+++ src/onegov/people/collections/people.py
@@ -1,3 +1,5 @@
+from functools import cached_property
+
 from onegov.core import utils
 from onegov.core.collection import GenericCollection
 from onegov.people.models import Person
@@ -55,3 +57,18 @@ class PersonCollection(BasePersonCollection[Person]):
     @property
     def model_class(self) -> type[Person]:
         return Person
+
+    @cached_property
+    def unique_organisations(self) -> list[str]:
+        query = self.session.query(Person.organisation)
+        query = query.filter(Person.organisation.isnot(None)).distinct()
+        query = query.order_by(Person.organisation)
+        return tuple(p.organisation for p in query if p.organisation != '')
+
+    @cached_property
+    def unique_sub_organisations(self) -> list[str]:
+        query = self.session.query(Person.sub_organisation)
+        query = query.filter(Person.sub_organisation.isnot(None)).distinct()
+        query = query.order_by(Person.sub_organisation)
+        return tuple(p.sub_organisation
+                     for p in query if p.sub_organisation != '')
