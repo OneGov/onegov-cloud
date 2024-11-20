ef6a0dad64aa998e867ceee97e414310de3d0b76
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-11-20 14:55:30 +0100

    use fallback

--- src/onegov/file/filters.py
+++ src/onegov/file/filters.py
@@ -1,9 +1,12 @@
+import os
 import shlex
 import subprocess
 import logging
 from depot.fields.interfaces import FileFilter
 from depot.io.utils import file_from_content
 from io import BytesIO
+
+from onegov.core.utils import module_path
 from onegov.file.utils import IMAGE_MIME_TYPES, get_image_size
 from pathlib import Path
 from PIL import Image
@@ -245,4 +248,17 @@ class WithPDFThumbnailFilter(WithThumbnailFilter):
         # FIXME: This is kinda slow. We should be able to render the
         #        PDF directly at the thumbnail size. Maybe we should
         #        use pdf2image rather than roll our own?
-        return super().generate_thumbnail(self.generate_preview(fp))
+        try:
+            return super().generate_thumbnail(self.generate_preview(fp))
+        except Exception as e:
+            log.warning(f"Thumbnail generation failed: {str(e)}")
+            fallback = BytesIO()
+            icon_path = (
+                    module_path('onegov.org', 'static/pdf_preview')
+                    + os.sep
+                    + 'thumbnail_medium_pdf_preview_fallback.png'
+            )
+            with open(icon_path, 'rb') as f:
+                fallback.write(f.read())
+            fallback.seek(0)
+            return super().generate_thumbnail(fallback)
