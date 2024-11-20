247b65a4bb093b90b38105cbe33f790744106059
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2024-11-20 18:16:40 +0100

    clean up [skip ci]

--- tests/onegov/file/conftest.py
+++ tests/onegov/file/conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from depot.manager import DepotManager
 
-from onegov.core.utils import module_path
-
 
 @pytest.fixture(scope='function', autouse=True)
 def depot(temporary_directory):
@@ -14,17 +12,3 @@ def depot(temporary_directory):
     yield DepotManager.get()
 
     DepotManager._clear()
-
-
-@pytest.fixture(scope='function')
-def malicious_pdf():
-    name = 'fixtures/malicious.pdf'
-    with open(module_path('tests.onegov.file', name), 'rb') as f:
-        yield f.read()
-
-
-@pytest.fixture(scope='function')
-def pdf_example():
-    name = 'fixtures/example.pdf'
-    with open(module_path('tests.onegov.file', name), 'rb') as f:
-        yield f.read()
