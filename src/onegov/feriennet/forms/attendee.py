60dd10bbb7fe466ef376adf275f6d3c154f7332f
Chantal Trutmann <79152553+BreathingFlesh@users.noreply.github.com>
2024-11-28 10:54:24 +0100

    Feriennet: Rename the field for health information
    TYPE: Feature
    LINK: PRO-1336

--- src/onegov/feriennet/forms/attendee.py
+++ src/onegov/feriennet/forms/attendee.py
@@ -120,7 +120,7 @@ class AttendeeForm(AttendeeBase):
     )
 
     notes = TextAreaField(
-        label=_('Health Information'),
+        label=_('Is there anything the course instructor should know?'),
         description=_('Allergies, Disabilities, Particulars'),
     )
 
