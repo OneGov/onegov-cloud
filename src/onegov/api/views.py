9e317877f1d21f11c2658415fde14c4c187fe65d
David Salvisberg <david.salvisberg@seantis.ch>
2024-12-18 10:50:43 +0100

    Api: Makes sure invisible endpoint items are inaccessible
    TYPE: Bugfix
    LINK: OGC-1992

--- src/onegov/api/views.py
+++ src/onegov/api/views.py
@@ -6,7 +6,7 @@ from onegov.api.token import get_token
 from onegov.api.utils import authenticate, check_rate_limit
 from onegov.core.security import Public
 from onegov.form.fields import HoneyPotField
-from webob.exc import HTTPMethodNotAllowed, HTTPUnauthorized
+from webob.exc import HTTPMethodNotAllowed, HTTPNotFound, HTTPUnauthorized
 from wtforms import HiddenField
 
 
@@ -156,6 +156,12 @@ def view_api_endpoint_item(
         assert endpoint is not None
         links = self.links or {}
         data = self.data or {}
+
+        # make sure we are actually supposed to be able to see this
+        # the API shouldn't include invisible items either (for now)
+        if (item := self.item) and not request.is_visible(item):
+            raise HTTPNotFound()
+
         payload: dict[str, JSONObject] = {
             'collection': {
                 'version': '1.0',
@@ -221,6 +227,11 @@ def edit_api_endpoint_item(
             api_key = authenticate(request)
             if api_key.read_only:
                 raise HTTPUnauthorized()
+
+        # make sure we are actually supposed to be able to see this
+        # the API shouldn't include invisible items either (for now)
+        if (item := self.item) and not request.is_visible(item):
+            raise HTTPNotFound()
 
         def walk_errors(
             errors: 'Sequence[str] | _FormErrors',
