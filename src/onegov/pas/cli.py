b7528e271438de8a15c5df32f044e8e2c5e33646
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-02-17 02:18:38 +0100

    provisional PAS import standalone.

---
+++ src/onegov/pas/cli.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+import click
+from onegov.core.cli import command_group
+from onegov.pas.constants import commission_expected_headers_variant_1,\
+    commission_expected_headers_variant_2
+from onegov.pas.data_import import import_commissions
+
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from onegov.pas.app import PasApp
+    from onegov.pas.app import TownRequest
+    from typing import TypeAlias
+
+    Processor: TypeAlias = Callable[[TownRequest, PasApp], None]
+
+
+cli = command_group()
+
+
+@cli.command('import-commission-data')
+@click.argument('excel_file', type=click.Path(exists=True))
+def import_commission_data(
+    excel_file: str,
+) -> Processor:
+    """Import commission data from an Excel or csv file.
+
+    Assumes that the name of the commission is the filename.
+
+    Each row of this import contains a single line, which is a single
+    parliamentarian and all the information about them.
+
+    Example:
+        onegov-pas --select '/onegov_pas/zug' import-commission-data \
+            "Kommission_Gesundheit_und_Soziales.xlsx"
+    """
+
+    def import_data(request: TownRequest, app: PasApp) -> None:
+
+        try:
+            import_commissions(
+                excel_file,
+                request.session,
+                excel_file,
+                expected_headers=commission_expected_headers_variant_1,
+            )
+            click.echo('Ok.')
+        except Exception as e:
+            click.echo('Trying the other variant of headers...')
+            import_commissions(
+                excel_file,
+                request.session,
+                excel_file,
+                expected_headers=commission_expected_headers_variant_2,
+            )
+            click.echo('Ok.')
+
+    return import_data
---
+++ src/onegov/pas/constants.py
@@ -0,0 +1,108 @@
+"""
+These values were obtained by running the following command:
+
+    onegov-pas --select '/onegov_pas/zug' import-commission-data \
+        "name_of_commission.xlsx"
+
+And then, inside the preprocess* function printing out the `header_row.`
+"""
+
+commission_expected_headers_variant_1 = [
+    'ID',
+    'Personalnummer',
+    'Vertragsnummer',
+    'Geschlecht',
+    'Vorname',
+    'Nachname',
+    'Versandart',
+    'Versand-Adresse',
+    'Versand-Adresszusatz',
+    'Versand-PLZ',
+    'Versand-Ort',
+    'Privat-Adresse',
+    'Privat-Adresszusatz',
+    'Privat-PLZ',
+    'Privat-Ort',
+    'Rolle Kommission',
+    'Partei',
+    'Fraktion',
+    'Wahlkreis',
+    'Eintritt Kommission',
+    'Austritt Kommission',
+    'Zusatzinformationen',
+    'Geburtsdatum',
+    'Bürgerort',
+    'Beruf',
+    'Akademischer Titel',
+    'Anrede',
+    'Adress-Anrede',
+    'Brief-Anrede',
+    'Spedition KR-Vorlagen',
+    'Telefon Privat',
+    'Telefon Mobile',
+    'Telefon Geschäft',
+    '1. E-Mail',
+    '2. E-Mail',
+    'Webseite',
+    'Bemerkungen',
+]
+
+# there is another type of commission import header row, which differs in the
+# headers diff:
+# Austritt, Eintritt, Funktion, Im Kantonsrat seit, Kantonsrat Funktion, Rolle, q
+
+
+commission_expected_headers_variant_2 = [
+    'q', # +
+    'Personalnummer',
+    'Vertragsnummer',
+    'Geschlecht',
+    'Vorname',
+    'Nachname',
+    'Versandart',
+    'Versand-Adresse',
+    'Versand-Adresszusatz',
+    'Versand-PLZ',
+    'Versand-Ort',
+    'Privat-Adresse',
+    'Privat-Adresszusatz',
+    'Privat-PLZ',
+    'Privat-Ort',
+    'Rolle', # + Parliamenarian Role, needs more: Stv.Landschreiberin
+    'Funktion',
+    'Kantonsrat Funktion',  # +
+    'Im Kantonsrat seit',
+    'Partei',
+    'Fraktion',
+    'Wahlkreis',
+    'Eintritt', # +
+    'Austritt', # +
+    'Zusatzinformationen',
+    'Geburtsdatum',
+    'Bürgerort',
+    'Beruf',
+    'Akademischer Titel',
+    'Anrede',
+    'Adress-Anrede',
+    'Brief-Anrede',
+    'Spedition KR-Vorlagen',
+    'Telefon Privat',
+    'Telefon Mobile',
+    'Telefon Geschäft',
+    '1. E-Mail',
+    '2. E-Mail',
+    'Webseite',
+    'Bemerkungen',
+]
+
+# fixme: incomplete, these needs mapping and implementation
+diff = [
+    'Austritt',
+    'Eintritt',
+    'Funktion',
+    'Im Kantonsrat seit',
+    'Kantonsrat',
+    'Funktion',
+    'Rolle',
+    'q',
+]
