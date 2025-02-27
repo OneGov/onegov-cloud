1f77b023b3b71e97453d5133a64634074818317c
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-02-27 07:06:55 +0100

    Adds vat settings to org

--- src/onegov/org/views/settings.py
+++ src/onegov/org/views/settings.py
@@ -21,7 +21,8 @@ from onegov.org.forms.settings import (
     OrgTicketSettingsForm, HeaderSettingsForm, FaviconSettingsForm,
     LinksSettingsForm, NewsletterSettingsForm, LinkMigrationForm,
     LinkHealthCheckForm, SocialMediaSettingsForm, EventSettingsForm,
-    GeverSettingsForm, OneGovApiSettingsForm, DataRetentionPolicyForm)
+    GeverSettingsForm, OneGovApiSettingsForm, DataRetentionPolicyForm,
+    VATSettingsForm)
 from onegov.org.management import LinkHealthCheck
 from onegov.org.layout import DefaultLayout
 from onegov.org.layout import SettingsLayout
@@ -528,6 +529,20 @@ def handle_ticket_data_deletion_settings(
         self, request, form, _('Data Retention Policy'),
         SettingsLayout(self, request),
     )
+
+
+@OrgApp.form(
+    model=Organisation, name='vat-settings', template='form.pt',
+    permission=Secret, form=VATSettingsForm, setting=_('VAT'),
+    icon='fa-file-invoice-dollar', order=450)
+def handle_vat_settings(
+        self: Organisation,
+        request: OrgRequest,
+        form: VATSettingsForm,
+        layout: SettingsLayout | None = None
+) -> RenderData | Response:
+    layout = layout or SettingsLayout(self, request, _('VAT'))
+    return handle_generic_settings(self, request, form, _('VAT'), layout)
 
 
 @OrgApp.form(
