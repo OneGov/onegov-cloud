37fdfe7dbfc32b72ba49fd96c8407717807f79c3
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-03-03 23:55:43 +0100

    Add `pytest.mark.skip_night_hours`.
    Postgres uses the date / time setting of the underlying OS (at
    least on UNIX-like systems) This is certainly not recommended and
    will invariably result in all sorts of nastiness, hence why we don't
    do that.
    
    `libfaketime` might be an option to consider. Currently we go with
    a pragmatic solution.

--- tests/shared/plugin.py
+++ tests/shared/plugin.py
@@ -1,7 +1,23 @@
 from .capturelog import CaptureLogPlugin
+from datetime import datetime
+from pytz import timezone
+import pytest
 
 
 def pytest_configure(config):
 
     # activate log capturing
     config.pluginmanager.register(CaptureLogPlugin(config), '_capturelog')
+    # Add the skip_night_hours marker
+    config.addinivalue_line(
+        'markers',
+        'skip_night_hours: mark test to skip during night hours',
+    )
+
+
+def pytest_runtest_setup(item):
+    if "skip_night_hours" in item.keywords:
+        zurich_tz = timezone('Europe/Zurich')
+        current_hour = datetime.now(zurich_tz).hour
+        if 22 <= current_hour or current_hour < 6:
+            pytest.skip('Skipping time-dependent test during night hours.')
