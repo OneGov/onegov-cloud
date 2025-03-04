2c7762dcd131eea948c5feb42e719fe92927b64f
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-03-04 23:55:31 +0100

    Fixes OGC-2122

--- src/onegov/org/forms/extensions.py
+++ src/onegov/org/forms/extensions.py
@@ -298,25 +298,30 @@ class PushNotificationFormExtension(FormExtension[FormT], name='publish'):
     def create(self, timezone: str = 'Europe/Zurich') -> type[FormT]:
 
         class PublicationForm(self.form_class):  # type:ignore
-            send_push_notifications_to_app = BooleanField(
-                label=_('Send push notifications to app'),
-                fieldset=_('Publication'),
-                validators=[StrictOptional()],
-                render_kw={'disabled': 'disabled'},  # Starts as disabled
-            )
-            push_notifications = MultiCheckboxField(
-                label=('Topics'),
-                choices=[],
-                fieldset=_('Publication'),
-                depends_on=('send_push_notifications_to_app', 'y'),
-                validators=[StrictOptional()],
-                render_kw={'class_': 'indent-form-field'}
-            )
+
+            def __init__(self, *args, **kwargs) -> None:
+                super().__init__(*args, **kwargs)
+                if self.request.app.org.firebase_adminsdk_credential:
+                    self._add_push_notification_fields()
+
+            def _add_push_notification_fields(self) -> None:
+                self.send_push_notifications_to_app = BooleanField(
+                    label=_('Send push notifications to app'),
+                    fieldset=_('Publication'),
+                    validators=[StrictOptional()],
+                    render_kw={'disabled': 'disabled'},
+                )
+                self.push_notifications = MultiCheckboxField(
+                    label=('Topics'),
+                    choices=[],
+                    fieldset=_('Publication'),
+                    depends_on=('send_push_notifications_to_app', 'y'),
+                    validators=[StrictOptional()],
+                    render_kw={'class_': 'indent-form-field'},
+                )
 
             def on_request(self) -> None:
-                if not self.request.app.org.meta.get(
-                        'firebase_adminsdk_credential'
-                ):
+                if not hasattr(self, 'send_push_notifications_to_app'):
                     return None
 
                 default_topic = [[self.request.app.schema, 'News']]
