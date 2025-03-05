6d862946dfa2ccd4b621dafff166c5873a688ce5
Chantal Trutmann <79152553+BreathingFlesh@users.noreply.github.com>
2025-03-05 12:56:30 +0100

    Town6: Sidebar name
    Create title of sidebar from h3 elements
    
    TYPE: Feature
    LINK: OGC-2043

--- src/onegov/town6/assets/js/sidebar_mobile.js
+++ src/onegov/town6/assets/js/sidebar_mobile.js
@@ -3,4 +3,23 @@ $('#offCanvasSidebar a').click(function() { $('.off-canvas').foundation('close')
 if (!$('.sidebar-wrapper').length) {
     $('.sidebar-toggler').css('display', 'none');
 }
+
+newSidebarTitle = $('#right-sidebar').data("sidebarMobileTitle");
+// Take the text of the first two h3 elements in the sidebar and use them as the title
+console.log('newSidebarTitle', newSidebarTitle);
+if (!newSidebarTitle) {
+    console.log('kei titel');
+    newSidebarTitle = $('.sidebar-wrapper h3').first().text();
+    if ($('.sidebar-wrapper h3').length > 2) {
+        newSidebarTitle += ", " + $('.sidebar-wrapper h3').eq(1).text();
+        newSidebarTitle += ", ...";
+    } else if ($('.sidebar-wrapper h3').length > 1) {
+        newSidebarTitle += " & " + $('.sidebar-wrapper h3').eq(1).text();
+    }
+    $('.sidebar-toggler span.text').text(newSidebarTitle);
+}
+if (newSidebarTitle) {
+    $('.sidebar-toggler span.text').text(newSidebarTitle);
+}
+
 $(".sidebar-wrapper").clone().appendTo("#offCanvasSidebar");
