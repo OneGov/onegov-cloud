384f0f811831abe16e65e4647fcc06f8074e9874
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-03-10 06:28:19 +0100

    Remove unnecessary upgrade task

--- src/onegov/search/upgrade.py
+++ src/onegov/search/upgrade.py
@@ -2,25 +2,3 @@
 upgraded on the server. See :class:`onegov.core.upgrade.upgrade_task`.
 
 """
-from __future__ import annotations
-from uuid import uuid4
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.dialects.postgresql import TSVECTOR, JSONB
-
-from onegov.core.orm.types import UUID
-from onegov.core.upgrade import upgrade_task, UpgradeContext
-
-
-@upgrade_task('fts: Adds search index table to each schema')
-def adds_search_index_table(context: UpgradeContext) -> None:
-    if not context.has_table('search_index'):
-        context.operations.create_table(
-            'search_index',
-            Column('id', UUID, primary_key=True, default=uuid4),
-            Column('owner_id_int', Integer, nullable=True),
-            Column('owner_id_uuid', UUID, nullable=True),
-            Column('owner_type', String, nullable=False),
-            Column('fts_idx_data', JSONB, default={}),
-            Column('fts_idx', TSVECTOR)
-        )
