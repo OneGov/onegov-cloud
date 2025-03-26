8864191fa19256bbdc33d48efd82ba1a778d8d36
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-03-26 12:16:09 +0100

    Cleanup

--- src/onegov/org/views/directory.py
+++ src/onegov/org/views/directory.py
@@ -493,7 +493,7 @@ def view_directory(
         'submit': request.link(self, name='+submit'),
         'show_thumbnails': layout.thumbnail_field_id and True or False,
         'thumbnail_link': layout.thumbnail_link,
-        'overview_two_columns': self.directory.overview_two_columns,
+        'overview_two_columns': self.directory.overview_two_columns
     }
 
 
