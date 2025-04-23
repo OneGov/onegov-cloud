7d04fe9b929648b00469205f0080eae64ae1b605
David Salvisberg <david.salvisberg@seantis.ch>
2025-04-23 16:53:02 +0200

    Fixes regression

--- src/onegov/org/views/reservation.py
+++ src/onegov/org/views/reservation.py
@@ -644,7 +644,7 @@ def finalize_reservation(self: Resource, request: OrgRequest) -> Response:
             if data := reservations[0].data:
                 ticket.tag = data.get('ticket_tag')
                 tag_meta = data.get('ticket_tag_meta')
-                key_code = tag_meta.pop('Kaba Code')
+                key_code = tag_meta.pop('Kaba Code', None)
                 if key_code:
                     ticket.handler_data = {'key_code': key_code}
                 ticket.tag_meta = tag_meta
