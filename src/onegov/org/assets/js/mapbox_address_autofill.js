95f4e29db60facc4211257544d1289dc33ac5e42
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-05-07 15:19:06 +0200

    refactor: Wrap mapbox autofill script in IIFE

--- src/onegov/org/assets/js/mapbox_address_autofill.js
+++ src/onegov/org/assets/js/mapbox_address_autofill.js
@@ -1,7 +1,9 @@
-const getMapboxTokenForAutofill = () => document.body.dataset.mapboxToken || false;
-mapboxsearch.config.accessToken = getMapboxTokenForAutofill();
-mapboxsearch.autofill({
-    options: {
-      country: 'ch'
-    }
-});
+(function() {
+    const getMapboxTokenForAutofill = () => document.body.dataset.mapboxToken || false;
+    mapboxsearch.config.accessToken = getMapboxTokenForAutofill();
+    mapboxsearch.autofill({
+        options: {
+          country: 'ch'
+        }
+    });
+})();
