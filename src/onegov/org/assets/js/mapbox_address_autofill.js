6017b373b75f9e03940f652ce1521f3b1317b3c9
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-05-07 15:13:21 +0200

    refactor: Simplify Mapbox autofill initialization

--- src/onegov/org/assets/js/mapbox_address_autofill.js
+++ src/onegov/org/assets/js/mapbox_address_autofill.js
@@ -1,13 +1,7 @@
-const getMapboxToken = () => document.body.dataset.mapboxToken || false;
 
-console.log('search-js');
-
-document.getElementById('search-js').addEventListener('load', () => {
-    console.log('search-js');
-  mapboxsearch.config.accessToken = getMapboxToken();
-  mapboxsearch.autofill({
+mapboxsearch.config.accessToken = getMapboxToken();
+mapboxsearch.autofill({
     options: {
       country: 'ch'
     }
-  });
 });
