73f1df06fe4dd8030e50fdd6836480cd8353f0ad
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-05-13 15:20:19 +0200

    feat: Configure mapbox-address-autofill with options and access token

--- src/onegov/org/assets/js/mapbox_address_autofill.js
+++ src/onegov/org/assets/js/mapbox_address_autofill.js
@@ -1,7 +1,14 @@
 (function() {
     const getMapboxToken = () => document.body.dataset.mapboxToken || false;
-    mapboxsearch.config.accessToken = getMapboxToken();
-    mapboxsearch.autofill({
-        country: 'ch'
-    });
+    // mapboxsearch.config.accessToken = getMapboxToken();
+    //
+    mapboxsearch.autofill(
+        accessToken: getMapboxToken(),
+        options={
+            country: 'ch',
+            language: 'de',
+            types: 'place,region', // Prioritize cities and cantons
+            streets: false
+        });
+
 })();
