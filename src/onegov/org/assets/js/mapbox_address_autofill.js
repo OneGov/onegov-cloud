980d42d88cb644596cff573f7c8aeb8f540a330e
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-05-13 15:54:27 +0200

    feat: Target hometown field and set Mapbox options correctly

--- src/onegov/org/assets/js/mapbox_address_autofill.js
+++ src/onegov/org/assets/js/mapbox_address_autofill.js
@@ -2,7 +2,8 @@
     const getMapboxToken = () => document.body.dataset.mapboxToken || false;
     mapboxsearch.config.accessToken = getMapboxToken();
     mapboxsearch.autofill({
-        options: {
+        querySelector: '#hometown',
+        mapboxSearchBoxOptions: {
             country: 'CH',
             language: 'de',
             types: 'place,region', // Prioritize cities and cantons
