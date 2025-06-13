7a1d8dd248cb3613fb414d53ec3acdad567018bf
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-06-13 13:23:27 +0200

    fix: Filter address results from mapbox address autofill

--- src/onegov/org/assets/js/mapbox_address_autofill.js
+++ src/onegov/org/assets/js/mapbox_address_autofill.js
@@ -12,9 +12,13 @@
             }
         });
 
+        console.log('bar')
+        console.log(autofillInstance)
         if (autofillInstance && autofillInstance.searchBox) {
+            console.log('fo')
             autofillInstance.searchBox.addEventListener('results', (event) => {
                 if (event.detail && event.detail.results) {
+                    console.log('event.detail.results')
                     // Filter out results identified as 'address' type.
                     event.detail.results = event.detail.results.filter(result =>
                         !(result.place_type && result.place_type.includes('address'))
