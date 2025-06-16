9b7eccf71736e40d7bf8a158e8c8066a90e9a4d4
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-06-16 16:49:42 +0200

    Cleanup

--- src/onegov/parliament/layouts/__init__.py
+++ src/onegov/parliament/layouts/__init__.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
 
-# from onegov.parliament.layouts.default import DefaultLayout
-# from onegov.parliament.layouts.party import PartyLayout
-
 
 __all__ = (
-    # 'DefaultLayout',
-    # 'PartyLayout',
 )
