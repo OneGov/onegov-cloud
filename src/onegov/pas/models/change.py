79566f3a37b291470273d8adac056e464f880a45
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-06-16 12:09:36 +0200

    Fix indent

--- src/onegov/pas/models/change.py
+++ src/onegov/pas/models/change.py
@@ -18,38 +18,37 @@ class PASChange(Change):
 
     es_type_name = 'pas_change'
 
+    @classmethod
+    def add(
+        cls,
+        request: TownRequest,
+        action: Action,
+        attendence: Attendence
+    ) -> Change:
+        """ Create a new change and add it to the session. """
 
-@classmethod
-def add(
-    cls,
-    request: TownRequest,
-    action: Action,
-    attendence: Attendence
-) -> Change:
-    """ Create a new change and add it to the session. """
-
-    change = cls()
-    change.user_id = request.current_username
-    change.user_name = (
-        request.current_user.title if request.current_user else None
-    )
-    change.action = action
-    change.model = 'attendence'
-    change.changes = {
-        'id': str(attendence.id),
-        'date': attendence.date.isoformat(),
-        'duration': attendence.duration,
-        'type': attendence.type,
-        'parliamentarian': (
-            attendence.parliamentarian.title
-            if attendence.parliamentarian else ''
-        ),
-        'parliamentarian_id': str(attendence.parliamentarian_id),
-        'commission_id': str(attendence.commission_id or ''),
-        'commission': (
-            attendence.commission.title
-            if attendence.commission else ''
-        ),
-    }
-    request.session.add(change)
-    return change
+        change = cls()
+        change.user_id = request.current_username
+        change.user_name = (
+            request.current_user.title if request.current_user else None
+        )
+        change.action = action
+        change.model = 'attendence'
+        change.changes = {
+            'id': str(attendence.id),
+            'date': attendence.date.isoformat(),
+            'duration': attendence.duration,
+            'type': attendence.type,
+            'parliamentarian': (
+                attendence.parliamentarian.title
+                if attendence.parliamentarian else ''
+            ),
+            'parliamentarian_id': str(attendence.parliamentarian_id),
+            'commission_id': str(attendence.commission_id or ''),
+            'commission': (
+                attendence.commission.title
+                if attendence.commission else ''
+            ),
+        }
+        request.session.add(change)
+        return change
