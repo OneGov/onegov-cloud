066d47528061ca1b2ac2d58959f8eeca05fc829d
Chantal Trutmann <chatru.work@gmail.com>
2025-06-26 15:41:08 +0200

    Add models

---
+++ src/onegov/parliament/models/meeting_item.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import uuid
+
+from sqlalchemy import Column, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+from onegov.core.orm import Base
+from onegov.core.orm.types import UUID
+from onegov.parliament.models import Meeting
+from onegov.search import ORMSearchable
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import uuid
+
+class MeetingItem(Base, ORMSearchable):
+
+    __tablename__ = 'par_meeting_items'
+
+
+    es_public = True
+    es_properties = {
+        'title': {'type': 'text'},
+        'number': {'type': 'text'}
+    }
+
+    @property
+    def es_suggestion(self) -> str:
+        return self.title
+
+    #: Internal ID
+    id: Column[uuid.UUID] = Column(
+        UUID,  # type:ignore[arg-type]
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    #: The title of the meeting item
+    title: Column[str] = Column(Text, nullable=False)
+
+    #: number of the meeting item
+    number: Column[str | None] = Column(Text, nullable=True)
+
+    #: political business id
+    political_business_id: Column[uuid.UUID | None] = Column(
+        UUID,  # type:ignore[arg-type]
+        ForeignKey('par_political_businesses.id'),
+    )
+
+    #: link ID only used for mapping after import
+    political_business_link_id: Column[str | None] = Column(
+        Text, nullable=True)
+
+    #: The id of the meeting
+    meeting_id: Column[uuid.UUID] = Column(
+        UUID,  # type:ignore[arg-type]
+        ForeignKey('par_meetings.id'),
+        nullable=False
+    )
+
+    #: The meeting
+    meeting: relationship[Meeting] = relationship(
+        Meeting,
+        back_populates='meeting_items'
+    )
+
+    def __repr__(self) -> str:
+        return f'<Meeting Item {self.number} {self.title}>'
