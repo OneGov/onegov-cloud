cf10cd06c2fb74d4187ed5482134bb2aab41a57d
David Salvisberg <david.salvisberg@seantis.ch>
2025-07-03 13:38:49 +0200

    Form: Adds an optional reset button to forms
    Adds that reset button to reservation forms for admins
    
    TYPE: Feature
    LINK: OGC-2390

--- src/onegov/form/assets/js/chosen-init.js
+++ src/onegov/form/assets/js/chosen-init.js
@@ -19,4 +19,32 @@ $(document).ready(function() {
             });
         }
     });
+
+    var label = {
+        de: 'Formular zurücksetzen',
+        en: 'Reset form',
+        fr: 'Réinitialiser le formulaire',
+        it: 'Modulo di reset'
+    };
+    var language = document.documentElement.getAttribute("lang").split('-')[0] || "en";
+
+    $('form.resettable').each(function(_, element) {
+        var button = $('<a href="#" class="button">' + label[language] + '</a>');
+        $(element).prepend(button);
+        button.click(function() {
+            $(element).find('input[type="checkbox"]:checked').trigger('click');
+            // eslint-disable-next-line max-nested-callbacks
+            $(element).find('.field-type-radiofield').each(function(_i, field) {
+                $(field).find('input[type="radio"]').eq(0).trigger('click');
+            });
+            $(element).find(
+                'input[type="radio"]:checked'
+            ).prop('checked', false).trigger('change');
+            $(element).find('select').val('').filter('.chosen-select').trigger('chosen:updated');
+            $(element).find('input').not(
+                '[type="submit"], [type="radio"], [type="checkbox"]'
+            ).val('').trigger('change');
+            return false;
+        });
+    });
 });
