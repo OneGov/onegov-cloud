afb56542ec70a99be229ebe07b599ca347b83e66
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-07-24 14:57:31 +0200

    Revert "Directories: Allow editors to edit publication dates"
    This reverts commit cf2c106c0437778347139534b5c49961e89afed8.

--- src/onegov/org/views/directory.py
+++ src/onegov/org/views/directory.py
@@ -96,8 +96,7 @@ def get_directory_entry_form_class(
             if model.directory.enable_map == 'no':
                 self.delete_field('coordinates')
 
-            if (not model.directory.enable_publication and
-                    not request.is_manager):
+            if not model.directory.enable_publication and not request.is_admin:
                 self.delete_field('publication_start')
                 self.delete_field('publication_end')
             elif model.directory.required_publication:
