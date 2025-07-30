45859f8ecc45d3f7b9bf18c4cb74de033031faf5
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-07-30 16:47:12 +0200

    feat: add custom.js asset

---
+++ src/onegov/pas/assets/js/custom.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const commissionSelect = document.getElementById('commission_id');
+    const parliamentarianList = document.getElementById('parliamentarian_id');
+
+    if (!commissionSelect || !parliamentarianList) return;
+
+    // Store all parliamentarians by commission
+    let commissionParliamentarians = {};
+
+    // Load all commission-parliamentarian data once
+    const baseUrl = window.location.href.split('/').slice(0, -2).join('/');
+    console.log(baseUrl)
+    fetch(`${baseUrl}/commissions/commissions-parliamentarians-json`)
+        .then(response => response.json())
+        .then(data => {
+            commissionParliamentarians = data;
+            // Initial update based on selected commission
+            updateParliamentarians(commissionSelect.value);
+        });
+
+    var commissionSelectChosne = document.getElementById('commission_id_chosen');
+    commissionSelectChosne.addEventListener('change', function() {
+        console.log('commissionSelect.addEventListener(change')
+        updateParliamentarians(commissionSelect.value);
+    });
+
+    function updateParliamentarians(commissionId) {
+        parliamentarianList.innerHTML = '';
+        if (!commissionId) return;
+
+        const parliamentarians = commissionParliamentarians[commissionId] || [];
+        parliamentarians.forEach(parliamentarian => {
+            const li = document.createElement('li');
+            const id = `parliamentarian_id-${parliamentarian.id}`;
+            li.innerHTML = `
+                <input type="checkbox" name="parliamentarian_id" value="${parliamentarian.id}" id="${id}">
+                <label for="${id}">${parliamentarian.title}</label>
+            `;
+            parliamentarianList.appendChild(li);
+        });
+    }
+});
