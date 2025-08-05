6af98c9c2f4a15175fa162c7e50c1e5ad9163ac1
Cyrill Küttel <36520284+cyrillkuettel@users.noreply.github.com>
2025-08-05 12:06:58 +0200

    Agency: Add `miscField` for sync.
    TYPE: Feature
    LINK: OGC-1894

--- src/onegov/agency/forms/person.py
+++ src/onegov/agency/forms/person.py
@@ -9,6 +9,8 @@ class AgencyPersonForm(PersonForm):
 
     external_user_id = StringField(_('External ID'))
 
+    staff_number = StringField(label=_('Personnel Number'))
+
 
 class AuthenticatedPersonMutationForm(PersonMutationForm):
     """ Like PersonMutationForm, but includes fields which shouldn't be
@@ -17,4 +19,8 @@ class AuthenticatedPersonMutationForm(PersonMutationForm):
 
     external_user_id = StringField(
         fieldset=_('Proposed changes'), label=_('External ID')
+    )
+
+    staff_number = StringField(
+        fieldset=_('Proposed changes'), label=_('Number')
     )
