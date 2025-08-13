c4ca632bc5e2c20bcde206be5bfd91edd3a12d4b
David Salvisberg <david.salvisberg@seantis.ch>
2025-08-13 15:15:37 +0200

    mypy: Adds type hints for `tests.onegov.async_http`

--- tests/onegov/async_http/test_async_fetch.py
+++ tests/onegov/async_http/test_async_fetch.py
@@ -1,24 +1,33 @@
+from __future__ import annotations
+
 import pytest
 from aiohttp import InvalidURL
 
 from onegov.async_http.fetch import async_aiohttp_get_all
 from onegov.core.utils import Bunch
 
+
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    from onegov.async_http.fetch import UrlType
+
 valid_urls = [
     'https://seantis.ch',
     'https://www.google.ch',
 ]
 
-invalid_urls = [
+invalid_urls: list[Any] = [
     'invalid.url.com',
     Bunch(url='xxx', name='Failed')
 ]
 
 
-@pytest.mark.parametrize('url', invalid_urls)
-def test_fetch_all_invalid(url):
+def test_fetch_all_invalid() -> None:
 
-    def handle_invalid(url, exception):
+    def callback(url: UrlType, response: object) -> UrlType:
+        return url
+
+    def handle_invalid(url: UrlType, exception: Exception) -> str:
         if isinstance(exception, InvalidURL):
             if isinstance(url, Bunch):
                 return url.name
@@ -30,13 +39,16 @@ def test_fetch_all_invalid(url):
         async_aiohttp_get_all(invalid_urls[0])
 
     results = async_aiohttp_get_all(
-        invalid_urls, handle_exceptions=handle_invalid)
+        invalid_urls,
+        callback=callback,
+        handle_exceptions=handle_invalid
+    )
 
     assert isinstance(results[0], str)
     assert results[1] == 'Failed'
 
 
-def test_fetch_all_valid():
+def test_fetch_all_valid() -> None:
     # get status 200 without waiting for the content
     results = async_aiohttp_get_all(valid_urls, response_attr='status')
     assert results == list(zip(valid_urls, [200, 200]))
