498d9470d1a23a3d27f5bfc009a227568418cb88
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-08-26 11:41:51 +0200

    add translations

--- src/onegov/pas/layouts/attendence.py
+++ src/onegov/pas/layouts/attendence.py
@@ -45,7 +45,7 @@ class AttendenceCollectionLayout(DefaultLayout):
                             attrs={'class': 'new-attendence'}
                         ),
                         Link(
-                            text=_('Commission session (bulk)'),
+                            text=_('Commission meeting (bulk)'),
                             url=self.request.link(
                                 self.model, 'new-commission-bulk'
                             ),
