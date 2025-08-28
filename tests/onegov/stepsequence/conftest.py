ae5efcd699c9423fb14bda7e2739547f8fe2a108
David Salvisberg <david.salvisberg@seantis.ch>
2025-08-28 14:16:43 +0200

    mypy: Adds type hints for `tests.onegov.(shared|stepsequence)`

--- tests/onegov/stepsequence/conftest.py
+++ tests/onegov/stepsequence/conftest.py
@@ -1,8 +1,23 @@
+from __future__ import annotations
+
 import pytest
 import onegov.stepsequence
+from onegov.stepsequence.core import StepSequenceRegistry
+
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 
 @pytest.fixture(scope='function')
-def sequences():
-    yield onegov.stepsequence.step_sequences
-    # onegov.stepsequence.step_sequences.registry = {}
+def sequences(
+    monkeypatch: pytest.MonkeyPatch
+) -> Iterator[StepSequenceRegistry]:
+    sequences = StepSequenceRegistry()
+    monkeypatch.setattr(onegov.stepsequence, 'step_sequences', sequences)
+    monkeypatch.setattr(
+        'onegov.stepsequence.extension.step_sequences',
+        sequences
+    )
+    yield sequences
