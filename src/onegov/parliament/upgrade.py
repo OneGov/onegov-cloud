b17beb2bfaa127b58fa10181f6808392908a5141
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-09-05 16:17:09 +0200

    Adds upgrade step

--- src/onegov/parliament/upgrade.py
+++ src/onegov/parliament/upgrade.py
@@ -99,3 +99,91 @@ def add_start_end_columns_to_meetings(
             'par_meetings',
             Column('end_datetime', UTCDateTime, nullable=True)
         )
+
+
+@upgrade_task('Add type column to parliament models 2nd attempt')
+def add_type_column_to_parliament_models(
+        context: UpgradeContext
+) -> None:
+    # this is only needed as on the first attempt type columns and
+    # mapper args were missing on quite some models and additional models
+    # were identified afterwards
+
+    for table, type_name, poly_type in (
+            ('par_attendence', 'poly_type', 'pas_attendence'),
+            ('par_changes', 'type', 'pas_change'),
+            ('par_commission_memberships', 'type', 'pas_commission_membership'),
+            ('par_commissions', 'poly_type', 'pas_commission'),
+            ('par_legislative_periods', 'type', 'pas_legislative_period'),
+            ('par_meeting_items', 'type', 'pas_meeting_items'),
+            ('par_parliamentarian_roles', 'type', 'pas_parliamentarian_role'),
+            ('par_parliamentarians', 'type', 'pas_parliamentarian'),
+            ('par_parliamentary_groups', 'type', 'pas_parliamentary_group'),
+            ('par_parties', 'type', 'pas_party'),
+            ('par_political_businesses', 'type', 'pas_political_businesses'),
+            ('par_political_business_participants', 'type', 'pas_political_business_participants')
+    ):
+        if not context.has_column(table, type_name):
+            context.operations.add_column(
+                table,
+                Column(type_name, Text, nullable=True)
+            )
+
+            context.operations.execute(
+                f"UPDATE {table} SET {type_name} = '{poly_type}'"
+            )
+
+            context.operations.execute(
+                f"ALTER TABLE {table} ALTER COLUMN {type_name} "
+                f"SET DEFAULT 'generic'"
+            )
+
+            context.operations.execute(
+                f'ALTER TABLE {table} ALTER COLUMN {type_name} SET NOT NULL'
+            )
+
+        # verify
+        # if context.has_column(table, type_name):
+        #     click.secho(f'Column {type_name} added to {table}', fg='green')
+        # else:
+        #     click.secho(f'Column {type_name} not added to {table}', fg='red')
+        #     raise ValueError('Verification failed')
+
+
+# @upgrade_task('PAR Fix RIS and PAS table inconsistencies')
+# def par_fix_ris_and_pas_table_inconsistencies(
+#     context: UpgradeContext
+# ) -> None:
+#
+#     # copy all items from still existing pas_attendence to par_attendence
+#
+#     ## NOTE: copying commissions is not needed as the data in par_commissions is better
+#     # commissions prior attendence
+#     # # FIXME: prevent duplicates or delete entries prior copying?
+#     # if context.has_table('par_commissions') and context.has_table('pas_commissions'):
+#     #     context.operations.execute('''
+#     #        INSERT INTO par_commissions (id, name, start, "end", "type", meta, content, created, modified, external_kub_id, poly_type)
+#     #        SELECT a.id, a.name, a.start, a.end, 'normal', a.meta, a.content, a.created, a.modified, a.external_kub_id, 'pas_commission'
+#     #        FROM pas_commissions a;
+#     #    ''')
+#
+#     # parliamentarians prior attendence
+#     # FIXME: prevent duplicates or delete entries prior copying?
+#     if context.has_table('par_parliamentarians') and context.has_table('pas_parliamentarians'):
+#         # delete before copying to prevent duplicates
+#         context.operations.execute('DELETE FROM par_parliamentarians;')
+#
+#         context.operations.execute('''
+#            INSERT INTO par_parliamentarians (type, id, external_kub_id, first_name, last_name, personnel_number, contract_number, gender, shipping_method, shipping_address, shipping_address_addition, shipping_address_city, private_address, private_address_addition, private_address_zip_code, private_address_city, date_of_birth, date_of_death, place_of_origin, party, occupation, academic_title, salutation, salutation_for_address, salutation_for_letter, forwarding_of_bills, phone_private, phone_mobile, phone_business, email_primary, email_secondary, website, remarks, meta, content, created, modified)
+#               SELECT 'pas_parliamentarian', a.id, a.external_kub_id, a.first_name, a.last_name, a.personnel_number, a.contract_number, a.gender, a.shipping_method, a.shipping_address, a.shipping_address_addition, a.shipping_address_city, a.private_address, a.private_address_addition, a.private_address_zip_code, a.private_address_city, a.date_of_birth, a.date_of_death, a.place_of_origin, a.party, a.occupation, a.academic_title, a.salutation, a.salutation_for_address, a.salutation_for_letter, a.forwarding_of_bills, a.phone_private, a.phone_mobile, a.phone_business, a.email_primary, a.email_secondary, a.website, a.remarks, a.meta, a.content, a.created, a.modified
+#               FROM pas_parliamentarians a;
+#          ''')
+#
+#     # FIXME: prevent duplicates or delete entries prior copying?
+#     if context.has_table('par_attendence') and context.has_table('pas_attendence'):
+#        context.operations.execute('''
+#             INSERT INTO par_attendence (id, date, duration, type, parliamentarian_id, commission_id, created, modified)
+#             SELECT a.id, a.date, a.duration, a.type::text::par_attendence_type, a.parliamentarian_id, a.commission_id, a.created, a.modified
+#             FROM pas_attendence a
+#             INNER JOIN par_commissions c ON a.commission_id = c.id;
+#         ''')
