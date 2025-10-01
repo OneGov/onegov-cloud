2f4594a845912a461ddeb474dbc2cd4a8df2403b
David Salvisberg <david.salvisberg@seantis.ch>
2025-10-01 17:02:22 +0200

    Pay: Upgrades to Stripe v13.0.0
    TYPE: Feature
    LINK: OGC-2658

--- src/onegov/pay/errors.py
+++ src/onegov/pay/errors.py
@@ -78,7 +78,7 @@ class SaferpayApiError(SaferpayPaymentError):
 # the following exceptions should be caught and logged - the user should be
 # informed that the payment failed, but not why
 CARD_ERRORS = (
-    stripe.error.CardError,
+    stripe.CardError,
     DatatransPaymentError,
     SaferpayPaymentError,
 )
