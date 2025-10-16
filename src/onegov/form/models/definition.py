e6a9cc4f245ab73db00703bc713b19d01929dc3c
Chantal Trutmann <79152553+BreathingFlesh@users.noreply.github.com>
2025-10-16 11:39:37 +0200

    Town6: Custom text above footer for Form mails
    TYPE: Feature
    LINK: OGC-2467

--- src/onegov/form/models/definition.py
+++ src/onegov/form/models/definition.py
@@ -155,6 +155,8 @@ class FormDefinition(Base, ContentMixin, TimestampMixin,
     #: tickets created through this form
     reply_to: dict_property[str | None] = meta_property()
 
+    custom_above_footer: dict_property[str | None] = meta_property()
+
     __mapper_args__ = {
         'polymorphic_on': 'type',
         'polymorphic_identity': 'generic'
