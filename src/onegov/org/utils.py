757f0c97c0205d75aca29fb28f05857a5279a275
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2025-10-28 13:35:36 +0100

    Fix erroneous data

--- src/onegov/org/utils.py
+++ src/onegov/org/utils.py
@@ -329,6 +329,8 @@ def render_time_range(start: datetime | time, end: datetime | time) -> str:
 
 
 def complete_url(url: str | None) -> str | None:
+    if 'keine' in url.lower():
+        return None
     if url is None or url.startswith(('http://', 'https://', '/')):
         return url
     return f'https://{url}'
