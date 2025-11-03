00bc84310444de8dbb4f8c71d273dfe98c61a634
Chantal Trutmann <chatru.work@gmail.com>
2025-11-03 15:53:23 +0100

    Register widget

--- src/onegov/feriennet/homepage_widgets.py
+++ src/onegov/feriennet/homepage_widgets.py
@@ -16,6 +16,34 @@ if TYPE_CHECKING:
     from onegov.feriennet.layout import DefaultLayout
 
 
+@FeriennetApp.homepage_widget(tag='registration')
+class RegistrationWidget:
+    template = """
+        <xsl:template match="registration">
+            <div tal:condition="not:request.is_logged_in" class="register">
+                <a href="./auth/register" class="button">
+                    ${register_text}
+                </a>
+                <a href="./auth/login" class="button secondary">
+                    ${login_text}
+                </a>
+            </div>
+            <div tal:condition="request.is_logged_in" class="register">
+                <a href="./userprofile" class="button secondary">
+                    ${profile_text}
+                </a>
+            </div>
+        </xsl:template>
+    """
+
+    def get_variables(self, layout: DefaultLayout) -> RenderData:
+        return {
+            'register_text': _('Register a new account'),
+            'login_text': _('Go to Login'),
+            'profile_text': _('Go to Profile')
+        }
+
+
 @FeriennetApp.homepage_widget(tag='activities')
 class ActivitiesWidget:
     template = """
