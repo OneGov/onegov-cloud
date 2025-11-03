4ca38cd8dfc53c5e267e596b51b6d7c8fc4b6b3f
Reto Tschuppert <reto.tschuppert@seantis.ch>
2025-11-03 10:30:34 -0500

    Add role

--- tests/onegov/town6/test_views_parliamentarian.py
+++ tests/onegov/town6/test_views_parliamentarian.py
@@ -20,10 +20,23 @@ def test_parliamentarians(client: Client) -> None:
     settings.form['ris_enabled'] = True
     settings.form.submit()
 
+    # add parliamentary group
+    page = client.get('/parliamentary-groups/new')
+    page.form['name'] = 'Die Moderne Fraktion'
+    page.form.submit()
+
+    page = client.get('/parliamentary-groups/new')
+    page.form['name'] = 'Old Party Fraktion'
+    page.form.submit()
+
+    # add commission
+
     # configure interest ties
     # TODO
 
     with freeze_time("2025-11-03 8:00"):
+        parliamentarian_pages = []
+
         page = client.get('/parliamentarians')
         assert ('Keine Filterergebnisse gefunden oder noch keine '
                 'Parlamentarier erfasst') in page
@@ -41,26 +54,59 @@ def test_parliamentarians(client: Client) -> None:
         # TODO: set interest tie
 
         parliamentarian = new.form.submit().follow()
+        parliamentarian_pages.append(parliamentarian)
         assert 'Funny' in parliamentarian
         assert 'Comedian' in parliamentarian
         assert 'funny.comedian@fun.org' in parliamentarian
         assert 'Die Moderne' in parliamentarian
 
-        # add another parliamentarian
-        # TODO
-
-        # Test filters
-        # TODO
-
         # edit parliamentarian
         edit = parliamentarian.click('Bearbeiten')
         edit.form['first_name'] = 'Laugh'
-        # TODO: add new role
-        # TODO: add new commission
         parliamentarian = edit.form.submit().follow()
         assert 'Laugh' in parliamentarian
 
+        # TODO: add new role
+        role = parliamentarian.click('Neue Fraktionsfunktion')
+        options = role.form['parliamentary_group_id'].options
+        id = next(
+            (opt[0] for opt in options if opt[2] == 'Die Moderne Fraktion'))
+        role.form['parliamentary_group_id'] = id
+        role.form['parliamentary_group_role'] = 'member'
+        parliamentarian = role.form.submit().follow()
+        assert 'Neue Rolle hinzugefügt' in parliamentarian
+        assert 'Mitglied Parlament' in parliamentarian
+        assert 'Mitglied Fraktion' in parliamentarian
+        assert 'Die Moderne Fraktion' in parliamentarian
+
+        # TODO: add new commission
+
+        # add another parliamentarian
+        new = client.get('/parliamentarians/new')
+        new.form['first_name'] = 'Old'
+        new.form['last_name'] = 'Man'
+        new.form['email_primary'] = 'old.man@cov.org'
+        new.form['party'] = 'Old Party'
+
+        parliamentarian = new.form.submit().follow()
+        parliamentarian_pages.append(parliamentarian)
+        assert 'Old' in parliamentarian
+        assert 'Man' in parliamentarian
+        assert 'old.man@cov.org' in parliamentarian
+        assert 'Old Party' in parliamentarian
+
+        # Test filters on overview
+        page = client.get('/parliamentarians')
+        page = page.click('Inaktiv')  # inactive by default for now
+        assert 'Comedian Laugh' in page
+        assert 'Die Moderne' in page
+        assert 'Man Old' in page
+        assert 'Old Party' in page
+        # TODO: test more
+
         # delete parliamentarian
-        parliamentarian.click('Löschen')
+        for parliamentarian in parliamentarian_pages:
+            parliamentarian.click('Löschen')
+        page = client.get('/parliamentarians')
         assert ('Keine Filterergebnisse gefunden oder noch keine '
                 'Parlamentarier erfasst') in page
