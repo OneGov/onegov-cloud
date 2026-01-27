a42802c12b46f8e1fac6684e9500bd727f20746e
Chantal Trutmann <chatru.work@gmail.com>
2026-01-27 09:47:16 +0100

    Add D3 Scripts

--- src/onegov/town6/app.py
+++ src/onegov/town6/app.py
@@ -360,5 +360,7 @@ def get_staff_client_asset() -> Iterator[str]:
 
 @TownApp.webasset('d3-charts')
 def get_d3_chart_assets() -> Iterator[str]:
+    yield 'd3-flextree.js'
+    yield 'd3.v7.min.js'
+    yield 'd3-org-chart.js'
     yield 'd3-display.js'
-    # yield 'd3-display.scss'
