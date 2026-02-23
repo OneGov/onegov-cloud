3d0b3fe3d4c4a1e5db126e8122a64214260c8393
Reto Tschuppert <124258444+Tschuppi81@users.noreply.github.com>
2026-02-23 14:10:39 +0100

    WAB: Handle invalid polling day date
    TYPE: Feature
    LINK: ogc-2785

--- src/onegov/election_day/formats/imports/election/ech.py
+++ src/onegov/election_day/formats/imports/election/ech.py
@@ -134,12 +134,20 @@ def import_information_delivery(
     """ Import an election information delivery. """
 
     assert delivery is not None
+    errors = set()
 
     # get polling date and entities
     assert delivery.polling_day is not None
     polling_day = delivery.polling_day.to_date()
-    entities = principal.entities[polling_day.year]
-    errors = set()
+    if polling_day.year not in principal.entities:
+        errors.add(
+            FileImportError(
+                _('Cannot import election information. '
+                  'Year ${year} does not exist.',
+                  mapping={'year': polling_day.year}),
+            ))
+        return polling_day, [], [], set(), errors
+    entities = principal.entities[polling_day.year]  # tschupre
 
     # query existing compounds
     existing_compounds = session.query(ElectionCompound).filter(
@@ -385,6 +393,15 @@ def import_result_delivery(
     errors: set[FileImportError]
 ) -> None:
     """ Import an election result delivery. """
+
+    if polling_day.year not in principal.entities:
+        errors.add(
+            FileImportError(
+                _('Cannot import election results. '
+                  'Year ${year} does not exist.',
+                  mapping={'year': polling_day.year}),
+            ))
+        return
 
     entities = principal.entities[polling_day.year]
 
