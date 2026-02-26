a605371b66cc2da38ea42359b78f52ef35062656
Reto Tschuppert <124258444+Tschuppi81@users.noreply.github.com>
2026-02-26 13:44:03 +0100

    assign layout
    Co-authored-by: David Salvisberg <david.salvisberg@seantis.ch>

--- src/onegov/org/request.py
+++ src/onegov/org/request.py
@@ -291,7 +291,7 @@ class OrgRequest(CoreRequest):
         Get the registered layout for a model instance.
         """
 
-        layout_class = self.app.get_layout(model)
-        assert layout_class is not None
+        layout= self.app.get_layout(model, self)
+        assert layout is not None
 
-        return layout_class(model, self)
+        return layout
