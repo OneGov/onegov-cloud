495848763e51813113a9485f8ff7c1ef8989b075
Reto Tschuppert <124258444+Tschuppi81@users.noreply.github.com>
2026-03-12 10:59:57 +0100

    Photoalbum: Remove error message for non images, simplify template
    TYPE: Bugfix
    LINK: ogc-2997

--- tests/onegov/org/test_views_photoalbum.py
+++ tests/onegov/org/test_views_photoalbum.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 import re
 
+from onegov.org.models import ImageFileCollection
+
 from tests.shared.utils import create_image
 from webtest import Upload
 
-
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from .conftest import Client
 
@@ -54,3 +56,44 @@ def test_manage_album(client: Client) -> None:
 
     album = albums.click("Comicon 2016")
     assert "This is an alt text" in album
+
+
+def test_image_selection(client: Client) -> None:
+    client.login_admin()
+
+    number_of_images = 3
+
+    albums = client.get('/photoalbums')
+    new = albums.click('Fotoalbum')
+    new.form['title'] = 'Vacation Destinations 2026'
+    new.form.submit()
+
+    album = client.get('/photoalbums').click('Vacation Destinations 2026')
+    for idx in range(number_of_images):
+        select = albums.click("Bilder verwalten")
+        select.form['file'] = [
+            Upload(f'image_{idx}.jpg', create_image().read())]
+        select.form.submit()
+
+    # select all images
+    select = album.click('Bilder auswählen')
+    select.form[tuple(select.form.fields.keys())[1]] = True
+    select.form[tuple(select.form.fields.keys())[2]] = True
+    select.form[tuple(select.form.fields.keys())[3]] = True
+    select.form.submit()
+
+    collection = ImageFileCollection(client.app.session()).query()
+    assert collection.count() == number_of_images
+    images = {(img.id, img.name) for img in collection.all()}
+    album = client.get('/photoalbums').click('Vacation Destinations 2026')
+    for img in images:
+        assert img[0] in album, '{} not found in album'.format(img)
+
+    # switch to grid mode
+    settings = album.click('Bearbeiten')
+    settings.form['view'] = 'grid'
+    settings.form.submit()
+
+    album = client.get('/photoalbums').click('Vacation Destinations 2026')
+    for img in images:
+        assert img[0] in album, '{} not found in album'.format(img)
