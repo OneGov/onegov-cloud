fc23186645eec7d4b8bac3e26f437080e03c7a82
Chantal Trutmann <chatru.work@gmail.com>
2026-06-11 14:02:58 +0200

    Add ol class to sane attributes

--- src/onegov/core/html.py
+++ src/onegov/core/html.py
@@ -55,7 +55,8 @@ SANE_HTML_ATTRS = {
     'abbr': ['title', ],
     'acronym': ['title', ],
     'img': ['src', 'alt', 'title'],
-    'p': ['class']
+    'p': ['class'],
+    'ol': ['class']
 }
 
 # lines without these plaintext characters are excluded in html_to_text
