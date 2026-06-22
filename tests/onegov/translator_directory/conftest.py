9d79a22dba761222e8a0201c49fa2ed26bf6abea
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2026-06-22 15:51:08 +0200

    Translator Directory: feature flag for time reporting.
    TYPE: Feature
    LINK: OGC-2851

--- tests/onegov/translator_directory/conftest.py
+++ tests/onegov/translator_directory/conftest.py
@@ -80,6 +80,7 @@ def create_translator_app(
     org = create_new_organisation(app, name="Übersetzerverzeichnis")
     org.meta['reply_to'] = 'mails@govikon.ch'
     org.meta['locales'] = 'de_CH'
+    org.meta['enable_time_tracking'] = True
 
     # usually we don't want to create the users directly, anywhere else you
     # *need* to go through the UserCollection. Here however, we can improve
