2c69d1e835881357788f29a529e815913d8ecccc
Reto Tschuppert <reto.tschuppert@seantis.ch>
2026-07-17 10:15:31 +0200

    Switch to raisload from lazyload

--- src/onegov/event/collections/occurrences.py
+++ src/onegov/event/collections/occurrences.py
@@ -18,7 +18,7 @@ from sqlalchemy import and_, or_, text
 from sqlalchemy.dialects.postgresql import array
 from sqlalchemy.orm import contains_eager
 from sqlalchemy.orm import joinedload
-from sqlalchemy.orm import lazyload
+from sqlalchemy.orm import raiseload
 from sqlalchemy.orm import undefer
 from webob.multidict import MultiDict
 
@@ -773,10 +773,10 @@ class OccurrenceCollection(Pagination[Occurrence]):
 
         query = self.session.query(Event).filter(Event.id.in_(event_ids))
         query = query.options(undefer(Event.content))
-        # skip the eager selectin loads get_ical_vevents doesn't need
+        # raise instead of eager-loading relations get_ical_vevents won't use
         query = query.options(
-            lazyload(Event.occurrences),
-            lazyload(Event.filter_keyword_objects),
+            raiseload(Event.occurrences),
+            raiseload(Event.filter_keyword_objects),
         )
         for event in query:
             for vevent in event.get_ical_vevents(request.link(event)):
