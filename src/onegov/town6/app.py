30150b58da675e2ca6e9d7beffe2f7771d773f12
Reto Tschuppert <reto.tschuppert@seantis.ch>
2026-08-06 11:53:27 +0200

    Web components initial draft for og callout

--- src/onegov/town6/app.py
+++ src/onegov/town6/app.py
@@ -295,6 +295,7 @@ def get_common_asset() -> Iterator[str]:
     yield 'chosen_select_hierarchy.js'
     yield 'iframe_request_parameters.js'
     yield 'ai_formcoder.js'
+    yield 'web-components.js'
 
 
 @TownApp.webasset('editor')
