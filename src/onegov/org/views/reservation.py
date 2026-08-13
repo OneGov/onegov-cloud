6adfdcc3e792e1f1c73336f234ef4b25ce90528e
David Salvisberg <david.salvisberg@seantis.ch>
2026-08-13 09:05:15 +0200

    Org: Stores pricing settings on reservations, so they remain stable
    Previously price calculations always referenced the current state of the
    pricing settings on the targeted resource/allocation, which could lead
    to issues in the transition period, when those settings change, since
    old reservations should still calculate their prices according to the
    rules when they were first made.
    
    We now store the settings on each reservation, similar to how we store
    custom form definitions along with each form submission, so old
    submissions always remain valid.
    
    TYPE: Bugfix
    LINK: OGC-3382

--- src/onegov/org/views/reservation.py
+++ src/onegov/org/views/reservation.py
@@ -237,6 +237,10 @@ def reserve_allocation(self: Allocation, request: OrgRequest) -> JSON_ro:
 
         return respond_with_error(request, err)
 
+    # store the current pricing settings on the reservation
+    data: dict[str, Any] = {}
+    resource.store_pricing_settings(data, self)
+
     # ...otherwise, try to reserve
     scheduler = resource.scheduler
     try:
@@ -245,6 +249,7 @@ def reserve_allocation(self: Allocation, request: OrgRequest) -> JSON_ro:
         scheduler.reserve(
             email='0xdeadbeef@example.org',  # will be set later
             dates=(start, end),
+            data=data,
             quota=quota,
             session_id=resource.bound_session_id(request),
             single_token_per_session=True
@@ -1988,6 +1993,7 @@ def add_reservation(
     # if the matched allocation isn't partly available expand it to the
     # whole allocation for a better user experience, but don't ever shrink
     # it, since that's most certainly not what they want to have happen.
+    allocation = None
     for allocation in resource.scheduler.allocations_in_range(start, end):
         if not allocation.overlaps(start, end):
             continue
@@ -2009,6 +2015,13 @@ def add_reservation(
         temp_token = resource.scheduler.reserve(
             self.email,
             (start, end),
+            # NOTE: Copy all of the settings apart from kaba, we will
+            #       sort out kaba further below
+            data={
+                key: value
+                for key, value in self.data.items()
+                if key != 'kaba'
+            } if self.data else None,
             quota=form.quota.data
         )
     except LibresError as e:
