5820826dc98f2b512b5bccd531e38dd205cf91d0
David Salvisberg <david.salvisberg@seantis.ch>
2026-09-02 15:58:41 +0200

    Improves test case

--- tests/onegov/pdf/test_pdf.py
+++ tests/onegov/pdf/test_pdf.py
@@ -450,6 +450,18 @@ def test_pdf_mini_html() -> None:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     )
     assert paragraphs[6] == 'Aenean'
+    # NOTE: The only reason this previously wasn't included is because
+    #       it happened to occur at the end and got thus transformed
+    #       into the element.tail of the ul element, which was not
+    #       being displayed, which was incorrect anyways. Our new
+    #       transformer is more correct and more robust, so it makes
+    #       sense that this text is included. If we want to remove
+    #       certain tags entirely we would need to pre-process the
+    #       the html to first remove all those tags at the root-level
+    #       before we sanitize it and strip the tags
+    assert paragraphs[7] == (
+        '#header h1 a { display: block; width: 300px; height: 80px; }'
+    )
 
     lists = [
         [li.text for li in l._flowables]  # type: ignore[attr-defined]
@@ -481,15 +493,6 @@ def test_pdf_mini_html() -> None:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean '
         'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. '
         'Aliquam tincidunt mauris eu risus. '
-        # NOTE: The only reason this previously wasn't included is because
-        #       it happened to occur at the end and got thus transformed
-        #       into the element.tail of the ul element, which was not
-        #       being displayed, which was incorrect anyways. Our new
-        #       transformer is more correct and more robust, so it makes
-        #       sense that this text is included. If we want to remove
-        #       certain tags entirely we would need to pre-process the
-        #       the html to first remove all those tags at the root-level
-        #       before we sanitize it and strip the tags
         '#header h1 a { display: block; width: 300px; height: 80px; }'
     )
 
