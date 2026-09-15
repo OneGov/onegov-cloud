b8b0c1d79801b9eb542cc9d99f8ebb5155beb38d
David Salvisberg <david.salvisberg@seantis.ch>
2026-09-15 16:27:58 +0200

    Org: Fixes pasting topics into iframe topics
    TYPE: Bugfix
    LINK: ONEGOV-CLOUD-5Z0

--- src/onegov/org/models/page.py
+++ src/onegov/org/models/page.py
@@ -97,26 +97,27 @@ class Topic(Page, TraitInfo, SearchableContent, AccessExtension,
 
     @property
     def paste_target(self) -> Topic | News:
-        if self.trait == 'link':
-            return self.parent or self  # type: ignore[return-value]
+        match self.trait:
+            case 'link' | 'iframe':
+                return self.parent or self  # type: ignore[return-value]
 
-        if self.trait == 'page':
-            return self
+            case 'page':
+                return self
 
-        raise NotImplementedError
+            case _:
+                raise NotImplementedError
 
     @property
     def allowed_subtraits(self) -> tuple[str, ...]:
-        if self.trait == 'link':
-            return ()
+        match self.trait:
+            case 'link' | 'iframe':
+                return ()
 
-        if self.trait == 'page':
-            return ('page', 'link', 'iframe')
+            case 'page':
+                return ('page', 'link', 'iframe')
 
-        if self.trait == 'iframe':
-            return ()
-
-        raise NotImplementedError
+            case _:
+                raise NotImplementedError
 
     def is_supported_trait(self, trait: str) -> bool:
         return trait in {'link', 'page', 'iframe'}
