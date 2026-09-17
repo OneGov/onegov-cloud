2b96120a222b537f2d34913244c9fc46ed29f24b
Reto Tschuppert <124258444+Tschuppi81@users.noreply.github.com>
2026-09-17 07:32:26 -0400

    Remove unneeded type coercion 3
    Co-authored-by: David Salvisberg <david.salvisberg@seantis.ch>

--- src/onegov/org/views/resource.py
+++ src/onegov/org/views/resource.py
@@ -1876,7 +1876,7 @@ def view_my_reservations_ical(
         stmt.c.accepted.is_(True)
     ]
     if reservation_token:
-        conditions.append(stmt.c.token == UUID(reservation_token))
+        conditions.append(stmt.c.token == reservation_token)
     records = request.session.execute(select(*stmt.c).where(and_(*conditions)))
 
     ticket_label = request.translate(_('Check request status'))
